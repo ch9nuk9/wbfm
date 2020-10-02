b882be2f49c2b1a0efb8aebb133d18c3a8dd9860
Fieseler <charles.fieseler@imp.ac.at>
2020-10-02 17:06:55 +0200

    Making this a packages

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='dlc_for_wbfm',
+      version='0.1',
+      description='Using DeepLabCut for Whole Brain Freely Moving neuron tracking',
+      url='https://github.com/Charles-Fieseler/DLC_for_WBFM.git',
+      author='Charles Fieseler',
+      author_email='charles.fieseler@gmail.com',
+      license='MIT',
+      zip_safe=False)
