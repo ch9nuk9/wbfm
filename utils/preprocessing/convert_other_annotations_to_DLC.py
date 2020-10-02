843b3ecafb2c22ed2669741c38caedac42014c23
Fieseler <charles.fieseler@imp.ac.at>
2020-10-02 16:08:47 +0200

    Add utils for bcpd annotation conversion and custom frame/volume extraction

---
+++ utils/preprocessing/convert_other_annotations_to_DLC.py
@@ -0,0 +1,54 @@
+import csv
+import h5py
+import pandas as pd
+import os
+
+def bpcd_tracker2config_names(path_config_file):
+    """
+    Automatically updates the config file with the proper number of neurons, and deletes any other default bodyparts.
+    Only affects the "bodyparts" field
+    """
+
+    # Get number of neurons from annotations
+    home = os.path.dirname(path_config_file)
+    # TODO: hardcoded folder
+    annotations_fname = os.path.join(home,'labeled-data', 'test_100frames.ome','CollectedData_Charlie.csv')
+    df = pd.read_csv(annotations_fname)
+    num_neurons = int(df.shape[1] / 3)
+    print("Adding body part annotations for {} neurons".format(num_neurons))
+#     error()
+
+    # Read in entire config file into a list
+    config_rows = []
+    with open(path_config_file) as config:
+        c_reader = csv.reader(config)#, delimiter=' ')
+        for row in c_reader:
+            config_rows.append(row)
+
+    ## Delete the current bodypart lines
+    delete_these_rows = False
+    config_rows_edit = config_rows.copy()
+    for row in config_rows:
+        if row == ['bodyparts:']:
+            delete_these_rows = True # Start deleting next row
+        elif row == ['start: 0']:
+            delete_these_rows = False # Do not delete this row, or others
+            break
+        elif delete_these_rows == True:
+            # Don't delete either of the two above, but only in between those rows
+            config_rows_edit.remove(row)
+
+    ## Add in the named neuron lines
+    # Using "list slicing" https://www.geeksforgeeks.org/python-insert-list-in-another-list/
+    new_names = [['- neuron{}'.format(i)] for i in range(num_neurons)]
+    insert_index = config_rows_edit.index(['start: 0'])
+    config_rows_edit[insert_index:insert_index] = new_names
+
+    ## Write the file again
+    if True:
+        with open(path_config_file, 'w', newline='') as config:
+            c_writer = csv.writer(config)
+            for row in config_rows_edit:
+                c_writer.writerow(row)
+
+    print("Finished! Check the config.yaml file to make sure the bodyparts are properly written"
---
+++ utils/preprocessing/custom_frame_extraction.py
@@ -0,0 +1,141 @@
+
+
+#
+# Basically this is a custom extract_frames() function
+#
+
+from pathlib import Path
+from deeplabcut.utils import auxiliaryfunctions
+import tifffile
+import platform
+import os
+
+def extract_volumes_from_many_files(path_config_file, which_vol=None):
+    """
+    Assumes a folder of many different volumes, and copies some in the folder given by 'out_folder'
+    """
+
+    if platform.system() is 'Windows':
+        is_windows = True
+    else:
+        is_windows = False
+
+    config_file = Path(path_config_file).resolve()
+    cfg = auxiliaryfunctions.read_config(config_file)
+    print("Config file read successfully.")
+
+    video_fnames = [i for i in cfg['video_sets'].keys()]
+    print(type(video_fnames))
+    print("Found {} volumes.".format(len(video_fnames)))
+
+    # Read basic metadata
+
+    # Get volume indices to save
+    if which_vol is None:
+        which_vol = [0]
+
+    for i_vol in which_vol:
+        print("Reading volume {}/{}".format(i_vol, len(which_vol)))
+
+        this_fname = video_fnames[i_vol]
+
+        # Read and make output name
+        this_volume = tifffile.imread(this_fname)
+        output_name = 'img{}.tif'.format(i_vol)
+
+        # Save in output folder
+        fname = Path(video_fnames[0])
+        output_path = os.path.join(Path(path_config_file).parents[0],'labeled-data',fname.stem)
+
+        tifffile.imsave(os.path.join(str(output_path),output_name), this_volume)
+
+        print('Saved volume to {}\\{}'.format(output_path, output_name))
+
+def extract_volumes_from_MATLAB_output(path_config_file, which_vol=None):
+    """
+    Takes a video filename, which is a large ome-tiff file, and saves a volume in the folder given by 'out_folder'
+    """
+
+    if platform.system() is 'Windows':
+        is_windows = True
+    else:
+        is_windows = False
+
+    config_file = Path(path_config_file).resolve()
+    cfg = auxiliaryfunctions.read_config(config_file)
+    print("Config file read successfully.")
+
+    video_fname = [i for i in cfg['video_sets'].keys()][0] # Assume one video for now (will be giant, ~3GB)
+    print(video_fname)
+
+    # Read basic metadata to get 'nz' and 'nt'
+    with tifffile.TiffFile(video_fname) as vid:
+        #print(tifffile.xml2dict(vid.ome_metadata))
+        print(vid.ome_metadata)
+        ome_metadata = vid.ome_metadata
+        if isinstance(ome_metadata, str):
+            # Appears to be a bug that just returns a string... can fix manually though
+            ome_metadata = tifffile.xml2dict(ome_metadata)['OME']
+        print(ome_metadata.keys())
+        mdat = ome_metadata['Image']['Pixels']
+        nz, nt = mdat['SizeZ'], mdat['SizeT']
+
+    # Get volume indices to save
+    if which_vol is None:
+        which_vol = [0]
+
+    for i_vol in which_vol:
+        print("Read volume {}/{}".format(i_vol, len(which_vol)))
+
+        # Convert scalar volume label to the sequential frames
+        # Note: this may change for future input videos!
+        vol_indices = list(range(i_vol*nz, i_vol*nz + nz))
+
+        # Read and make output name
+        this_volume = tifffile.imread(video_fname, key=vol_indices)
+        output_name = 'img{}.tif'.format(i_vol)
+
+        # Save in output folder
+        fname = Path(video_fname)
+        output_path = os.path.join(Path(path_config_file).parents[0],'labeled-data',fname.stem)
+
+        tifffile.imsave(os.path.join(str(output_path),output_name), this_volume)
+
+        print('Saved volume to {}\\{}'.format(output_path, output_name))
+
+
+def extract_volumes_from_charlie_output(path_config_file, nz, which_vol=None):
+    """
+    Takes a video filename, which is a large ome-tiff file, and saves a volume in the folder given by 'out_folder'
+    """
+
+    config_file = Path(path_config_file).resolve()
+    cfg = auxiliaryfunctions.read_config(config_file)
+    print("Config file read successfully.")
+
+    video_fname = [i for i in cfg['video_sets'].keys()][0] # Assume one video for now (will be giant, ~3GB)
+    print(video_fname)
+
+    # Get volume indices to save
+    if which_vol is None:
+        which_vol = [0]
+
+    for i_vol in which_vol:
+        print("Reading volume {}/{}".format(i_vol, len(which_vol)-1))
+
+        # Convert scalar volume label to the sequential frames
+        # Note: this may change for future input videos!
+        vol_indices = list(range(i_vol*nz, i_vol*nz + nz))
+        print("Converted volume indices: {} to {} (not including last frame)".format(i_vol*nz, i_vol*nz + nz))
+
+        # Read and make output name
+        this_volume = tifffile.imread(video_fname, key=vol_indices)
+        output_name = 'img{}.tif'.format(i_vol)
+
+        # Save in output folder
+        fname = Path(video_fname)
+        output_path = os.path.join(Path(path_config_file).parents[0],'labeled-data',fname.stem)
+
+        tifffile.imsave(os.path.join(str(output_path),output_name), this_volume)
+
+        print('Saved volume to {}'.format(os.path.join(output_path, output_name)))
