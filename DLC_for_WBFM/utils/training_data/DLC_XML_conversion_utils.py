d2b582a0168ca058b3e8438be76732afc43cab8a
Charles-Fieseler <charles.fieseler@imp.ac.at>
2020-10-22 12:01:17 +0200

    New function for automatically updating config file from Icy annotations

--- DLC_for_WBFM/utils/training_data/DLC_XML_conversion_utils.py
+++ DLC_for_WBFM/utils/training_data/DLC_XML_conversion_utils.py
@@ -89,9 +89,12 @@ def icy_xml_to_dlc(path_config_file,
     # scorer = "test"
     output_path = os.path.join(project_folder, 'labeled-data', xml_folder)
     dataFrame.to_csv(os.path.join(output_path,"CollectedData_" + scorer + ".csv"))
-    dataFrame.to_hdf(os.path.join(output_path,"CollectedData_" + scorer + '.h5'),'df_with_missing',format='table', mode='w')
+    deeplabcut.convertcsv2h5(path_config_file, userfeedback=False)
+    # dataFrame.to_hdf(os.path.join(output_path,"CollectedData_" + scorer + '.h5'),'df_with_missing',format='table', mode='w')
 
     print(f"Finished; wrote {i_neuron_name} neurons")
+
+    return dataFrame
 
 
 ##
@@ -152,3 +155,71 @@ def find_xml_in_project(path_config_file):
             break
 
     return project_folder, xml_folder, xml_fname, png_fnames
+
+
+
+##
+## Synchronizing the config file
+##
+
+def csv_annotations2config_names(path_config_file, num_dims=2, actually_write=False):
+    """
+    Automatically updates the config file with the proper number of neurons, and deletes any other default bodyparts.
+    Only affects the "bodyparts" field
+    """
+
+    # Get number of neurons from annotations
+    home = os.path.dirname(path_config_file)
+    # TODO: must have XML in the folder to find it
+    annotations_fname = get_annotations_converted_from_xml(path_config_file)
+    # TODO: hardcoded folder
+    # annotations_fname = os.path.join(home,'labeled-data', 'test_100frames.ome','CollectedData_Charlie.csv')
+    df = pd.read_csv(annotations_fname)
+    num_neurons = int(df.shape[1] / num_dims)
+    print("Adding body part annotations for {} neurons".format(num_neurons))
+
+    # Read in entire config file into a list
+    config_rows = []
+    with open(path_config_file) as config:
+        c_reader = csv.reader(config)#, delimiter=' ')
+        for row in c_reader:
+            config_rows.append(row)
+
+    ## Delete the current bodypart lines
+    delete_these_rows = False
+    config_rows_edit = config_rows.copy()
+    for row in config_rows:
+        if row == ['bodyparts:']:
+            delete_these_rows = True # Start deleting next row
+        elif row == ['start: 0']:
+            delete_these_rows = False # Do not delete this row, or others
+            break
+        elif delete_these_rows == True:
+            # Don't delete either of the two above, but only in between those rows
+            config_rows_edit.remove(row)
+
+    ## Add in the named neuron lines
+    # Using "list slicing" https://www.geeksforgeeks.org/python-insert-list-in-another-list/
+    new_names = [['- neuron{}'.format(i)] for i in range(num_neurons)]
+    insert_index = config_rows_edit.index(['start: 0'])
+    config_rows_edit[insert_index:insert_index] = new_names
+
+    ## Write the file again
+    if actually_write:
+        with open(path_config_file, 'w', newline='') as config:
+            c_writer = csv.writer(config)
+            for row in config_rows_edit:
+                c_writer.writerow(row)
+
+    print("Finished! Check the config.yaml file to make sure the bodyparts are properly written")
+
+
+def get_annotations_converted_from_xml(path_config_file):
+
+    _, xml_folder, _, _ = find_xml_in_project(path_config_file)
+    config_file = pathlib.Path(path_config_file).resolve()
+    cfg = deeplabcut.auxiliaryfunctions.read_config(config_file)
+    scorer = cfg['scorer']
+    output_path = os.path.join(project_folder, 'labeled-data', xml_folder)
+
+    return os.path.join(output_path,"CollectedData_" + scorer + ".csv")
