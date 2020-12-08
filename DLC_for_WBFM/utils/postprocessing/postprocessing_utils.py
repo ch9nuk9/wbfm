7286a3a5dceab67c1e267ff7990a76b73c063d29
Charles-Fieseler <charles.fieseler@imp.ac.at>
2020-12-08 18:07:36 +0100

    Helper function for checking annotations

--- DLC_for_WBFM/utils/postprocessing/postprocessing_utils.py
+++ DLC_for_WBFM/utils/postprocessing/postprocessing_utils.py
@@ -59,6 +59,18 @@ def xy_from_dlc_dat(fname, which_neuron=0, num_frames=100):
     return this_xy, this_prob
 
 
+def get_number_of_annotations(annotation_fname):
+    # Get the number of neurons and frames annotated
+    with h5py.File(annotation_fname, 'r') as dlc_dat:
+        dlc_table = dlc_dat['df_with_missing']['table']
+        # Each table entry has: x, y, probability
+        num_neurons = len(dlc_table[0][1])//3
+        which_neurons = range(num_neurons)
+        num_frames = len(dlc_table)
+    print(f'Found annotations for {num_neurons} neurons and {num_frames} frames')
+
+    return num_neurons, which_neurons, num_frames
+
 ##
 ## Building cropped videos
 ##
