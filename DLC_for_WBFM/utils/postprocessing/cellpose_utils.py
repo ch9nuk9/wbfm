7e28a397417b1a9489c027adfc5108745ef5b44d
Charles-Fieseler <charles.fieseler@imp.ac.at>
2020-12-08 17:27:37 +0100

    Utilities for use with cellpose-based segmentation

---
+++ DLC_for_WBFM/utils/postprocessing/cellpose_utils.py
@@ -0,0 +1,161 @@
+## Utilities for trace extraction via cellpose-based segmentation
+
+
+# Use the project config file
+from DLC_for_WBFM.bin.configuration_definition import *
+from DLC_for_WBFM.utils.postprocessing.postprocessing_utils import _get_crop_from_ometiff_virtual
+from cellpose import models
+from cellpose import utils as cellutils
+from scipy.ndimage import center_of_mass
+
+
+def extract_single_trace_cp(config_filename,
+                            which_neuron=None,
+                            num_frames=100,
+                            cellpose_opt={'diameter':8}):
+    """
+    Extracts single trace of a single neuron via cellpose segmentation
+
+    Returns:
+
+    all_traces : [dict,...]
+        Array of dicts, where the keys are 'red', 'green', and 'num_pixels'
+        Each final element is a 1d array
+    """
+
+    # Two channels
+    cropped_dat_red = _get_crop_from_ometiff_virtual(fname,
+                                                     which_neuron=which_neuron,
+                                                     num_frames=num_frames,
+                                                     use_red_channel=True)
+
+    cropped_dat_green = _get_crop_from_ometiff_virtual(fname,
+                                                       which_neuron=which_neuron,
+                                                       num_frames=num_frames,
+                                                       use_red_channel=False)
+    # Do the segmentation on red only
+    channels = [0,0]
+    model = models.Cellpose(gpu=False, model_type='nuclei')
+    # Get time series of segmentations
+    all_masks = []
+
+    for i in range(num_frames):
+        this_vol = np.squeeze(cropped_dat_red[i,...])
+        m, f, s, d = model.eval(this_vol,
+                                channels=channels,
+                                do_3D=True,
+                                **cellpose_opt)
+
+        print(f"Segmenting Volume: {i}/{num_frames}")
+        all_masks.append(m)
+
+    # TODO: better saving
+    fname = f'test_masks_neuron_{which_neuron}'
+    pickle.dump(all_masks, open(fname, 'wb'))
+
+    # Finally, get traces
+    trace_red = np.zeros(num_frames)
+    trace_green = np.zeros(num_frames)
+    num_pixels = np.zeros(num_frames)
+
+    for i, m in enumerate(all_masks):
+        this_vol_red = np.squeeze(cropped_dat_red[i,...])
+        this_vol_green = np.squeeze(cropped_dat_green[i,...])
+        # TODO: Assume there is only one neuron detected
+        trace_red[i] = brightness_from_roi(this_vol_red, m, 1)
+        trace_green[i] = brightness_from_roi(this_vol_green, m, 1)
+        num_pixels[i] = np.count_nonzero(m)
+
+    final_trace = {'red': trace_red,
+                   'green': trace_green,
+                   'num_pixels': num_pixels}
+
+    return final_trace
+
+
+def brightness_from_roi(img, all_masks, which_neuron):
+    """
+    Just averages the pixels given a 3d mask
+
+    TODO: better determination of brightness
+    """
+    mask = all_masks==which_neuron
+    return np.mean(img[mask])
+
+
+##
+## Functions for linking across time
+##
+
+
+def calc_best_overlap(mask_v0, # Only one mask
+                      masks_v1):
+    """
+    Calculates the best overlap between an initial mask and all subsequent masks
+        Note: calculates pairwise for adjacent time points
+    """
+    best_overlap = 0
+    best_ind = None
+    best_mask = np.zeros_like(masks_v1)
+    all_vals = np.unique(masks_v1)
+    for i,val in enumerate(all_vals):
+        if val == 0:
+            continue
+        this_neuron_mask = masks_v1==val
+        overlap = np.count_nonzero(mask_v0*this_neuron_mask)
+        if overlap > best_overlap:
+            best_overlap = overlap
+            best_ind = i
+            best_mask = this_neuron_mask
+
+    print(f'Best Neuron: {best_ind}, overlap between {best_overlap} and original')
+    return best_ind, best_overlap, best_mask
+
+def calc_all_overlaps(start_neuron,
+                      all_multi_masks):
+    """
+    Get the "tube" of a neuron through time via most overlapping pixels
+
+    See also: calc_best_overlap
+    """
+    num_frames = len(all_multi_masks)
+    all_masks = []
+    all_neurons = np.zeros(num_frames)
+    all_overlaps = np.zeros(num_frames)
+
+    # Initial neuron
+    all_masks.append(all_multi_masks[0] == start_neuron)
+    all_neurons[0] = start_neuron
+
+    for i, masks_v1 in enumerate(all_multi_masks):
+        if i==0:
+            continue
+        prev_mask = all_multi_masks[i-1] == all_neurons[i-1]
+
+        all_neurons[i], all_overlaps[i], this_mask = calc_best_overlap(prev_mask, masks_v1)
+        all_masks.append(this_mask)
+
+    return all_neurons, all_overlaps, all_masks
+
+def calc_center_neuron(initial_mask):
+    """
+    Calculates neuron that is closest to the center
+
+    Uses: scipy.ndimage.center_of_mass
+    """
+    center_point = np.array(initial_mask.shape) / 2.0
+    all_vals = np.unique(initial_mask)
+    closest_neuron = 0
+    best_dist = np.inf
+    for i in all_vals:
+        if i==0:
+            continue
+        this_center = center_of_mass(initial_mask==i)
+#         print(this_center)
+        dist = np.linalg.norm(this_center-center_point)
+        if dist < best_dist:
+            closest_neuron = i
+            best_dist = dist
+
+    print(f'Found closest neuron to be {closest_neuron}')
+    return closest_neuron, best_dist
