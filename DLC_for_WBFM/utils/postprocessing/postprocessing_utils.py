4064f1c0ae75b480ff967b1fa038eaa1b8d391f2
Charles-Fieseler <charles.fieseler@imp.ac.at>
2020-12-12 14:26:02 +0100

    New plotting functions

--- DLC_for_WBFM/utils/postprocessing/postprocessing_utils.py
+++ DLC_for_WBFM/utils/postprocessing/postprocessing_utils.py
@@ -478,16 +478,6 @@ def plot3d_with_max_and_hist(dat, z, t, max_ind):
 ## Functions for use with data from 'extract_all_traces'
 ##
 
-def visualize_all_traces(all_traces, all_names=None,
-                         to_save=False):
-    all_names = check_default_names(all_names, len(all_traces))
-
-    for i, t_dict in enumerate(all_traces):
-        visualize_mcherry_and_gcamp(t_dict, name=all_names[i])
-        if to_save:
-            plt.savefig(f'traces_{all_names[i]}')
-
-
 def visualize_traces_with_reference(all_traces,
                                     reference_ind, reference_name,
                                     all_names=None,
@@ -516,20 +506,20 @@ def visualize_traces_with_reference(all_traces,
             plt.savefig(f'traces_{all_names[i]}_ref_{reference_name}')
 
 
-def visualize_mcherry_and_gcamp(t_dict, name,
+def visualize_mcherry_and_gcamp(t_dict,
+                                name,
+                                which_neuron,
                                 make_new_fig=True,
                                 make_new_title=True,
-                                ax1 = None, ax2 = None,
+                                ax1 = None,
+                                ax2 = None,
                                 to_normalize=False):
     if make_new_fig:
         plt.figure(figsize=(35,5))#, fontsize=12)
 
     if make_new_fig:
         ax1 = plt.subplot(121)
-    try:
-        dat = t_dict['mcherry']
-    except:
-        dat = t_dict['red']
+    dat = get_tracking_channel(t_dict)
     if to_normalize:
         dat = dat / np.max(np.array(dat))
     if make_new_title:
@@ -541,10 +531,7 @@ def visualize_mcherry_and_gcamp(t_dict, name,
 
     if make_new_fig:
         ax2 = plt.subplot(122)
-    try:
-        dat = t_dict['gcamp']
-    except:
-        dat = t_dict['green']
+    dat = get_measurement_channel(t_dict)
     if to_normalize:
         dat = dat / np.max(np.array(dat))
     if make_new_title:
@@ -554,9 +541,102 @@ def visualize_mcherry_and_gcamp(t_dict, name,
         ax2.plot(dat, label=f'Green channel for neuron {name}')
         ax2.legend()
 
+    set_big_font()
+
     return ax1, ax2
+
+
+def visualize_ratio(t_dict,
+                    name,
+                    which_neuron,
+                    background=[0,0],
+                    preprocess_func=None):
+    """
+    Divides the green by the red channel to produce a normalized time series
+        Optionally subtracts a background value
+    """
+    plt.figure(figsize=(35,5))
+
+    red = get_tracking_channel(t_dict)
+    green = get_measurement_channel(t_dict)
+
+    if preprocess_func is not None:
+        red = preprocess_func(red, which_neuron)
+        green = preprocess_func(green, which_neuron)
+
+    plt.plot((green-background[0]) / (red-background[1]))
+    plt.title(f"Ratiometric for neuron {name}")
+
+
+def visualize_all_traces(all_traces,
+                         all_names=None,
+                         plot_subfunction=visualize_mcherry_and_gcamp,
+                         preprocess_func=None,
+                         to_save=False):
+    """
+    Plots all neurons in a struct using a subfunction with the following API:
+        plot_subfunction(t_dict,
+                         which_neuron=i,
+                         name=all_names[i],
+                         preprocess_func=preprocess_func)
+    """
+    all_names = check_default_names(all_names, len(all_traces))
+
+    for i, t_dict in enumerate(all_traces):
+        plot_subfunction(t_dict,
+                         which_neuron=i,
+                         name=all_names[i],
+                         preprocess_func=preprocess_func)
+        if to_save:
+            plt.savefig(f'traces_{all_names[i]}')
+
+
+
+##
+## Helper functions
+##
 
 def check_default_names(all_names, num_neurons):
     if all_names is None:
         all_names = [str(i) for i in range(num_neurons)]
     return all_names
+
+
+def set_big_font(size=22):
+    # From: https://stackoverflow.com/questions/3899980/how-to-change-the-font-size-on-a-matplotlib-plot
+    import matplotlib
+
+    font = {'weight' : 'bold',
+            'size'   : size}
+    matplotlib.rc('font', **font)
+
+
+def get_tracking_channel(t_dict):
+    try:
+        dat = t_dict['mcherry']
+    except:
+        dat = t_dict['red']
+    return dat
+
+def get_measurement_channel(t_dict):
+    try:
+        dat = t_dict['gcamp']
+    except:
+        dat = t_dict['green']
+    return dat
+
+def nan_tracking_failures(config,
+                          dat,
+                          which_neuron,
+                          threshold=0.9):
+    c = load_config(config)
+
+    _, this_prob = xy_from_dlc_dat(c.tracking.annotation_fname,
+                                   which_neuron,
+                                   c.preprocessing.num_frames)
+
+    bad_vals = np.array(this_prob) < threshold
+    dat[bad_vals] = np.nan
+    # print(np.count_nonzero(this_prob < threshold))
+
+    return dat
