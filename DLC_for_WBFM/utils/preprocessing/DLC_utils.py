e01a7fa8f9d1805040fa0af5ce38e184a2953c4e
Charles-Fieseler <charles.fieseler@imp.ac.at>
2020-12-17 09:51:40 +0100

    Utilities for creating DLC subproject

---
+++ DLC_for_WBFM/utils/preprocessing/DLC_utils.py
@@ -0,0 +1,26 @@
+import deeplabcut
+from DLC_for_WBFM.bin.configuration_definition import *
+
+##
+## Functions for building DLC projects using config files
+##
+
+def create_dlc_project_from_config(config, label='',copy_videos=True):
+    """
+    Creates a DLC subproject within a parent folder defined by config
+
+    Note: copy_videos is required on Windows
+    """
+
+    c = load_config(config)
+
+    # Force shorter name
+    dlc_opt = {'project':c.task_name[0] + label,
+               'experimenter':c.experimenter[0],
+               'videos':[c.datafiles.red_avi_fname],
+               'copy_videos':copy_videos,
+               'working_directory':c.get_dirname()}
+
+    dlc_config_file = deeplabcut.create_new_project(**dlc_opt)
+
+    return dlc_config_file
