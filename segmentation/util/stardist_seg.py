78b03379d455e6f6392063d7d69184e2ff16704e
Niklas_Khoss <niklas.khoss@imp.ac.at>
2021-02-08 12:15:58 +0100

    Stardist segmentation function. Input: volume, Output: 3D array of masks

---
+++ segmentation/util/stardist_seg.py
@@ -0,0 +1,52 @@
+from __future__ import print_function, unicode_literals, absolute_import, division
+import sys
+import os
+import numpy as np
+
+from glob import glob
+import tifffile as tiff
+from csbdeep.utils import Path, normalize
+from csbdeep.io import save_tiff_imagej_compatible
+
+from stardist import random_label_cmap, _draw_polygons, export_imagej_rois
+from stardist.models import StarDist2D
+
+def seg_with_stardist(vol_path):
+    # segments a volume (via path)
+    # vars set
+    # Stardist models: versatile showed best results
+
+    # model_list = [ '2D_paper_dsb2018', '2D_versatile_fluo', '2D_demo' ]
+    mdl = '2D_versatile_fluo'
+    axis_norm = (0, 1)
+    n_channel = 1
+
+    # logging/redirecting stdout
+    log_file = "./log_" + mdl + ".log"
+
+    with open(log_file, 'w') as log:
+        # load stardist model
+        model = StarDist2D.from_pretrained(mdl)
+
+        # open volume.tif
+        with tiff.TiffFile(vol_path) as vol:
+            # initialize output dimensions
+            z = len(vol.pages)
+            xy = vol.pages[1].shape
+            output_file = np.zeros((z, *xy))
+
+            # iterate over images to run stardist on single images
+            for idx, page in enumerate(vol.pages):
+                img = page.asarray()
+                print(idx, img.shape, file=log)
+
+                # normalizing images (stardist function)
+                img = normalize(img, 1, 99.8, axis=axis_norm)
+
+                # run the prediction
+                labels, details = model.predict_instances(img)
+
+                # save labels in 3D array for output
+                output_file[idx] = labels
+
+    return output_file
