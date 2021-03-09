b827f254b74aa9ff2002db98df23e1c0aaeda40a
Niklas_Khoss <niklas.khoss@imp.ac.at>
2021-03-09 17:48:30 +0100

    minor changes

--- segmentation/scripts/plot_accuracy.py
+++ segmentation/scripts/plot_accuracy.py
@@ -5,73 +5,75 @@ import numpy as np
 import pandas as pd
 from tabulate import tabulate
 
+plot = 0
 
-# sv_path = r'C:\Segmentation_working_area\accuracy'
-sv_path = r'C\Segmentation_working_area\results\accuracy_summary_results'
+sv_path = r'C\Segmentation_working_area\results\new_acc_metrics\leifer'
+filepath = r'C:\Segmentation_working_area\results\new_acc_metrics\leifer\leifer_all_accuracy_results_with_volumes_and_percentages.pickle'
 
-with open(r'C:\Segmentation_working_area\results\accuracy_summary_results\all_accuracy_results_with_areas_and_percentages.pickle', 'rb') as file:
-# with open(r'C:\Segmentation_working_area\accuracy\accuracy_results.pickle', 'rb') as file
+with open(filepath, 'rb') as file:
     results = pickle.load(file)
 
     keys = list(results.keys())
     values = list(results.values())
 
-    # Scatterplot false negatives
-    # b1 = plt.figure()
-    #
-    # for i in range(len(keys)):
-    #     plt.bar(keys[i], values[i]['fn'], color='b')
-    #
-    # plt.title('False negatives comparison')
-    # plt.ylabel('False negatives (abs)')
-    # plt.xticks(rotation=45)
-    # plt.tight_layout()
-    #
-    # plt.show()
-    #
-    # # False positives
-    # for i in range(len(keys)):
-    #     plt.bar(keys[i], values[i]['fp'], color='r')
-    #
-    # plt.title('False positives comparison')
-    # plt.ylabel('False positives (abs)')
-    # plt.xticks(rotation=45)
-    # plt.tight_layout()
-    #
-    # plt.show()
-    #
-    # # True positives
-    # for i in range(len(keys)):
-    #     plt.bar(keys[i], values[i]['tp'], color='k')
-    #
-    # plt.title('True positives comparison')
-    # plt.ylabel('True positives (abs)')
-    # plt.xticks(rotation=45)
-    # plt.tight_layout()
-    #
-    # plt.show()
-    #
-    # # Undersegmentation
-    # for i in range(len(keys)):
-    #     plt.bar(keys[i], values[i]['us'], color='m')
-    #
-    # plt.title('Undersegmentation comparison')
-    # plt.ylabel('Undersegmentations (abs)')
-    # plt.xticks(rotation=45)
-    # plt.tight_layout()
-    #
-    # plt.show()
-    #
-    # # Oversegmentation
-    # for i in range(len(keys)):
-    #     plt.bar(keys[i], values[i]['os'], color='g')
-    #
-    # plt.title('Oversegmentation comparison')
-    # plt.ylabel('Oversegmentations (abs)')
-    # plt.xticks(rotation=45)
-    # plt.tight_layout()
-    #
-    # plt.show()
+    if plot >= 1:
+
+        # Scatterplot false negatives
+        b1 = plt.figure()
+
+        for i in range(len(keys)):
+            plt.bar(keys[i], values[i]['fn'], color='b')
+
+        plt.title('False negatives comparison')
+        plt.ylabel('False negatives (abs)')
+        plt.xticks(rotation=45)
+        plt.tight_layout()
+
+        plt.show()
+
+        # False positives
+        for i in range(len(keys)):
+            plt.bar(keys[i], values[i]['fp'], color='r')
+
+        plt.title('False positives comparison')
+        plt.ylabel('False positives (abs)')
+        plt.xticks(rotation=45)
+        plt.tight_layout()
+
+        plt.show()
+
+        # True positives
+        for i in range(len(keys)):
+            plt.bar(keys[i], values[i]['tp'], color='k')
+
+        plt.title('True positives comparison')
+        plt.ylabel('True positives (abs)')
+        plt.xticks(rotation=45)
+        plt.tight_layout()
+
+        plt.show()
+
+        # Undersegmentation
+        for i in range(len(keys)):
+            plt.bar(keys[i], values[i]['us'], color='m')
+
+        plt.title('Undersegmentation comparison')
+        plt.ylabel('Undersegmentations (abs)')
+        plt.xticks(rotation=45)
+        plt.tight_layout()
+
+        plt.show()
+
+        # Oversegmentation
+        for i in range(len(keys)):
+            plt.bar(keys[i], values[i]['os'], color='g')
+
+        plt.title('Oversegmentation comparison')
+        plt.ylabel('Oversegmentations (abs)')
+        plt.xticks(rotation=45)
+        plt.tight_layout()
+
+        plt.show()
 
     # TODO create a table (pandas dataframe) with all results
     # list of lists for pandas table
@@ -85,13 +87,13 @@ with open(r'C:\Segmentation_working_area\results\accuracy_summary_results\all_ac
 
     df = pd.DataFrame.from_dict(results)
     df.index = ['False negative', 'False negative %', 'False Positives', 'False Positives %',
-                'True Positives', 'True Positives %', 'Over-segmentations', 'Under-segmentations', 'v1', 'v2']
+                'True Positives', 'True Positives %', 'Under-segmentations', 'Over-segmentations', 'v1', 'v2']
     pd.set_option('display.max_rows', None, 'display.max_columns', None)
 
     print(tabulate(df, headers='keys', tablefmt='psql'))
     print(f'Means of rows:\n{df.mean(axis=1)}')
 
-    df.to_csv(r'C:\Segmentation_working_area\results\accuracy_summary_results\bipartite_accuracy_summary.csv')
+    df.to_csv(r'C:\Segmentation_working_area\results\new_acc_metrics\normal_vol_bipartite_accuracy_summary.csv')
     # save the table
     # with open(os.path.join(sv_path, 'dataframe_accuracy_results.pickle')) as p_file:
     #     pickle.dump(df, p_file)
--- segmentation/scripts/testing_script.py
+++ segmentation/scripts/testing_script.py
@@ -11,16 +11,21 @@ sd_files = [os.path.join(sd_dir, f.name) for f in os.scandir(sd_dir) if f.is_fil
 cp_dir = r'C:\Segmentation_working_area\data\cellpose'
 cp_files = [os.path.join(cp_dir, f.name) for f in os.scandir(cp_dir) if f.is_file()]
 
-# all_algo_files = cp_files + sd_files
-all_algo_files = sd_files
+all_algo_files = cp_files + sd_files
+# all_algo_files = sd_files
 
 # load raw array
 raw_array = ol.create_3d_array_from_tiff(
     r'C:\Segmentation_working_area\data\raw_volume')
+if raw_array.shape[0] == 33:
+    raw_array = raw_array[1:]
+
 preprocessed_array = ol.create_3d_array_from_tiff(
     r'C:\Segmentation_working_area\data\raw_volume\preprocessed')
+if preprocessed_array.shape[0] == 33:
+    preprocessed_array = preprocessed_array[1:]
 
-print(raw_array.shape, preprocessed_array.shape)
+print('raw array shapes: ', raw_array.shape, preprocessed_array.shape)
 
 for file in all_algo_files:
     print(f'File: {file}')
@@ -30,13 +35,11 @@ for file in all_algo_files:
         print(f'{file} had 33 slices')
         algo_array = algo_array[1:]
 
-    print(f'But now only {algo_array.shape}')
-
     if 'pre' in file:
-        final_mask, final_lengths, final_brightnesses = ol.level2_overlap(preprocessed_array, algo_array)
+        final_mask, final_lengths, final_brightnesses, brightness_planes, df = ol.level2_overlap(preprocessed_array, algo_array)
         print(f'- preprocessed: {file}')
     else:
-        final_mask, final_lengths, final_brightnesses = ol.level2_overlap(raw_array, algo_array)
+        final_mask, final_lengths, final_brightnesses, brightness_planes, df = ol.level2_overlap(raw_array, algo_array)
 
     # first, create results folder if not existent
     results_path = os.path.join(r'C:\Segmentation_working_area\results\accuracy_summary_results',
@@ -49,9 +52,11 @@ for file in all_algo_files:
             final_mask, allow_pickle=True)
 
     with open(os.path.join(results_path, os.path.split(file)[1][:-4] + 'lengths_and_brightnesses.pickle'), 'wb') as pickle_out:
-        pickle.dump([final_lengths, final_brightnesses], pickle_out)
+        pickle.dump([final_lengths, final_brightnesses, brightness_planes], pickle_out)
 
     # save length histograms
-    ol.neuron_length_hist(final_lengths, results_path, 1)
+    h1, h2 = ol.neuron_length_hist(final_lengths, results_path, 1)
 
 print('Completely done!')
+
+
