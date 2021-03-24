64da4dec4f0a82cabf69554c61d6fa55b527042c
Charles-Fieseler <charles.fieseler@imp.ac.at>
2021-03-24 16:13:10 +0100

    Remove old duplicate function

--- DLC_for_WBFM/utils/training_data/DLC_XML_conversion_utils.py
+++ DLC_for_WBFM/utils/training_data/DLC_XML_conversion_utils.py
@@ -165,57 +165,57 @@ def find_xml_in_project(path_config_file):
 ## Synchronizing the config file
 ##
 
-def csv_annotations2config_names(path_config_file, num_dims=2, actually_write=True):
-    """
-    Automatically updates the config file with the proper number of neurons, and deletes any other default bodyparts.
-    Only affects the "bodyparts" field
-    """
-
-    # Get number of neurons from annotations
-    home = os.path.dirname(path_config_file)
-    # TODO: must have XML in the folder to find it
-    annotations_fname = get_annotations_converted_from_xml(path_config_file)
-    # TODO: hardcoded folder
-    # annotations_fname = os.path.join(home,'labeled-data', 'test_100frames.ome','CollectedData_Charlie.csv')
-    df = pd.read_csv(annotations_fname)
-    num_neurons = int(df.shape[1] / num_dims)
-    print(f"Adding body part annotations for {num_neurons} neurons")
-    print("(Note: postprocessing may filter these tracks and only display a subset)")
-
-    # Read in entire config file into a list
-    config_rows = []
-    with open(path_config_file) as config:
-        c_reader = csv.reader(config)#, delimiter=' ')
-        for row in c_reader:
-            config_rows.append(row)
-
-    ## Delete the current bodypart lines
-    delete_these_rows = False
-    config_rows_edit = config_rows.copy()
-    for row in config_rows:
-        if row == ['bodyparts:']:
-            delete_these_rows = True # Start deleting next row
-        elif row == ['start: 0']:
-            delete_these_rows = False # Do not delete this row, or others
-            break
-        elif delete_these_rows == True:
-            # Don't delete either of the two above, but only in between those rows
-            config_rows_edit.remove(row)
-
-    ## Add in the named neuron lines
-    # Using "list slicing" https://www.geeksforgeeks.org/python-insert-list-in-another-list/
-    new_names = [['- neuron{}'.format(i)] for i in range(num_neurons)]
-    insert_index = config_rows_edit.index(['start: 0'])
-    config_rows_edit[insert_index:insert_index] = new_names
-
-    ## Write the file again
-    if actually_write:
-        with open(path_config_file, 'w', newline='') as config:
-            c_writer = csv.writer(config)
-            for row in config_rows_edit:
-                c_writer.writerow(row)
-
-    print("Finished! Check the config.yaml file to make sure the bodyparts are properly written")
+# def csv_annotations2config_names(path_config_file, num_dims=2, actually_write=True):
+#     """
+#     Automatically updates the config file with the proper number of neurons, and deletes any other default bodyparts.
+#     Only affects the "bodyparts" field
+#     """
+#
+#     # Get number of neurons from annotations
+#     home = os.path.dirname(path_config_file)
+#     # TODO: must have XML in the folder to find it
+#     annotations_fname = get_annotations_converted_from_xml(path_config_file)
+#     # TODO: hardcoded folder
+#     # annotations_fname = os.path.join(home,'labeled-data', 'test_100frames.ome','CollectedData_Charlie.csv')
+#     df = pd.read_csv(annotations_fname)
+#     num_neurons = int(df.shape[1] / num_dims)
+#     print(f"Adding body part annotations for {num_neurons} neurons")
+#     print("(Note: postprocessing may filter these tracks and only display a subset)")
+#
+#     # Read in entire config file into a list
+#     config_rows = []
+#     with open(path_config_file) as config:
+#         c_reader = csv.reader(config)#, delimiter=' ')
+#         for row in c_reader:
+#             config_rows.append(row)
+#
+#     ## Delete the current bodypart lines
+#     delete_these_rows = False
+#     config_rows_edit = config_rows.copy()
+#     for row in config_rows:
+#         if row == ['bodyparts:']:
+#             delete_these_rows = True # Start deleting next row
+#         elif row == ['start: 0']:
+#             delete_these_rows = False # Do not delete this row, or others
+#             break
+#         elif delete_these_rows == True:
+#             # Don't delete either of the two above, but only in between those rows
+#             config_rows_edit.remove(row)
+#
+#     ## Add in the named neuron lines
+#     # Using "list slicing" https://www.geeksforgeeks.org/python-insert-list-in-another-list/
+#     new_names = [['- neuron{}'.format(i)] for i in range(num_neurons)]
+#     insert_index = config_rows_edit.index(['start: 0'])
+#     config_rows_edit[insert_index:insert_index] = new_names
+#
+#     ## Write the file again
+#     if actually_write:
+#         with open(path_config_file, 'w', newline='') as config:
+#             c_writer = csv.writer(config)
+#             for row in config_rows_edit:
+#                 c_writer.writerow(row)
+#
+#     print("Finished! Check the config.yaml file to make sure the bodyparts are properly written")
 
 
 def get_annotations_converted_from_xml(path_config_file):
