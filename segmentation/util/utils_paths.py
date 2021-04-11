e042617351c6b7dec3982df8c2ad7bfdf1f7a281
Charles-Fieseler <charles.fieseler@imp.ac.at>
2021-04-11 15:38:28 +0200

    makes a subfolder now

--- segmentation/util/utils_paths.py
+++ segmentation/util/utils_paths.py
@@ -1,5 +1,7 @@
 import os.path as osp
 from datetime import datetime
+from pathlib import Path
+
 
 def get_output_fnames(video_path, _config):
     if _config['output_params']['output_folder'] is None:
@@ -7,15 +9,20 @@ def get_output_fnames(video_path, _config):
     else:
         output_folder = _config['output_params']['output_folder']
 
-    suffix = datetime.now().strftime("%Y_%m_%d-%I_%M_%p")
+    # Make a subfolder
+    subfolder = datetime.now().strftime("%Y_%m_%d-%I_%M_%p")
+    subfolder = osp.join(output_folder, subfolder)
+    Path(subfolder).mkdir(parents=True, exist_ok=True)
+
+    # Make a suffix
     num_frames = _config['dataset_params']['num_frames']
 
     # Actual masks
-    fname = f'masks_{num_frames}_{suffix}.btf'
-    mask_fname = osp.join(output_folder, fname)
+    fname = f'masks_{num_frames}.btf'
+    mask_fname = osp.join(subfolder, fname)
 
     # Metadata
-    fname = f'metadata_{num_frames}_{suffix}.btf'
-    metadata_fname = osp.join(output_folder, fname)
+    fname = f'metadata_{num_frames}.btf'
+    metadata_fname = osp.join(subfolder, fname)
 
     return mask_fname, metadata_fname
