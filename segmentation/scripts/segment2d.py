46e0d5ed62206ac291c3735fe9f0cd73e920050e
Charles-Fieseler <charles.fieseler@imp.ac.at>
2021-04-11 15:26:38 +0200

    sacred-enabled refactoring of the main pipeline function

---
+++ segmentation/scripts/segment2d.py
@@ -0,0 +1,144 @@
+"""
+The top level functions for segmenting a full (WBFM) recording.
+"""
+
+from tqdm import tqdm
+import pickle
+import os
+import tifffile as tiff
+# preprocessing
+from DLC_for_WBFM.utils.video_and_data_conversion.import_video_as_array import get_single_volume
+from DLC_for_WBFM.utils.preprocessing.utils_tif import PreprocessingSettings
+from DLC_for_WBFM.utils.preprocessing.utils_tif import perform_preprocessing
+# segmentation
+from segmentation.util.utils_model import get_stardist_model
+from segmentation.util.utils_model import segment_with_stardist_2d
+from segmentation.util.utils_model import segment_with_stardist_3d
+# postproc
+from segmentation.util.utils_pipeline import perform_post_processing_2d
+from segmentation.util.utils_pipeline import perform_post_processing_3d
+# metadata
+from segmentation.util.utils_metadata import get_metadata_dictionary
+from segmentation.util.utils_paths import get_output_fnames
+# Experiment tracking
+from sacred import Experiment
+ex = Experiment()
+
+@ex.config
+def cfg(video_path, preprocessing_config):
+
+    verbose = 0
+    DEBUG = True
+
+    # Check paths
+    assert os.path.exists(video_path)
+
+    # Check preprocessing settings
+    assert os.path.exists(preprocessing_config)
+    preprocessing_settings = PreprocessingSettings.load_from_yaml(preprocessing_config)
+
+
+@ex.automain()
+def segment2d(_config):
+    """
+    Function to segment, stitch and curate a recording on a volume-to-volume basis.
+
+    Parameters
+    ----------
+    video_path : str
+        path to recording
+    start_volume : int
+        starting frame in absolute number starting from 0
+    num_frames : int
+        number of volumes to be analyzed
+    stardist_model_name : str
+        Name/alias of the StarDist model to be used for segmentation. I.e. 'lukas', 'charlie', 'versatile', 'Charlie100-3d'
+        see also: get_stardist_model()
+    preprocessing : yaml file for preprocessing settings
+        see also PreprocessingSettings
+    num_slices : int
+        number of total Z-slices per volume
+    to_remove_border : boolean
+        Flag for zeroing edge areas, because of edge effect artefacts in segmentations of prealigned volumes
+    flag_3d : Boolean
+        Flag for using Stardists' 3D segmentation
+    options : object
+        contains further necessary options for running
+    verbose : int
+        flag for print statements. Increasing by 1, increase depth by 1
+    DEBUG : bool
+        flag for a 10 second run; segments one volume and writes small versions of all files
+
+    Returns
+    -------
+    output_fname : str
+        path to the TIF-file of segmented masks
+    metadata : dict
+        dictionary containing metadata per volume and neuron. Each volume's dataframe has N rows, where
+        N = number of unique neurons in that slice and 3 columns = ('total_brightness', 'neuron_volume', 'centroids')
+        {volume_number : [dataframe('total_brightness', 'neuron_volume', 'centroids')]}
+
+        Use the column name to index directly into the dataframe to get all e.g. centroid values
+    """
+
+
+    # get stardist model object
+    stardist_model_name = _config['segmentation_params']['stardist_model_name']
+    sd_model = get_stardist_model(stardist_model_name, verbose=verbose-1)
+
+    # Initializing variables
+    start_volume, num_frames, num_slices = _config['dataset_params']
+    mask_fname, metadata_fname = get_output_fnames(video_path, _config)
+    metadata = dict()
+
+    for i in tqdm(list(range(start_volume, start_volume + num_frames))):
+        # use get single volume function from charlie
+        import_opt = {'which_vol': i, 'num_slices': num_slices, 'alpha': 1.0, 'dtype': 'uint16'}
+        volume = get_single_volume(video_path, **import_opt)
+
+        # preprocess
+        volume = perform_preprocessing(volume, preprocessing_settings)
+
+        # segment the volume using Stardist
+        if verbose >= 2:
+            print('--- Segmentation ---')
+        segmented_masks = segment_with_stardist_2d(volume, sd_model, verbose=verbose-1)
+
+        # process masks: remove large areas, stitch, split long neurons, remove short neurons
+        if verbose >= 2:
+            print('---- Post-processing ----')
+        opt = config['postprocessing_params']
+        final_masks = perform_post_processing_2d(segmented_masks,
+                                                 volume,
+                                                 **opt,
+                                                 verbose=verbose-1)
+
+        if verbose >= 2:
+            print('----- Saving to BIG-TIF -----')
+
+        # concatenate masks to tiff file and save
+        if i == start_volume:
+            tiff.imwrite(mask_fname,
+                         final_masks,
+                         append=False,
+                         bigtiff=True)
+        else:
+            tiff.imwrite(mask_fname,
+                         final_masks,
+                         append=True,
+                         bigtiff=True)
+
+        # metadata dictionary
+        # metadata_dict = {(Vol #, Neuron #) = [Total brightness, neuron volume, centroids]}
+        meta_df = get_metadata_dictionary(final_masks, volume)
+        metadata[i] = meta_df
+
+        if DEBUG:
+            break
+
+    # saving metadata
+    with open(metadata_fname, 'wb') as meta_save:
+        pickle.dump(metadata, meta_save)
+
+    if verbose >= 1:
+        print(f'Done with segmentation pipeline! Data saved at {mask_fname}')
