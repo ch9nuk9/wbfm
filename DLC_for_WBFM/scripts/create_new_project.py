abeeea32b3cd684fb81c746209276ab9cdeb1397
Charles-Fieseler <charles.fieseler@gmail.com>
2021-04-12 18:29:07 +0200

    Fix bugs; script runs now

--- DLC_for_WBFM/scripts/create_new_project.py
+++ DLC_for_WBFM/scripts/create_new_project.py
@@ -1,17 +1,18 @@
 
-from DLC_for_WBFM.utils.projects.utils_projects import build_project_structure
+from DLC_for_WBFM.utils.projects.utils_project import build_project_structure
 import os.path as osp
 
 import sacred
 from sacred import Experiment
-
+from sacred import SETTINGS
+SETTINGS.CONFIG.READ_ONLY_CONFIG=False
 
 ex = Experiment()
 ex.add_config('new_project_defaults/project_config.yaml')
 
 
 @ex.config
-def cfg(path, red_bigtiff_fname, green_bigtiff_fname):
+def cfg(red_bigtiff_fname, green_bigtiff_fname):
     # assert osp.exists(red_bigtiff_fname)
     # assert osp.exists(green_bigtiff_fname)
     pass
@@ -20,4 +21,4 @@ def cfg(path, red_bigtiff_fname, green_bigtiff_fname):
 def main(_config, _run):
     sacred.commands.print_config(_run)
 
-    build_project_structure(_config, path)
+    build_project_structure(_config)
--- DLC_for_WBFM/utils/projects/utils_project.py
+++ DLC_for_WBFM/utils/projects/utils_project.py
@@ -5,84 +5,78 @@ from shutil import copyfile
 from ruamel.yaml import YAML
 
 
-def build_project_structure(_config, path):
+def build_project_structure(_config):
 
-    dir_name = get_project_name(_config)
+    # parent_folder = Path(_config['project_dir']).resolve()
+    parent_folder = _config['project_dir']
+    rel_dir_name = get_project_name()
 
     # Build blank folder structure
-    Path(dir_name).mkdir(parents=True, exist_ok=False)
+    abs_dir_name = osp.join(parent_folder, rel_dir_name)
+    print(f"Building new project at: {abs_dir_name}")
+    Path(abs_dir_name).mkdir(parents=True)
 
     subfolders = list(_config['subfolder_configs'])
     for sub in subfolders:
-        Path(osp.join(dir_name, sub)).mkdir(parents=True)
+        Path(osp.join(abs_dir_name, sub)).mkdir(parents=True)
 
     # Copy config files and readmes over
     tmp = list(_config['subfolder_configs'].items())
     for subfolder, src in tmp:
         # Config file
-        dest = osp.basename(src)
-        dest = osp.join(dir_name, subfolder, dest)
+        dest_fname = osp.basename(src)
+        dest = osp.join(abs_dir_name, subfolder, dest_fname)
         copyfile(src, dest)
         # Also update in the config file itself
-        _config['subfolder_configs'][subfolder] = dest
+        _config['subfolder_configs'][subfolder] = osp.join(subfolder, dest_fname)
         # README
-        dest = 'README.md'
-        dest = osp.join(dir_name, subfolder, dest)
+        dest_fname = 'README.md'
+        dest = osp.join(abs_dir_name, subfolder, dest_fname)
         src_readme = osp.join(osp.dirname(src), 'README.md')
         copyfile(src_readme, dest)
 
     # Also copy those outside the subfolders
     src = _config['preprocessing_config']
-    dest = osp.join(dir_name, 'preprocessing_config.yaml')
+    dest_fname = 'preprocessing_config.yaml'
+    dest = osp.join(abs_dir_name, dest_fname)
     copyfile(src, dest)
-    _config['preprocessing_config'] = dest
+    _config['preprocessing_config'] = dest_fname
 
     # the current project_config file itself
     # TODO: hardcoded
-    src = osp.join('new_project_defaults', 'project_config.yaml')
-    project_fname = osp.join(dir_name, 'project_config.yaml')
+    dest_fname = 'project_config.yaml'
+    src = osp.join('new_project_defaults', dest_fname)
+    project_fname = osp.join(abs_dir_name, dest_fname)
     copyfile(src, project_fname)
-    # _config['project_path'] = dest
 
     # Finally, update the copied project config with the new dest folder
+    project_fname = str(Path(project_fname).resolve())
     edit_config(project_fname, _config)
 
 
-def get_project_name(video_path, _config):
-    if _config['output_params']['output_folder'] is None:
-        output_folder = osp.split(video_path)[0]
-    else:
-        output_folder = _config['output_params']['output_folder']
-
-    # Make a subfolder
-    subfolder = datetime.now().strftime("%Y_%m_%d-%I_%M_%p")
-    subfolder = osp.join(output_folder, subfolder)
-    Path(subfolder).mkdir(parents=True, exist_ok=True)
-
-    # Make a suffix
-    num_frames = _config['dataset_params']['num_frames']
-
-    # Actual masks
-    fname = f'masks_{num_frames}.btf'
-    mask_fname = osp.join(subfolder, fname)
-
-    # Metadata
-    fname = f'metadata_{num_frames}.pickle'
-    metadata_fname = osp.join(subfolder, fname)
-
-    return mask_fname, metadata_fname
+def get_project_name():
+    # Use current time
+    project_name = datetime.now().strftime("proj_%Y_%m_%d-%I_%M")
+    return project_name
 
 
-def edit_config(config_fname, edits):
+def edit_config(config_fname, edits, DEBUG=False):
     """Generic overwriting, based on DLC"""
 
-    yaml = YAML()
-    cfg = yaml.load(config_fname)
+    assert osp.exists(config_fname), f"{config_fname} not found!"
 
-    for k, v in edits:
+    if DEBUG:
+        print(f"Editing config file at: {config_fname}")
+    with open(config_fname, 'r') as f:
+        cfg = YAML().load(f)
+    if DEBUG:
+        print(f"Initial config: {cfg} {type(cfg)}")
+        print(f"Edits: {edits}")
+
+    for k, v in edits.items():
         cfg[k] = v
 
     with open(config_fname, "w") as f:
-        yaml.dump(cfg, f)
+        YAML().dump(cfg, f)
 
     return cfg
