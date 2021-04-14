c76691807ff996e253e542d04b9d3eeeaf4ef4e8
Charles-Fieseler <charles.fieseler@imp.ac.at>
2021-04-14 09:56:33 +0200

    Working version of loading subreferenced config files

--- DLC_for_WBFM/scripts/segment_video.py
+++ DLC_for_WBFM/scripts/segment_video.py
@@ -6,8 +6,9 @@ To be used with Niklas' Stardist-based segmentation package
 
 
 import os
+from pathlib import Path
 # main function
-from DLC_for_WBFM.utils.projects.utils_project import load_config
+from DLC_for_WBFM.utils.projects.utils_project import load_config, edit_config, synchronize_segment_config
 from segmentation.util.utils_pipeline import segment_video_using_config_2d
 # Experiment tracking
 import sacred
@@ -23,13 +24,22 @@ from sacred.observers import FileStorageObserver
 ex = Experiment()
 # TODO: script must be run from project folder for now
 # ex.add_config('project_config.yaml')
+ex.add_config(project_path=None)
+
 
 @ex.config
 def cfg(project_path):
     # Manually load yaml files
-    project_cfg = load_config('project_config.yaml')
-    segment_fname = project_cfg['subfolder_configs']['segmentation']
+    project_cfg = load_config(project_path)
+    segment_fname = Path(project_cfg['subfolder_configs']['segmentation'])
+    # print(project_path, segment_fname)
+    project_dir = Path(project_path).parent
+    segment_fname = Path(project_dir).joinpath(segment_fname)
     segment_cfg = dict(load_config(segment_fname))
+
+    # Sync filename in segmentation config from project_cfg
+    segment_cfg = synchronize_segment_config(project_path, segment_cfg)
+    edit_config(segment_fname, segment_cfg)
 
 
 @ex.automain
