bc9edbf99a762a1343e842c7ddc0b1495498014b
Charles-Fieseler <charles.fieseler@imp.ac.at>
2021-04-14 14:03:56 +0200

    Fix import bug

--- segmentation/util/utils_paths.py
+++ segmentation/util/utils_paths.py
@@ -6,23 +6,22 @@ from pathlib import Path
 def get_output_fnames(video_path, _config):
     if _config['output_params']['output_folder'] is None:
         output_folder = osp.split(video_path)[0]
+        # Make a subfolder
+        subfolder = datetime.now().strftime("%Y_%m_%d-%I_%M_%p")
+        output_folder = osp.join(output_folder, subfolder)
+        Path(output_folder).mkdir(parents=True, exist_ok=True)
     else:
         output_folder = _config['output_params']['output_folder']
-
-    # Make a subfolder
-    subfolder = datetime.now().strftime("%Y_%m_%d-%I_%M_%p")
-    subfolder = osp.join(output_folder, subfolder)
-    Path(subfolder).mkdir(parents=True, exist_ok=True)
 
     # Make a suffix
     num_frames = _config['dataset_params']['num_frames']
 
     # Actual masks
     fname = f'masks_{num_frames}.btf'
-    mask_fname = osp.join(subfolder, fname)
+    mask_fname = osp.join(output_folder, fname)
 
     # Metadata
     fname = f'metadata_{num_frames}.pickle'
-    metadata_fname = osp.join(subfolder, fname)
+    metadata_fname = osp.join(output_folder, fname)
 
     return mask_fname, metadata_fname
