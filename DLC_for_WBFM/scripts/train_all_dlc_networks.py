5ab95ccf577b88a79f90e922aba9e4d848ec3406
Charles-Fieseler <charles.fieseler@imp.ac.at>
2021-04-17 16:45:20 +0200

    New script for training all networks

---
+++ DLC_for_WBFM/scripts/train_all_dlc_networks.py
@@ -0,0 +1,47 @@
+"""
+The top level function for initializing a stack of DLC projects
+"""
+
+import os
+from pathlib import Path
+# main function
+from DLC_for_WBFM.utils.projects.utils_project import load_config, edit_config, safe_cd
+from DLC_for_WBFM.utils.preprocessing.DLC_utils import create_dlc_training_from_tracklets
+# Experiment tracking
+import sacred
+from sacred import Experiment
+from sacred import SETTINGS
+SETTINGS.CONFIG.READ_ONLY_CONFIG=False
+
+# Initialize sacred experiment
+ex = Experiment()
+# Add single variable so that the cfg() function works
+ex.add_config(project_path=None, DEBUG=False)
+
+
+@ex.config
+def cfg(project_path):
+    # Manually load yaml files
+    project_cfg = load_config(project_path)
+    project_dir = Path(project_path).parent
+
+    with safe_cd(project_dir):
+        tracking_fname = Path(project_cfg['subfolder_configs']['tracking'])
+        tracking_cfg = dict(load_config(tracking_fname))
+
+
+@ex.automain
+def initialize_dlc_stack(_config, _run):
+    sacred.commands.print_config(_run)
+
+    this_config = _config['tracking_cfg'].copy()
+    this_config['dataset_params'] = _config['project_cfg']['dataset_params'].copy()
+
+    opt = {}
+    opt['scorer'] = _config['project_cfg']['experimenter']
+    opt['task_name'] = _config['project_cfg']['experimenter']
+    opt['verbose'] = _config['project_cfg']['other']['verbose']
+    opt['DEBUG'] = _config['DEBUG']
+
+    with safe_cd(_config['project_dir']):
+        train_all_dlc_from_config(this_config, **opt)
