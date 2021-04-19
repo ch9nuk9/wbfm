e1f2905b52e5c907e05ff8cd532108615acea88c
Charles-Fieseler <charles.fieseler@imp.ac.at>
2021-04-19 14:53:16 +0200

    Remove very old commented-out method

--- DLC_for_WBFM/utils/preprocessing/convert_matlab_annotations_to_DLC.py
+++ DLC_for_WBFM/utils/preprocessing/convert_matlab_annotations_to_DLC.py
@@ -116,62 +116,23 @@ def wb_tracker2config_names(path_config_file):
         #c_writer.writerow(row)
      #   print(row[:])
 
-def csv_annotations2config_names(path_config_file, annotations_fname=None, num_dims=2):
+def csv_annotations2config_names(path_config_file,
+                                 annotations_fname=None,
+                                 num_dims=2):
     """
     Automatically updates the config file with the proper number of neurons, and deletes any other default bodyparts.
     Only affects the "bodyparts" field
     """
 
     # Get number of neurons from annotations
-    home = os.path.dirname(path_config_file)
-    # TODO: hardcoded folder
-    # if annotations_fname is None:
-    #     annotations_fname = os.path.join(home,'labeled-data', 'test_100frames.ome','CollectedData_Charlie.csv')
-    # df = pd.read_csv(annotations_fname)
     df = pd.read_hdf(annotations_fname)
     num_neurons = int(df.shape[1] / num_dims)
     print("Adding body part annotations for {} neurons".format(num_neurons))
 
     # Use DLC function to edit the file
-    cfg = auxiliaryfunctions.read_config(path_config_file)
     names = list(df.columns.get_level_values(1))
     # Remove repetitions
-    # cfg["bodyparts"] = [names[i] for i in range(0,len(names),num_dims)]
-    updates = {"bodyparts":[names[i] for i in range(0,len(names),num_dims)]}
-    # auxiliaryfunctions.write_config(path_config_file, cfg)
+    updates = {"bodyparts": [names[i] for i in range(0, len(names), num_dims)]}
     auxiliaryfunctions.edit_config(path_config_file, updates)
-
-    # Read in entire config file into a list
-    # config_rows = []
-    # with open(path_config_file) as config:
-    #     c_reader = csv.reader(config)#, delimiter=' ')
-    #     for row in c_reader:
-    #         config_rows.append(row)
-
-    ## Delete the current bodypart lines
-    # delete_these_rows = False
-    # config_rows_edit = config_rows.copy()
-    # for row in config_rows:
-    #     if row == ['bodyparts:']:
-    #         delete_these_rows = True # Start deleting next row
-    #     elif row == ['start: 0']:
-    #         delete_these_rows = False # Do not delete this row, or others
-    #         break
-    #     elif delete_these_rows == True:
-    #         # Don't delete either of the two above, but only in between those rows
-    #         config_rows_edit.remove(row)
-
-    ## Add in the named neuron lines
-    # Using "list slicing" https://www.geeksforgeeks.org/python-insert-list-in-another-list/
-    # new_names = [['- neuron{}'.format(i)] for i in range(num_neurons)]
-    # insert_index = config_rows_edit.index(['start: 0'])
-    # config_rows_edit[insert_index:insert_index] = new_names
-
-    ## Write the file again
-    # if True:
-    #     with open(path_config_file, 'w', newline='') as config:
-    #         c_writer = csv.writer(config)
-    #         for row in config_rows_edit:
-    #             c_writer.writerow(row)
 
     print("Finished! Check the config.yaml file to make sure the bodyparts are properly written")
