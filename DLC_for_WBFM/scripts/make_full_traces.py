9114d7b59c3fd13931babf32148ba0e846312828
Charles-Fieseler <charles.fieseler@gmail.com>
2021-04-22 09:41:05 +0200

    New script for getting traces

---
+++ DLC_for_WBFM/scripts/make_full_traces.py
@@ -0,0 +1,49 @@
+"""
+The top level function for getting final traces from 3d tracks and neuron masks
+"""
+
+from pathlib import Path
+# main function
+from DLC_for_WBFM.utils.projects.utils_project import load_config, safe_cd
+from DLC_for_WBFM.utils.pipeline.dlc_pipeline import get_traces_from_3d_tracks
+# Experiment tracking
+import sacred
+from sacred import Experiment
+
+# Initialize sacred experiment
+ex = Experiment()
+# Add single variable so that the cfg() function works
+ex.add_config(project_path=None, DEBUG=False)
+
+
+@ex.config
+def cfg(project_path):
+    # Manually load yaml files
+    project_cfg = load_config(project_path)
+    project_dir = Path(project_path).parent
+
+    with safe_cd(project_dir):
+        trace_fname = Path(project_cfg['subfolder_configs']['traces'])
+        trace_cfg = dict(load_config(trace_fname))
+        track_fname = Path(project_cfg['subfolder_configs']['tracking'])
+        track_cfg = dict(load_config(track_fname))
+        seg_fname = Path(project_cfg['subfolder_configs']['segmentation'])
+        seg_cfg = dict(load_config(track_fname))
+
+
+@ex.automain
+def make_full_tracks(_config, _run):
+    sacred.commands.print_config(_run)
+
+    DEBUG = _config['DEBUG']
+    seg_cfg = _config['seg_cfg'].copy()
+    track_cfg = _config['track_cfg'].copy()
+    trace_cfg = _config['trace_cfg'].copy()
+    dataset_params = _config['project_cfg']['dataset_params'].copy()
+
+    with safe_cd(_config['project_dir']):
+        get_traces_from_3d_tracks(seg_cfg,
+                                  track_cfg,
+                                  trace_cfg,
+                                  dataset_params,
+                                  DEBUG=DEBUG)
