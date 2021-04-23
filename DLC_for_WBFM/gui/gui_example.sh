34c88ff7af5b6e39a1b4619f3f4a49c109327bc2
Charles-Fieseler <charles.fieseler@imp.ac.at>
2021-04-23 11:17:25 +0200

    Example script for running my gui

---
+++ DLC_for_WBFM/gui/gui_example.sh
@@ -0,0 +1,5 @@
+#!C:\Users\charles.fieseler\AppData\Local\Programs\Git\usr\bin\bash.exe
+PROJECT="C:/Users/charles.fieseler/dlc/Charlie-test-2021_04_14/project_config.yaml"
+COMMAND="gui/gui.py"
+
+python $COMMAND $PROJECT
