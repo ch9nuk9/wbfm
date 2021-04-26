046bd9a9f034f2f00331157dcaf698e320b68e1d
Charles-Fieseler <charles.fieseler@imp.ac.at>
2021-04-26 09:01:55 +0200

    Example script for gfp gui

---
+++ DLC_for_WBFM/gui/gui_gfp.sh
@@ -0,0 +1,5 @@
+#!C:\Users\charles.fieseler\AppData\Local\Programs\Git\usr\bin\bash.exe
+PROJECT="Y:/shared_projects/wbfm/dlc_stacks/Charlie-test-2021_04_21/project_config.yaml"
+COMMAND="gui/gui.py"
+
+python $COMMAND $PROJECT
