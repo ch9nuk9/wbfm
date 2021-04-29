087827d7ce68db5cf11914a56add401166477607
Charles-Fieseler <charles.fieseler@imp.ac.at>
2021-04-29 15:41:11 +0200

    Refactored main loop to subfunctions; prep for parallelization

--- segmentation/util/utils_pipeline.py
+++ segmentation/util/utils_pipeline.py
@@ -2,8 +2,6 @@ import segmentation.util.utils_postprocessing as post
 import numpy as np
 from tqdm import tqdm
 import pickle
-import os
-import tifffile as tiff
 # preprocessing
 from DLC_for_WBFM.utils.video_and_data_conversion.import_video_as_array import get_single_volume
 from DLC_for_WBFM.utils.preprocessing.utils_tif import PreprocessingSettings
@@ -13,6 +11,9 @@ from DLC_for_WBFM.utils.projects.utils_project import edit_config
 from segmentation.util.utils_metadata import get_metadata_dictionary
 from segmentation.util.utils_paths import get_output_fnames
 import zarr
+from segmentation.util.utils_model import segment_with_stardist_2d
+from segmentation.util.utils_model import get_stardist_model
+import concurrent.futures
 
 
 def segment_video_using_config_2d(_config):
@@ -25,13 +26,13 @@ def segment_video_using_config_2d(_config):
     # Initializing variables
     start_volume = _config['dataset_params']['start_volume']
     num_frames = _config['dataset_params']['num_frames']
+    if _config['DEBUG']:
+        num_frames = 1
     num_slices = _config['dataset_params']['num_slices']
     frame_list = list(range(start_volume, start_volume + num_frames))
     video_path = _config['video_path']
+    opt_postprocessing = _config['postprocessing_params']
     mask_fname, metadata_fname = get_output_fnames(video_path, _config)
-    # Initialize zarr output file (format: TZXY)
-    # vol = get_single_volume(video_path, 0, num_slices)
-    # _, x_sz, y_sz = vol.shape
     # Save settings
     _config['output']['masks'] = mask_fname
     _config['output']['metadata'] = metadata_fname
@@ -41,73 +42,35 @@ def segment_video_using_config_2d(_config):
     preprocessing_settings = PreprocessingSettings.load_from_yaml(
         _config['preprocessing_config']
     )
-    if verbose >= 1:
-        print("Loaded preprocessing_settings:")
-        print(preprocessing_settings)
 
     # get stardist model object
-    from segmentation.util.utils_model import get_stardist_model
-    from segmentation.util.utils_model import segment_with_stardist_2d
     stardist_model_name = _config['segmentation_params']['stardist_model_name']
     sd_model = get_stardist_model(stardist_model_name, verbose=verbose-1)
 
-    if verbose >= 1:
-        print(f"Starting loop over {num_frames} frames")
-    for i in tqdm(frame_list):
-        # use get single volume function from charlie
-        import_opt = {'which_vol': i, 'num_slices': num_slices, 'alpha': 1.0, 'dtype': 'uint16'}
-        volume = get_single_volume(video_path, **import_opt)
+    # Do first loop to initialize the zarr data
+    i = 0
+    i_volume = frame_list[i]
+    final_masks, volume = segment_single_volume(i_volume, num_slices, opt_postprocessing, preprocessing_settings,
+                                                sd_model,
+                                                verbose, video_path)
+    _, x_sz, y_sz = final_masks.shape
+    sz = (num_frames, num_slices, x_sz, y_sz)
+    chunks = (1, num_slices, x_sz, y_sz)
+    masks_zarr = zarr.open(mask_fname, mode='w-',
+                           shape=sz, chunks=chunks, dtype=np.uint16)
+    save_masks_and_metadata(final_masks, i, i_volume, masks_zarr, metadata, volume)
 
-        if _config['DEBUG']:
-            break
+    # Full loop
+    # with concurrent.futures.ThreadPoolExecutor() as executor:
+    #     metadata = executor.map(func, frame_list)
+    for i, i_volume in tqdm(enumerate(frame_list[1:])):
+        final_masks, volume = segment_single_volume(i_volume, num_slices, opt_postprocessing, preprocessing_settings,
+                                                    sd_model,
+                                                    verbose, video_path)
 
-        # preprocess
-        if verbose >= 2:
-            print(f'--- Volume {i}/{num_frames} ---')
-        volume = perform_preprocessing(volume, preprocessing_settings)
+        save_masks_and_metadata(final_masks, i+1, i_volume, masks_zarr, metadata, volume)
 
-        # segment the volume using Stardist
-        if verbose >= 2:
-            print('--- Segmentation ---')
-        segmented_masks = segment_with_stardist_2d(volume, sd_model, verbose=verbose-1)
-
-        # process masks: remove large areas, stitch, split long neurons, remove short neurons
-        if verbose >= 2:
-            print('---- Post-processing ----')
-        opt = _config['postprocessing_params']
-        final_masks = perform_post_processing_2d(segmented_masks,
-                                                 volume,
-                                                 **opt,
-                                                 verbose=verbose-1)
-
-        if verbose >= 2:
-            print(f"Found {len(np.unique(final_masks))} masks")
-            print('----- Saving to BIG-TIF -----')
-
-        # Add masks to zarr file; automatically saves
-        if i == start_volume:
-            _, x_sz, y_sz = final_masks.shape
-            sz = (num_frames, num_slices, x_sz, y_sz)
-            chunks = (1, num_slices, x_sz, y_sz)
-            masks_zarr = zarr.open(mask_fname, mode='w-',
-                                   shape=sz, chunks=chunks, dtype=np.uint16)
-        masks_zarr[i, :, :, :] = final_masks
-        # if i == start_volume:
-        #     tiff.imwrite(mask_fname,
-        #                  final_masks,
-        #                  append=False,
-        #                  bigtiff=True)
-        # else:
-        #     tiff.imwrite(mask_fname,
-        #                  final_masks,
-        #                  append=True,
-        #                  bigtiff=True)
-
-        # metadata dictionary
-        meta_df = get_metadata_dictionary(final_masks, volume)
-        metadata[i] = meta_df
-
-    # saving metadata
+    # saving metadata and settings
     with open(metadata_fname, 'wb') as meta_save:
         pickle.dump(metadata, meta_save)
 
@@ -115,8 +78,29 @@ def segment_video_using_config_2d(_config):
         edit_config(_config['self_path'], _config)
 
     if verbose >= 1:
-        print(f'Done with segmentation pipeline! Data saved at {mask_fname}')
+        print(f'Done with segmentation pipeline! Mask data saved at {mask_fname}')
 
+
+def save_masks_and_metadata(final_masks, i, i_volume, masks_zarr, metadata, volume):
+    # Add masks to zarr file; automatically saves
+    masks_zarr[i, :, :, :] = final_masks
+    # metadata dictionary
+    meta_df = get_metadata_dictionary(final_masks, volume)
+    metadata[i_volume] = meta_df
+
+
+def segment_single_volume(i, num_slices, opt_postprocessing, preprocessing_settings, sd_model, verbose, video_path):
+    # use get single volume function from charlie
+    import_opt = {'which_vol': i, 'num_slices': num_slices, 'alpha': 1.0, 'dtype': 'uint16'}
+    volume = get_single_volume(video_path, **import_opt)
+    volume = perform_preprocessing(volume, preprocessing_settings)
+    segmented_masks = segment_with_stardist_2d(volume, sd_model, verbose=verbose - 1)
+    # process masks: remove large areas, stitch, split long neurons, remove short neurons
+    final_masks = perform_post_processing_2d(segmented_masks,
+                                             volume,
+                                             **opt_postprocessing,
+                                             verbose=verbose - 1)
+    return final_masks, volume
 
 
 def perform_post_processing_2d(mask_array, img_volume, border_width_to_remove, to_remove_border=True,
