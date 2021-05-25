589e81f437f2ace5fc6a6330150b144825ffeaea
Charles-Fieseler <charles.fieseler@imp.ac.at>
2021-05-25 11:42:46 +0200

    Example project bash script now expects an experimenter name. Also add seperate version for linux and windows

--- DLC_for_WBFM/scripts/0-create_new_project-EXAMPLE.sh
+++
@@ -1,7 +0,0 @@
-#!bash
-
-RED_PATH="D:\More-stabilized-wbfm\test2020-10-22_16-15-20_test4-channel-0-pco_camera1\test2020-10-22_16-15-20_test4-channel-0-pco_camera1bigtiff.btf"
-GREEN_PATH="D:\More-stabilized-wbfm\test2020-10-22_16-15-20_test4-channel-1-pco_camera2\test2020-10-22_16-15-20_test4-channel-1-pco_camera2bigtiff.btf"
-COMMAND="scripts/create_new_project.py"
-
-python $COMMAND with project_dir=./scratch red_bigtiff_fname=$RED_PATH green_bigtiff_fname=$GREEN_PATH
---
+++ DLC_for_WBFM/scripts/0-create_new_project-windows-EXAMPLE.sh
@@ -0,0 +1,18 @@
+#!bash
+
+# Get the experimenter name from the command line
+if [[ "$#" -eq 1 ]]; then
+  EXPERIMENTER=${1%/}
+else
+  echo "Please rerun and pass the experimenter name"
+  exit 1
+fi
+
+# Hardcoded example locations
+PROJECT_DIR="Y:/shared_projects/wbfm/tutorial"
+RED_PATH="Y:/shared_projects/wbfm/dat/immobilized_with_WBFM_hardware/ZIM2051_trial_21_HEAD_mcherry_FULL_bigtiff.btf"
+GREEN_PATH="Y:/shared_projects/wbfm/dat/immobilized_with_WBFM_hardware/ZIM2051_trial_21_HEAD_gcamp_FULL_bigtiff.btf"
+
+# Actually run
+COMMAND="scripts/create_new_project.py"
+python $COMMAND with project_dir=$PROJECT_DIR red_bigtiff_fname=$RED_PATH green_bigtiff_fname=$GREEN_PATH experimenter=EXPERIMENTER
