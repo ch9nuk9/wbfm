50619f11e1c8175a4b3a05731197cdff4ef662c4
Charles-Fieseler <charles.fieseler@gmail.com>
2021-05-25 08:25:05 +0200

    Segmentation command line function now uses 'segmentation_type' field in the parameter file, and can be 2d or 3d

--- DLC_for_WBFM/scripts/1-segment_video.py
+++ DLC_for_WBFM/scripts/1-segment_video.py
@@ -9,7 +9,7 @@ import os
 from pathlib import Path
 # main function
 from DLC_for_WBFM.utils.projects.utils_project import load_config, edit_config, synchronize_segment_config, safe_cd
-from segmentation.util.utils_pipeline import segment_video_using_config_2d
+from segmentation.util.utils_pipeline import segment_video_using_config_2d, segment_video_using_config_3d
 # Experiment tracking
 import sacred
 from sacred import Experiment
@@ -48,4 +48,10 @@ def segment2d(_config, _run):
     this_config['dataset_params'] = _config['project_cfg']['dataset_params'].copy()
 
     with safe_cd(_config['project_dir']):
-        segment_video_using_config_2d(this_config)
+        mode = this_config['segmentation_type']
+        if mode == "3d":
+            segment_video_using_config_3d(this_config)
+        elif mode == "2d":
+            segment_video_using_config_2d(this_config)
+        else:
+            raise ValueError(f"Unknown segmentation_type; expected '2d' or '3d' instead of {mode}")
