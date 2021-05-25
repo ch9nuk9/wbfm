a0d35f9453b5ffc1c97f34afe80a1f54da5b4c43
Charles-Fieseler <charles.fieseler@imp.ac.at>
2021-05-25 11:47:26 +0200

    Fix typo

--- DLC_for_WBFM/scripts/0-create_new_project-linux-EXAMPLE.sh
+++ DLC_for_WBFM/scripts/0-create_new_project-linux-EXAMPLE.sh
@@ -14,5 +14,5 @@ RED_PATH="/groups/zimmer/shared_projects/wbfm/dat/immobilized_with_WBFM_hardware
 GREEN_PATH="/groups/zimmer/shared_projects/wbfm/dat/immobilized_with_WBFM_hardware/ZIM2051_trial_21_HEAD_gcamp_FULL_bigtiff.btf"
 
 # Actually run
-COMMAND="scripts/create_new_project.py"
+COMMAND="scripts/0-create_new_project.py"
 python $COMMAND with project_dir=$PROJECT_DIR red_bigtiff_fname=$RED_PATH green_bigtiff_fname=$GREEN_PATH experimenter=EXPERIMENTER
