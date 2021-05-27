9372269415f4dd00841f54b7084429ed3e4a90b0
Charles-Fieseler <charles.fieseler@gmail.com>
2021-05-27 11:48:52 +0200

    Finish draft of reindexing function; only works for final matches as produced in the traces step (4)

--- DLC_for_WBFM/utils/visualization/utils_segmentation.py
+++ DLC_for_WBFM/utils/visualization/utils_segmentation.py
@@ -1,3 +1,5 @@
+import os
+
 from DLC_for_WBFM.utils.projects.utils_project import load_config
 import pandas as pd
 import numpy as np
@@ -15,9 +17,26 @@ def reindex_segmentation(project_path):
     seg_fname = seg_cfg['output']['masks']
     seg_masks = zarr.open(seg_fname)
 
-    # Get tracking (dataframe) with neuron names
-    track_cfg = load_config(cfg['subfolder_configs']['tracking'])
-    seg_fname = seg_cfg['output']['masks']
+    out_fname = os.path.join("4-trackes", "reindexed_masks.zarr")
+    new_masks = zarr.zeros_like(seg_masks, path=out_fname)
 
+    # Get tracking (dataframe) with neuron names
+    trace_cfg = load_config(cfg['subfolder_configs']['traces'])
+    matches_fname = trace_cfg['traces']['all_matches']
+    all_matches = pd.read_hdf(matches_fname)
+    # Format: dict with i_volume -> Nx3 array of [local_ind, global_ind, confidence] triplets
 
     # Convert dataframe to lookup tables, per volume
+    # Note: if not all neurons are in the dataframe, then they are set to 0
+    all_lut = {}
+    for i_volume, match in all_matches.items():
+        lut = np.zeros_like(match[:, 0])
+        lut[match[:, 0]] = match[:, 1]  # Raw indices of the lut should match the local index
+        all_lut[i_volume] = lut
+
+    # Apply lookup tables to each volume
+    # Also see link for ways to speed this up:
+    # https://stackoverflow.com/questions/14448763/is-there-a-convenient-way-to-apply-a-lookup-table-to-a-large-array-in-numpy
+    for i_volume, lut in all_lut.items():
+        new_masks[i_volume, ...] = lut[seg_masks[i_volume, ...]]
+
