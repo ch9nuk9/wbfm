03edade03f2f7e0616db214c8adc86ea85d18003
Charles-Fieseler <charles.fieseler@gmail.com>
2021-05-28 11:58:48 +0200

    New convinience function for writing data locally

---
+++ DLC_for_WBFM/scripts/visualization/0+copy_data_locally.py
@@ -0,0 +1,18 @@
+"""
+"""
+
+# main function
+from DLC_for_WBFM.utils.projects.utils_data_subsets import write_data_subset_from_config
+# Experiment tracking
+import sacred
+from sacred import Experiment
+
+# Initialize sacred experiment
+ex = Experiment()
+ex.add_config(project_path=None, out_fname=None)
+
+@ex.automain
+def segment2d(_config, _run):
+    sacred.commands.print_config(_run)
+
+    write_data_subset_from_config(_config['project_path'], _config['out_fname'])
