427dc4ff0d4a2a8c542a1bf3a218b55dbb755227
Charles-Fieseler <charles.fieseler@imp.ac.at>
2021-06-09 14:22:06 +0200

    New function for recalculating the metadata after the initial segmentation

---
+++ DLC_for_WBFM/scripts/visualization/1+rebuild_segmentation_metadata.py
@@ -0,0 +1,34 @@
+
+
+# main function
+from DLC_for_WBFM.utils.projects.utils_project import load_config
+from segmentation.util.utils_pipeline import recalculate_metadata_from_config
+# Experiment tracking
+import sacred
+from sacred import Experiment
+from pathlib import Path
+
+# Initialize sacred experiment
+ex = Experiment()
+ex.add_config(project_path=None, out_fname=None)
+
+
+@ex.config
+def cfg(project_path):
+    # Manually load yaml files
+    project_cfg = load_config(project_path)
+    segment_fname = Path(project_cfg['subfolder_configs']['segmentation'])
+    project_dir = Path(project_path).parent
+    segment_fname = Path(project_dir).joinpath(segment_fname)
+    segment_cfg = dict(load_config(segment_fname))
+
+
+@ex.automain
+def main(_config, _run):
+
+    sacred.commands.print_config(_run)
+
+    this_config = _config['segment_cfg'].copy()
+    this_config['dataset_params'] = _config['project_cfg']['dataset_params'].copy()
+
+    recalculate_metadata_from_config(this_config)
