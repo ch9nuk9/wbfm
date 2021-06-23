d2c68a54e11d0f824b0b3470fb0bc81940e272e9
Charles-Fieseler <charles.fieseler@gmail.com>
2021-06-23 16:26:34 +0200

    Debugging sbatch stuff

--- DLC_for_WBFM/scripts/examples/0+copy_immobilized_project_data_to_zarr.sh
+++ DLC_for_WBFM/scripts/examples/0+copy_immobilized_project_data_to_zarr.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/bash
 
-ENV_CMD="conda activate DLC-for-WBFM;"
+ENV_CMD="source activate DLC-for-WBFM;"
 PYTHON_CMD="/users/charles.fieseler/DLC_for_WBFM/DLC_for_WBFM/scripts/visualization/0+copy_video_data.py"
 PROJECT="project_path=/groups/zimmer/shared_projects/wbfm/dlc_stacks/Charlie-immobilized-long/project_config.yaml"
 OPT="tiff_not_zarr=False copy_locally=False DEBUG=True"
 
 LOG="/groups/zimmer/shared_projects/wbfm/dlc_stacks/Charlie-immobilized-long/log.log"
 
-srun "$ENV_CMD python $PYTHON_CMD with $PROJECT $OPT > $LOG"
+sbatch --wrap="$ENV_CMD python $PYTHON_CMD with $PROJECT $OPT > $LOG"
