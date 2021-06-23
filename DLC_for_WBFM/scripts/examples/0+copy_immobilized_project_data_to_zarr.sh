2f35e9813581b49a971b11724791407465093c67
Charles-Fieseler <charles.fieseler@gmail.com>
2021-06-23 16:22:57 +0200

    Add log output

--- DLC_for_WBFM/scripts/examples/0+copy_immobilized_project_data_to_zarr.sh
+++ DLC_for_WBFM/scripts/examples/0+copy_immobilized_project_data_to_zarr.sh
@@ -5,4 +5,6 @@ PYTHON_CMD="/users/charles.fieseler/DLC_for_WBFM/DLC_for_WBFM/scripts/visualizat
 PROJECT="project_path=/groups/zimmer/shared_projects/wbfm/dlc_stacks/Charlie-immobilized-long/project_config.yaml"
 OPT="tiff_not_zarr=False copy_locally=False DEBUG=True"
 
-srun "$ENV_CMD python $PYTHON_CMD with $PROJECT $OPT"
+LOG="/groups/zimmer/shared_projects/wbfm/dlc_stacks/Charlie-immobilized-long/log.log"
+
+srun "$ENV_CMD python $PYTHON_CMD with $PROJECT $OPT > $LOG"
