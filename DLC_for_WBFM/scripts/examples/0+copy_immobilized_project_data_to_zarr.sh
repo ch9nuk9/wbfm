67a446571ec7f339209c9c1479447e86995dec05
Charles-Fieseler <charles.fieseler@gmail.com>
2021-06-23 17:26:05 +0200

    Remove debug mode

--- DLC_for_WBFM/scripts/examples/0+copy_immobilized_project_data_to_zarr.sh
+++ DLC_for_WBFM/scripts/examples/0+copy_immobilized_project_data_to_zarr.sh
@@ -3,7 +3,7 @@
 #ENV_CMD="source activate DLC-for-WBFM;"
 PYTHON_CMD="/users/charles.fieseler/DLC_for_WBFM/DLC_for_WBFM/scripts/visualization/0+copy_video_data.py"
 PROJECT="project_path=/groups/zimmer/shared_projects/wbfm/dlc_stacks/Charlie-worm3-long/project_config.yaml"
-OPT="tiff_not_zarr=False copy_locally=False DEBUG=True"
+OPT="tiff_not_zarr=False copy_locally=False DEBUG=False"
 
 LOG="/groups/zimmer/shared_projects/wbfm/dlc_stacks/Charlie-worm3-long/log.log"
 
