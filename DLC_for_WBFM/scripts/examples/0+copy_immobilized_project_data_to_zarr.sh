837148e0f345c49b7cb2e2ab48a1b0885c49db4b
Charles-Fieseler <charles.fieseler@gmail.com>
2021-06-23 16:34:27 +0200

    Change projects to data that is on the cluster

--- DLC_for_WBFM/scripts/examples/0+copy_immobilized_project_data_to_zarr.sh
+++ DLC_for_WBFM/scripts/examples/0+copy_immobilized_project_data_to_zarr.sh
@@ -2,9 +2,9 @@
 
 #ENV_CMD="source activate DLC-for-WBFM;"
 PYTHON_CMD="/users/charles.fieseler/DLC_for_WBFM/DLC_for_WBFM/scripts/visualization/0+copy_video_data.py"
-PROJECT="project_path=/groups/zimmer/shared_projects/wbfm/dlc_stacks/Charlie-immobilized-long/project_config.yaml"
+PROJECT="project_path=/groups/zimmer/shared_projects/wbfm/dlc_stacks/Charlie-worm3-long/project_config.yaml"
 OPT="tiff_not_zarr=False copy_locally=False DEBUG=True"
 
-LOG="/groups/zimmer/shared_projects/wbfm/dlc_stacks/Charlie-immobilized-long/log.log"
+LOG="/groups/zimmer/shared_projects/wbfm/dlc_stacks/Charlie-worm3-long/log.log"
 
 sbatch --wrap="python $PYTHON_CMD with $PROJECT $OPT > $LOG"
