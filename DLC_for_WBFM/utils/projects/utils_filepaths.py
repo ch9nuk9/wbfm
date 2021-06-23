3162cb25b2269e4f2fe6b0c26dd18251f6000f67
Charles-Fieseler <charles.fieseler@gmail.com>
2021-06-23 17:01:20 +0200

    Add utility for resolving Y: on the cluster

---
+++ DLC_for_WBFM/utils/projects/utils_filepaths.py
@@ -0,0 +1,36 @@
+import os
+from pathlib import Path, PurePosixPath, PureWindowsPath
+
+
+def resolve_mounted_path_in_current_os(path: str):
+    """
+    Removes windows-specific mounted drive names (Y:, D:, etc.) and replaces them with the networked system equivalent
+
+    Does nothing if the path is relative
+
+    Note: This is specific to the Zimmer lab, as of 23.06.2021 (at the IMP)
+    """
+    is_abs = PurePosixPath(path).is_absolute() or PureWindowsPath(path).is_absolute()
+    if not is_abs:
+        return path
+
+    # Swap mounted drive locations
+    # UPDATE REGULARLY
+    mounted_drive_dict = {
+        'Y:': "/groups/zimmer"
+    }
+
+    for win_drive, linux_drive in mounted_drive_dict.items():
+        if os.name.lower() == "linux" and path.startswith(win_drive):
+            path.replace(win_drive, linux_drive)
+        if os.name.lower() == "windows" and path.startswith(linux_drive):
+            path.replace(linux_drive, win_drive)
+
+    # Check for unreachable local drives
+    local_drives = ['C:', 'D:']
+    if os.name.lower() == "linux":
+        for drive in local_drives:
+            if path.startswith(drive):
+                raise FileNotFoundError("File mounted to local drive; network system can't find it")
+
+    return str(Path(path).resolve())
