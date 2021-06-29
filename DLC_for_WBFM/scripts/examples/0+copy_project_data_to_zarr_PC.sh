cf42407753ee34884b465b6337288d6bf5c7c0ac
Charles-Fieseler <charles.fieseler@imp.ac.at>
2021-06-29 10:19:44 +0200

    New function for just copying locally (the cluster ran out of memory)

--- DLC_for_WBFM/scripts/examples/0+copy_project_data_to_zarr_PC.sh
+++ DLC_for_WBFM/scripts/examples/0+copy_project_data_to_zarr_PC.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/bash
 
 #ENV_CMD="source activate DLC-for-WBFM;"
-PYTHON_CMD="/users/charles.fieseler/DLC_for_WBFM/DLC_for_WBFM/scripts/visualization/0+copy_video_data.py"
-PROJECT="project_path=/groups/zimmer/shared_projects/wbfm/dlc_stacks/Charlie-worm3-long/project_config.yaml"
+PYTHON_CMD="C:/Users/charles.fieseler/Documents/Current_work/DLC_for_WBFM/DLC_for_WBFM/scripts/visualization/0+copy_video_data.py"
+PROJECT="project_path=Y:/shared_projects/wbfm/dlc_stacks/Charlie-worm3-long/project_config.yaml"
 OPT="tiff_not_zarr=False copy_locally=False"
 
-LOG="/groups/zimmer/shared_projects/wbfm/dlc_stacks/Charlie-worm3-long/log.log"
+LOG="Y:/shared_projects/wbfm/dlc_stacks/Charlie-worm3-long/log.log"
 
-sbatch --mem 128G --wrap="python $PYTHON_CMD with $PROJECT $OPT > $LOG"
+python $PYTHON_CMD with $PROJECT $OPT > $LOG
