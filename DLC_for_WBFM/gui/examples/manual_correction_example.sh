7bd28d8fa1802d1ff20e35d7255686547d109f8e
Charles-Fieseler <charles.fieseler@gmail.com>
2021-07-14 11:02:51 +0200

    Add example script for building a manual correction gui

---
+++ DLC_for_WBFM/gui/examples/manual_correction_example.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+COMMAND="gui/utils/manual_annotation.py"
+PROJECT="Y:/shared_projects/wbfm/dlc_stacks/Charlie-worm3-new-seg/project_config.yaml"
+
+python COMMAND --project_path $PROJECT
