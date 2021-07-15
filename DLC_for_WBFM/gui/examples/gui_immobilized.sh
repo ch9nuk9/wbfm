85133f655ecba860bbab49193ba09f12271aa2c8
Charles-Fieseler <charles.fieseler@imp.ac.at>
2021-07-15 17:21:54 +0200

    Changes to scripts

--- DLC_for_WBFM/gui/examples/gui_immobilized.sh
+++ DLC_for_WBFM/gui/examples/gui_immobilized.sh
@@ -1,5 +1,5 @@
-#!bash
-PROJECT="Y:/shared_projects/wbfm/dlc_stacks/Charlie-immobilized-2021_05_11/project_config.yaml"
-COMMAND="gui/gui.py"
+#!/usr/bin/env bash
+PROJECT="Y:/shared_projects/wbfm/dlc_stacks/Charlie-immobilized-long/project_config.yaml"
+COMMAND="gui/progress_gui.py"
 
-python $COMMAND $PROJECT
+python $COMMAND --project_path $PROJECT
--- DLC_for_WBFM/gui/examples/manual_correction_example.sh
+++ DLC_for_WBFM/gui/examples/manual_correction_example.sh
@@ -4,4 +4,4 @@ COMMAND="gui/utils/manual_annotation.py"
 PROJECT="Y:/shared_projects/wbfm/dlc_stacks/Charlie-worm3-new-seg/project_config.yaml"
 NAME="Itamar"
 
-python $COMMAND --project_path $PROJECT --corrector_name $NAME
+python $COMMAND --project_path $PROJECT --corrector_name $NAME --annotation
