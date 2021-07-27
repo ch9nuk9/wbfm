293480fe2afd64fb60ae229111f905e7bcc62344
Charles-Fieseler <charles.fieseler@imp.ac.at>
2021-07-27 10:29:48 +0200

    Refactor the dataclass to a new file

--- DLC_for_WBFM/gui/utils/napari_trace_explorer.py
+++ DLC_for_WBFM/gui/utils/napari_trace_explorer.py
@@ -1,113 +1,15 @@
 import argparse
-from dataclasses import dataclass
-import os
-from pathlib import Path
 
 import napari
 import numpy as np
 import pandas as pd
-import zarr
 from PyQt5 import QtWidgets
 from matplotlib.backends.backend_qt5agg import FigureCanvas
 from matplotlib.figure import Figure
 
 from DLC_for_WBFM.gui.utils.utils_gui import zoom_using_viewer, change_viewer_time_point
-from DLC_for_WBFM.utils.projects.utils_project import safe_cd, load_config
-from DLC_for_WBFM.utils.training_data.tracklet_to_DLC import get_or_recalculate_which_frames
+from DLC_for_WBFM.utils.projects.finished_project_data import finished_project_data
 from DLC_for_WBFM.utils.visualization.visualization_behavior import shade_using_behavior
-
-
-@dataclass
-class finished_project_data:
-    project_dir: str
-
-    red_data: zarr.Array
-    green_data: zarr.Array
-
-    red_traces: pd.DataFrame
-    green_traces: pd.DataFrame
-
-    dlc_raw: pd.DataFrame
-
-    raw_segmentation: zarr.Array
-    segmentation: zarr.Array
-
-    behavior_annotations: pd.DataFrame
-    background_per_pixel: float
-
-    @staticmethod
-    def unpack_config_file(project_path):
-        project_dir = Path(project_path).parent
-        cfg = load_config(project_path)
-        with safe_cd(project_dir):
-            traces_cfg = load_config(cfg['subfolder_configs']['traces'])
-            segment_cfg = load_config(cfg['subfolder_configs']['segmentation'])
-            tracking_cfg = load_config(cfg['subfolder_configs']['tracking'])
-
-        return cfg, segment_cfg, tracking_cfg, traces_cfg, project_dir
-
-    @staticmethod
-    def load_data_from_configs(cfg, segment_cfg, tracking_cfg, traces_cfg, project_dir):
-        red_dat_fname = cfg['preprocessed_red']
-        green_dat_fname = cfg['preprocessed_green']
-        red_traces_fname = traces_cfg['traces']['red']
-        green_traces_fname = traces_cfg['traces']['green']
-        dlc_raw_fname = tracking_cfg['final_3d_tracks']['df_fname']
-        seg_fname_raw = segment_cfg['output']['masks']
-        seg_fname = os.path.join('4-traces', 'reindexed_masks.zarr')
-
-        fname = r"3-tracking\postprocessing\manual_behavior_annotation.xlsx"  # TODO
-
-        red_data = zarr.open(red_dat_fname)
-        green_data = zarr.open(green_dat_fname)
-
-        with safe_cd(project_dir):
-            red_traces = pd.read_hdf(red_traces_fname)
-            green_traces = pd.read_hdf(green_traces_fname)
-            dlc_raw = pd.read_hdf(dlc_raw_fname)
-
-            # Segmentation
-            if '.zarr' in seg_fname_raw:
-                raw_segmentation = zarr.open(seg_fname_raw, mode='r')
-            else:
-                raw_segmentation = None
-
-            if os.path.exists(seg_fname):
-                segmentation = zarr.open(seg_fname, mode='r')
-            else:
-                segmentation = None
-
-            behavior_annotations = pd.read_excel(fname, sheet_name='behavior')['Annotation']
-
-        # TODO: do not hardcode
-        background_per_pixel = 15
-
-        start = cfg['dataset_params']['start_volume']
-        end = start + cfg['dataset_params']['num_frames']
-        x = list(range(start, end))
-
-        # Return a full object
-        obj = finished_project_data(
-            project_dir,
-            red_data,
-            green_data,
-            red_traces,
-            green_traces,
-            dlc_raw,
-            raw_segmentation,
-            segmentation,
-            behavior_annotations,
-            background_per_pixel
-        )
-
-        return obj
-
-    @staticmethod
-    def load_all_project_data_from_config(project_path):
-        args = finished_project_data.unpack_config_file(project_path)
-        return finished_project_data.load_data_from_configs(*args)
-
-
 
 
 class napari_trace_explorer(QtWidgets.QWidget):
@@ -121,70 +23,6 @@ class napari_trace_explorer(QtWidgets.QWidget):
         # Load Configs
         ########################
         self.dat = finished_project_data.load_all_project_data_from_config(project_path)
-
-        # out = finished_project_data.unpack_config_file(project_path)
-        # cfg, segment_cfg, tracking_cfg, traces_cfg, project_dir = out
-        # self._load_data(cfg, segment_cfg, tracking_cfg, traces_cfg, project_dir)
-
-        # Temporary
-        # self.df = self.dlc_raw
-
-    # def _load_data(self, cfg, segment_cfg, tracking_cfg, traces_cfg, project_dir):
-    #     ########################
-    #     # Load Data
-    #     ########################
-    #     # Raw data
-    #     red_dat_fname = cfg['preprocessed_red']
-    #     green_dat_fname = cfg['preprocessed_green']
-    #     self.red_data = zarr.open(red_dat_fname)
-    #     self.green_data = zarr.open(green_dat_fname)
-    #
-    #     with safe_cd(self.project_dir):
-    #         # Traces
-    #         red_traces_fname = traces_cfg['traces']['red']
-    #         green_traces_fname = traces_cfg['traces']['green']
-    #         dlc_raw_fname = tracking_cfg['final_3d_tracks']['df_fname']
-    #         self.red_traces = pd.read_hdf(red_traces_fname)
-    #         self.green_traces = pd.read_hdf(green_traces_fname)
-    #         self.dlc_raw = pd.read_hdf(dlc_raw_fname)
-    #
-    #         # Segmentation
-    #         seg_fname_raw = segment_cfg['output']['masks']
-    #         if '.zarr' in seg_fname_raw:
-    #             self.raw_segmentation = zarr.open(seg_fname_raw, mode='r')
-    #         else:
-    #             self.raw_segmentation = None
-    #
-    #         seg_fname = os.path.join('4-traces', 'reindexed_masks.zarr')
-    #         if os.path.exists(seg_fname):
-    #             self.segmentation = zarr.open(seg_fname, mode='r')
-    #         else:
-    #             self.segmentation = None
-    #
-    #         fname = r"3-tracking\postprocessing\manual_behavior_annotation.xlsx"
-    #         self.behavior_annotations = pd.read_excel(fname, sheet_name='behavior')['Annotation']
-    #
-    #     # TODO: do not hardcode
-    #     self.background_per_pixel = 15
-    #
-    #     start = cfg['dataset_params']['start_volume']
-    #     end = start + cfg['dataset_params']['num_frames']
-    #     self.x = list(range(start, end))
-
-
-    # def _load_config_files(self, project_config):
-    #     self.project_dir = Path(project_config).parent
-    #     cfg = load_config(project_config)
-    #     self.cfg = cfg
-    #     with safe_cd(self.project_dir):
-    #         traces_cfg = load_config(cfg['subfolder_configs']['traces'])
-    #         self.traces_cfg = traces_cfg
-    #         segment_cfg = load_config(cfg['subfolder_configs']['segmentation'])
-    #         self.segment_cfg = segment_cfg
-    #         tracking_cfg = load_config(cfg['subfolder_configs']['tracking'])
-    #         self.tracking_cfg = tracking_cfg
-    #
-    #     return cfg, traces_cfg, tracking_cfg
 
     def setupUi(self, viewer: napari.Viewer):
 
---
+++ DLC_for_WBFM/utils/projects/finished_project_data.py
@@ -0,0 +1,99 @@
+import os
+from dataclasses import dataclass
+from pathlib import Path
+
+import pandas as pd
+import zarr
+
+from DLC_for_WBFM.utils.projects.utils_project import load_config, safe_cd
+
+
+@dataclass
+class finished_project_data:
+    project_dir: str
+
+    red_data: zarr.Array
+    green_data: zarr.Array
+
+    red_traces: pd.DataFrame
+    green_traces: pd.DataFrame
+
+    dlc_raw: pd.DataFrame
+
+    raw_segmentation: zarr.Array
+    segmentation: zarr.Array
+
+    behavior_annotations: pd.DataFrame
+    background_per_pixel: float
+
+    @staticmethod
+    def unpack_config_file(project_path):
+        project_dir = Path(project_path).parent
+        cfg = load_config(project_path)
+        with safe_cd(project_dir):
+            traces_cfg = load_config(cfg['subfolder_configs']['traces'])
+            segment_cfg = load_config(cfg['subfolder_configs']['segmentation'])
+            tracking_cfg = load_config(cfg['subfolder_configs']['tracking'])
+
+        return cfg, segment_cfg, tracking_cfg, traces_cfg, project_dir
+
+    @staticmethod
+    def load_data_from_configs(cfg, segment_cfg, tracking_cfg, traces_cfg, project_dir):
+        red_dat_fname = cfg['preprocessed_red']
+        green_dat_fname = cfg['preprocessed_green']
+        red_traces_fname = traces_cfg['traces']['red']
+        green_traces_fname = traces_cfg['traces']['green']
+        dlc_raw_fname = tracking_cfg['final_3d_tracks']['df_fname']
+        seg_fname_raw = segment_cfg['output']['masks']
+        seg_fname = os.path.join('4-traces', 'reindexed_masks.zarr')
+
+        fname = r"3-tracking\postprocessing\manual_behavior_annotation.xlsx"  # TODO
+
+        red_data = zarr.open(red_dat_fname)
+        green_data = zarr.open(green_dat_fname)
+
+        with safe_cd(project_dir):
+            red_traces = pd.read_hdf(red_traces_fname)
+            green_traces = pd.read_hdf(green_traces_fname)
+            dlc_raw = pd.read_hdf(dlc_raw_fname)
+
+            # Segmentation
+            if '.zarr' in seg_fname_raw:
+                raw_segmentation = zarr.open(seg_fname_raw, mode='r')
+            else:
+                raw_segmentation = None
+
+            if os.path.exists(seg_fname):
+                segmentation = zarr.open(seg_fname, mode='r')
+            else:
+                segmentation = None
+
+            behavior_annotations = pd.read_excel(fname, sheet_name='behavior')['Annotation']
+
+        # TODO: do not hardcode
+        background_per_pixel = 15
+
+        start = cfg['dataset_params']['start_volume']
+        end = start + cfg['dataset_params']['num_frames']
+        x = list(range(start, end))
+
+        # Return a full object
+        obj = finished_project_data(
+            project_dir,
+            red_data,
+            green_data,
+            red_traces,
+            green_traces,
+            dlc_raw,
+            raw_segmentation,
+            segmentation,
+            behavior_annotations,
+            background_per_pixel
+        )
+
+        return obj
+
+    @staticmethod
+    def load_all_project_data_from_config(project_path):
+        args = finished_project_data.unpack_config_file(project_path)
+        return finished_project_data.load_data_from_configs(*args)
