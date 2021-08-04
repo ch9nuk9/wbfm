2e45a2ac7f6daa5979b0d4971f85f568a6b55a82
Charles Fieseler <charles.fieseler@univie.ac.at>
2021-08-04 18:40:32 +0200

    New script for trace explorer

--- DLC_for_WBFM/gui/utils/napari_trace_explorer.py
+++ DLC_for_WBFM/gui/utils/napari_trace_explorer.py
@@ -235,19 +235,3 @@ def build_napari_trace_explorer(project_config):
     print("Finished GUI setup")
 
     napari.run()
-
-
-if __name__ == "__main__":
-
-    parser = argparse.ArgumentParser(description='Build GUI with a project')
-    parser.add_argument('--project_path', default=None,
-                        help='path to config file')
-    parser.add_argument('--DEBUG', default=False,
-                        help='')
-    args = parser.parse_args()
-    project_path = args.project_path
-    DEBUG = args.DEBUG
-
-    print("Starting trace explorer GUI, may take a while to load...")
-
-    build_napari_trace_explorer(project_path)
