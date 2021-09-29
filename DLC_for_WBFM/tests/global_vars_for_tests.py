d5b5d3f8b90d9f1afc6b415a36fa154b93eb395e
Charles-Fieseler-Vienna <charles.fieseler@univie.ac.at>
2021-09-29 17:50:54 +0200

    Change pytest to use a folder on /scratch, not locally

--- DLC_for_WBFM/tests/global_vars_for_tests.py
+++ DLC_for_WBFM/tests/global_vars_for_tests.py
@@ -1,2 +1,3 @@
 # project_path = r"C:/dlc_stacks/Test_project/project_config.yaml"
-project_path = "/home/charles/dlc_stacks/worm3-pytest/project_config.yaml"
+# project_path = "/home/charles/dlc_stacks/worm3-pytest/project_config.yaml"
+project_path = "/scratch/zimmer/Charles/dlc_stacks/project-pytest/project_config.yaml"
