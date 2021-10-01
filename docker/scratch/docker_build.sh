19c96c329578b011b8b05e5ee21298e57a0f6e14
Charles-Fieseler-Vienna <charles.fieseler@univie.ac.at>
2021-10-01 18:20:15 +0200

    More working on getting docker to work

--- docker/scratch/docker_build.sh
+++ docker/scratch/docker_build.sh
@@ -4,5 +4,11 @@
 eval "$(ssh-agent)"
 ssh-add ~/.ssh/id_ed25519
 
+echo "Found ssh keys: $(ssh-add -L)"
+echo "$(ssh -T git@github.com)"
+
 # Main command
-sudo DOCKER_BUILDKIT=1 docker build --ssh default="${SSH_AUTH_SOCK}" .
+sudo DOCKER_BUILDKIT=1 docker build--ssh default="${SSH_AUTH_SOCK}" .
+
+# Clean up
+eval "$(ssh-agent -k)"
