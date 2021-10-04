a31db7e35b9171474780cb89b8dfb87b60a90bff
Charles-Fieseler-Vienna <charles.fieseler@univie.ac.at>
2021-10-04 17:40:56 +0200

    Bugfix with loop directories

--- DLC_for_WBFM/scripts/cluster/3b-train_all_dlc_networks.sh
+++ DLC_for_WBFM/scripts/cluster/3b-train_all_dlc_networks.sh
@@ -9,7 +9,10 @@ for f in "${TARGET_DIR}"/*; do
     if [ -d "$f" ]; then
         dlc_config="${f}/config.yaml"
 
-        sbatch --job-name=dlc_$i --output=dlc_$i.out train_single_dlc_network.sbatch $dlc_config
+        # Using srun to not overload the GPU memory
+        srun --job-name=dlc_$i --output=dlc_$i.out train_single_dlc_network.sbatch $dlc_config
         i=$((i+1))
+
+        sleep 1
     fi
 done
