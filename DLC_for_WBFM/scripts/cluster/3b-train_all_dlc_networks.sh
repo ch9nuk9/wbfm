aa99e51b926a1e12f62e30bb4275653600b8e0a1
Charles-Fieseler-Vienna <charles.fieseler@univie.ac.at>
2021-10-04 17:34:19 +0200

    Bugfix with loop directories

--- DLC_for_WBFM/scripts/cluster/3b-train_all_dlc_networks.sh
+++ DLC_for_WBFM/scripts/cluster/3b-train_all_dlc_networks.sh
@@ -5,9 +5,9 @@ echo "Running on parent directory: $TARGET_DIR"
 
 # TODO: actually use an array job, not just looping
 i=0
-for f in $TARGET_DIR; do
+for f in "${TARGET_DIR}"/*; do
     if [ -d "$f" ]; then
-        dlc_config="${f}config.yaml"
+        dlc_config="${f}/config.yaml"
 
         sbatch --job-name=dlc_$i --output=dlc_$i.out train_single_dlc_network.sbatch $dlc_config
         i=$((i+1))
