ca5a482ca4682d7944c0f5daa219cbe196df3500
Charles-Fieseler-Vienna <charles.fieseler@univie.ac.at>
2021-10-05 08:22:07 +0200

    Add possibly necessary flag to cluster-running gpu script

--- DLC_for_WBFM/scripts/cluster/train_single_dlc_network.py
+++ DLC_for_WBFM/scripts/cluster/train_single_dlc_network.py
@@ -1,5 +1,9 @@
 import argparse
 from DLC_for_WBFM.utils.pipeline.dlc_pipeline import train_single_dlc_network
+import os
+# Might be necessary on tf 2.3.0 and the GPU on the cluster... see:
+# https://github.com/tensorflow/tensorflow/issues/41990
+os.environ['TF_FORCE_GPU_ALLOW_GROWTH'] = 'true'
 
 
 if __name__ == "__main__":
