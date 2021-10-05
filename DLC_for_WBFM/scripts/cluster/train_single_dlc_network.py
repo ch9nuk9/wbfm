1d28d695f7eb9046f12803408fad40d905f704aa
Charles-Fieseler-Vienna <charles.fieseler@univie.ac.at>
2021-10-05 08:30:49 +0200

    Add possible fixes and more printing to scripts

--- DLC_for_WBFM/scripts/cluster/train_single_dlc_network.py
+++ DLC_for_WBFM/scripts/cluster/train_single_dlc_network.py
@@ -3,6 +3,8 @@ from DLC_for_WBFM.utils.pipeline.dlc_pipeline import train_single_dlc_network
 import os
 # Might be necessary on tf 2.3.0 and the GPU on the cluster... see:
 # https://github.com/tensorflow/tensorflow/issues/41990
+# See also this for discussions of this being a design flaw in tensorflow:
+# https://github.com/tensorflow/tensorflow/issues/36465
 os.environ['TF_FORCE_GPU_ALLOW_GROWTH'] = 'true'
 
 
@@ -14,3 +16,8 @@ if __name__ == "__main__":
     dlc_config = args.dlc_config
 
     train_single_dlc_network(dlc_config)
+
+    # Unclear if this is necessary or works:
+    # https://github.com/tensorflow/tensorflow/issues/36465
+    tf.keras.backend.clear_session()
+    gc.collect()
