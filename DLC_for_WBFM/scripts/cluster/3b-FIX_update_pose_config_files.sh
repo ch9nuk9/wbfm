f3a5dfe9dd98c2f91a2bb30a99fd2bb780d74140
Charles-Fieseler-Vienna <charles.fieseler@univie.ac.at>
2021-10-05 08:58:07 +0200

    New script for updating the paths to the pretrained networks on the cluster

---
+++ DLC_for_WBFM/scripts/cluster/3b-FIX_update_pose_config_files.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Updates the config files for all dlc sub-projects to point to the correct cluster locations
+# In detail: DLC uses pretrained resnet weights that are stored in the conda environment (hard coded)
+# So if you create the project on windows, these paths won't be correct
+
+# First, reset all the paths to work for the cluster
+PROJECT="$1"
+INIT_WEIGHTS="/home/user/fieseler/.conda/envs/wbfm-cluster/lib/python3.8/site-packages/deeplabcut/pose_estimation_tensorflow/models/pretrained/resnet_v1_50.ckpt"
+CMD="/scratch/zimmer/Charles/dlc_for_wbfm/DLC_for_WBFM/scripts/alternate/3b-alternate-update_all_pose_configs.py"
+python $CMD with project_path=$PROJECT update_key=init_weights update_val=$INIT_WEIGHTS
+
+# Then, reset all the paths that can start from my pretrained networks
+python $CMD with project_path=$PROJECT
+
+echo "Finished"
+date
