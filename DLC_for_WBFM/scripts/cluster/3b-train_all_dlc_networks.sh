20bb79f0f0d6821a29748928b8005335f9a2e532
Charles-Fieseler-Vienna <charles.fieseler@univie.ac.at>
2021-10-05 12:53:58 +0200

    Removing forced wait in sbatch... might successfully submit all the sbatch jobs now

--- DLC_for_WBFM/scripts/cluster/3b-train_all_dlc_networks.sh
+++ DLC_for_WBFM/scripts/cluster/3b-train_all_dlc_networks.sh
@@ -9,8 +9,10 @@ for f in "${TARGET_DIR}"/*; do
     if [ -d "$f" ]; then
         dlc_config="${f}/config.yaml"
 
-        # Using --wait to not overload the GPU memory
-        sbatch --wait --job-name=dlc_$i --output=dlc_$i.out train_single_dlc_network.sbatch $dlc_config
+        # If overloading GPU memory, user --wait
+        sbatch --job-name=dlc_$i --output=dlc_$i.out train_single_dlc_network.sbatch "$dlc_config"
         i=$((i+1))
+
+        sleep 1
     fi
 done
