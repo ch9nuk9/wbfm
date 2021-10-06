24e0f31738e61d1acc4a6381e139480f3c3a9cdc
Charles-Fieseler-Vienna <charles.fieseler@univie.ac.at>
2021-10-06 19:04:23 +0200

    Fix bug with incorrect minimum length not reading from parameters

--- DLC_for_WBFM/utils/training_data/tracklet_to_DLC.py
+++ DLC_for_WBFM/utils/training_data/tracklet_to_DLC.py
@@ -103,7 +103,7 @@ def convert_training_dataframe_to_dlc_format(df, min_length=10, scorer=None):
         frame = pd.DataFrame(coords, columns=index, index=which_frames)
         all_dfs.append(frame)
     if len(all_dfs) == 0:
-        raise ParametersTooStringentError(min_length, 'min_length')
+        raise ParameterTooStringentError(min_length, 'min_length')
     new_df = pd.concat(all_dfs, axis=1)
 
     return new_df
@@ -124,9 +124,7 @@ def save_training_data_as_dlc_format(tracking_config: config_file_with_project_c
 
     """
 
-    fname = os.path.join('2-training_data', 'raw', 'clust_df_dat.pickle')
-    fname = training_config.resolve_relative_path(fname)
-    df = pd.read_pickle(fname)
+    df, min_length_to_save = _unpack_config_training_data_conversion(training_config)
 
     # Get the frames chosen as training data, or recalculate
     num_frames = len(df)
@@ -138,7 +136,8 @@ def save_training_data_as_dlc_format(tracking_config: config_file_with_project_c
                   'max_z_dist': None,
                   'verbose': 1}
     subset_df = build_subset_df_from_tracklets(df, which_frames, **subset_opt)
-    training_df = convert_training_dataframe_to_dlc_format(subset_df, scorer=None)
+    training_df = convert_training_dataframe_to_dlc_format(subset_df,
+                                                           min_length=min_length_to_save, scorer=None)
 
     out_fname = os.path.join("2-training_data", "training_data_tracks.h5")
     training_df.to_hdf(out_fname, 'df_with_missing')
@@ -148,6 +147,14 @@ def save_training_data_as_dlc_format(tracking_config: config_file_with_project_c
 
     out_fname = Path(out_fname).with_suffix(".csv")
     training_df.to_csv(out_fname)
+
+
+def _unpack_config_training_data_conversion(training_config):
+    min_length_to_save = training_config.config['postprocessing_params']['min_length_to_save']
+    fname = os.path.join('2-training_data', 'raw', 'clust_df_dat.pickle')
+    fname = training_config.resolve_relative_path(fname)
+    df = pd.read_pickle(fname)
+    return df, min_length_to_save
 
 
 def save_all_tracklets_as_dlc_format(this_config, min_length, DEBUG=False):
