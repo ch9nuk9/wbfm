ed98122008f7962637d9bd3e27eca435563ab0e3
Charles-Fieseler-Vienna <charles.fieseler@univie.ac.at>
2021-10-06 11:44:26 +0200

    Properly activate GPU

--- DLC_for_WBFM/scripts/cluster/3c-make_full_tracks.sh
+++ DLC_for_WBFM/scripts/cluster/3c-make_full_tracks.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+pwd; hostname; date
+module load cudnn/7.6.5
+
 CMD="/scratch/zimmer/Charles/github_repos/dlc_for_wbfm/DLC_for_WBFM/scripts/3c-make_full_tracks.py"
 project_path="$1"
 
