861c5557366407b00e049d7caac78bcb89f8e29c
Charles-Fieseler-Vienna <charles.fieseler@univie.ac.at>
2021-10-14 11:58:07 +0200

    New cluster script

---
+++ DLC_for_WBFM/scripts/cluster/alt-3-track_using_fdnc.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+#SBATCH --job-name=fdnc_tracking   # Job name
+#SBATCH --nodes=1                   # Use one node
+#SBATCH --ntasks=1                  # Run a single task
+#SBATCH --qos=short             # Time limit hrs:min:sec
+#SBATCH --output=fdnc_tracking.out    # Standard output and error log
+#SBATCH --time=2:00:00
+#SBATCH --mem=16G
+
+pwd; hostname; date
+
+while getopts t:n: flag
+do
+    case "${flag}" in
+        t) project_path=${OPTARG};;
+        n) is_dry_run=${OPTARG};;
+        *) raise error "Unknown flag"
+    esac
+done
+
+CMD="/scratch/zimmer/Charles/github_repos/dlc_for_wbfm/DLC_for_WBFM/scripts/alternate/3-track_using_fdnc.py"
+
+if [ "$is_dry_run" ]; then
+  echo "Dry run with command: $CMD with project_path=$project_path"
+else
+  python $CMD with project_path="$project_path"
+fi
