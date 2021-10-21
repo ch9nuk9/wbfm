6f01f4dca70c42a8a2d447d46b3ecaf493a73a57
Charles-Fieseler-Vienna <charles.fieseler@univie.ac.at>
2021-10-21 18:34:16 +0200

    New cluster script for combining tracklets and tracks

---
+++ DLC_for_WBFM/scripts/cluster/alt-3-combine_tracks_and_tracklets.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+#SBATCH --job-name=fdnc_tracking
+#SBATCH --nodes=1                   # Use one node
+#SBATCH --ntasks=1                  # Run a single task
+#SBATCH --qos=medium             # Time limit hrs:min:sec
+#SBATCH --time=6:00:00
+#SBATCH --mem=16G
+#SBATCH --cpus-per-task=8
+
+pwd; hostname; date
+
+while getopts t:n: flag
+do
+    case "${flag}" in
+        t) project_path=${OPTARG};;
+        n) is_dry_run=${OPTARG};;
+        *) raise error "Unknown flag"
+    esac
+done
+
+CMD="/scratch/zimmer/Charles/github_repos/dlc_for_wbfm/DLC_for_WBFM/scripts/postprocessing/3c+combine_tracklets_and_dlc_tracks.py"
+
+if [ "$is_dry_run" ]; then
+  echo "Dry run with command: $CMD with project_path=$project_path"
+else
+  python $CMD with project_path="$project_path"
+fi
