ecec295c9a086b6f0e00e3106fa7a919e0b8b294
Charles-Fieseler-Vienna <charles.fieseler@univie.ac.at>
2021-10-29 19:22:04 +0200

    Bash script for traces (everything after tracks)

---
+++ DLC_for_WBFM/scripts/multistep_bash/all_steps_after_tracks.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+pwd; hostname; date
+
+while getopts t:n: flag
+do
+    case "${flag}" in
+        t) project_path=${OPTARG};;
+        n) is_dry_run=${OPTARG};;
+        *) raise error "Unknown flag"
+    esac
+done
+
+# Make conda functions available
+# https://github.com/conda/conda/issues/7980
+source /home/charles/anaconda3/etc/profile.d/conda.sh
+#conda activate my_env
+
+
+CODE_PATH="/scratch/zimmer/Charles/github_repos/dlc_for_wbfm/DLC_for_WBFM/scripts"
+SUFFIX="with project_path=$project_path"
+
+if [ "$is_dry_run" ]; then
+  echo "Dry run with command: $CMD with project_path=$project_path"
+else
+
+  conda activate torch
+  CMD="${CODE_PATH}/4a-match_tracks_and_segmentation.py"
+  python $CMD $SUFFIX
+  CMD="${CODE_PATH}/4b-reindex_segmentation_full.py"
+  python $CMD $SUFFIX
+  CMD="${CODE_PATH}/4c-extract_full_traces.py"
+  python $CMD $SUFFIX
+fi
