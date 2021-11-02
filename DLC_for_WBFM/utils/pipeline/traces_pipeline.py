e5d1c8ed08fa92b35658b18e51703fc781c58afc
Charles-Fieseler-Vienna <charles.fieseler@univie.ac.at>
2021-11-02 16:46:13 +0100

    Properly index if not starting from i=0

--- DLC_for_WBFM/utils/pipeline/traces_pipeline.py
+++ DLC_for_WBFM/utils/pipeline/traces_pipeline.py
@@ -89,22 +89,26 @@ def extract_traces_using_config(project_cfg: SubfolderConfigFile,
 def _convert_nested_dict_to_dataframe(coords, frame_list, green_all_neurons, red_all_neurons):
     # Convert nested dict of volumes to final dataframes
     sz_one_neuron = len(frame_list)
+    i_start = min(list(red_all_neurons.keys()))
     tmp_red = defaultdict(lambda: np.zeros(sz_one_neuron))
     tmp_green = defaultdict(lambda: np.zeros(sz_one_neuron))
-    for i_vol in red_all_neurons.keys():
+    volume_indices = list(red_all_neurons.keys())
+    volume_indices.sort()
+    for i_vol in volume_indices:
         red_props, green_props = red_all_neurons[i_vol], green_all_neurons[i_vol]
+        i_vol_numpy = i_vol - i_start
         for key in red_props.keys():
             if 'weighted_centroid' in key:
-                # Later formatting expects this to be split
+                # Later formatting expects this to be split, i.e. z x y are separate columns
                 for i, c in enumerate(coords):
                     new_key = (key[0], c)
-                    tmp_red[new_key][i_vol] = red_props[key][i]
-                    tmp_green[new_key][i_vol] = green_props[key][i]
+                    tmp_red[new_key][i_vol_numpy] = red_props[key][i]
+                    tmp_green[new_key][i_vol_numpy] = green_props[key][i]
             else:
-                tmp_red[key][i_vol] = red_props[key]
-                tmp_green[key][i_vol] = green_props[key]
-    df_red = pd.DataFrame(tmp_red)
-    df_green = pd.DataFrame(tmp_green)
+                tmp_red[key][i_vol_numpy] = red_props[key]
+                tmp_green[key][i_vol_numpy] = green_props[key]
+    df_red = pd.DataFrame(tmp_red, index=volume_indices)
+    df_green = pd.DataFrame(tmp_green, index=volume_indices)
     return df_green, df_red
 
 
@@ -220,7 +224,6 @@ def region_props_one_volume(this_mask_volume,
                 green_neurons_one_volume[key] = this_green[this_prop]
 
     return red_neurons_one_volume, green_neurons_one_volume
-
 
 
 def _pool_parallel_func(i_and_name, options):
