754100fd378a2682e0216d9ea85d1c102c1f9fff
Charles-Fieseler-Vienna <charles.fieseler@univie.ac.at>
2021-11-03 14:39:45 +0100

    Fix improper refactoring

--- DLC_for_WBFM/utils/projects/finished_project_data.py
+++ DLC_for_WBFM/utils/projects/finished_project_data.py
@@ -11,6 +11,10 @@ import zarr
 
 from DLC_for_WBFM.utils.visualization.filtering_traces import remove_outliers_via_rolling_mean, filter_rolling_mean, \
     filter_linear_interpolation, trace_from_dataframe_factory
+from DLC_for_WBFM.utils.visualization.napari_from_config import napari_labels_from_frames
+from DLC_for_WBFM.utils.visualization.napari_utils import napari_labels_from_traces_dataframe
+from DLC_for_WBFM.utils.visualization.visualization_behavior import shade_using_behavior
+from scipy.spatial.distance import cdist
 from segmentation.util.utils_metadata import DetectedNeurons
 from DLC_for_WBFM.utils.projects.utils_filepaths import ModularProjectConfig, read_if_exists, pickle_load_binary, \
     SubfolderConfigFile
@@ -277,6 +281,112 @@ class ProjectData:
 
         return y
 
+    def modify_confidences_of_frame_pair(self, pair, gamma, mode):
+        frame_match = self.raw_matches[pair]
+
+        matches = frame_match.modify_confidences_using_image_features(self.segmentation_metadata,
+                                                                      gamma=gamma,
+                                                                      mode=mode)
+        frame_match.final_matches = matches
+        return matches
+
+    def modify_confidences_of_all_frame_pairs(self, gamma, mode):
+        frame_matches = self.raw_matches
+        opt = dict(metadata=self.segmentation_metadata, gamma=gamma, mode=mode)
+        for pair, obj in frame_matches.items():
+            matches = obj.modify_confidences_using_image_features(**opt)
+            obj.final_matches = matches
+
+    def shade_axis_using_behavior(self, ax=None, behaviors_to_ignore='none'):
+        if self.behavior_annotations is None:
+            pass
+            # logging.warning("No behavior annotations present; skipping")
+        else:
+            shade_using_behavior(self.behavior_annotations, ax, behaviors_to_ignore)
+
+    def get_centroids_as_numpy(self, i_frame):
+        """Original format of metadata is a dataframe of tuples; this returns a normal np.array"""
+        return self.segmentation_metadata.detect_neurons_from_file(i_frame)
+
+    def get_centroids_as_numpy_training(self, i_frame: int, is_relative_index=True) -> np.ndarray:
+        """Original format of metadata is a dataframe of tuples; this returns a normal np.array"""
+        if is_relative_index:
+            i_frame = self.correct_relative_index(i_frame)
+        return self.reindexed_metadata_training.detect_neurons_from_file(i_frame)
+
+    def get_centroids_as_numpy_training_with_unmatched(self, i_rel: int):
+        i_abs = self.correct_relative_index(i_rel)
+        matched_pts = self.reindexed_metadata_training.detect_neurons_from_file(i_abs)
+        all_pts = self.segmentation_metadata.detect_neurons_from_file(i_abs)
+
+        # Any points that do not have a near-identical match in matched_pts are unmatched
+        # These will be appended
+        tol = 2.0
+        ind_unmatched = ~np.any(cdist(all_pts, matched_pts) < tol, axis=1)
+
+        pts_to_add = all_pts[ind_unmatched, :]
+        final_pts = np.vstack([matched_pts, pts_to_add])
+        return final_pts
+
+    def correct_relative_index(self, i):
+        return self.which_training_frames[i]
+
+    def napari_of_single_match(self, pair, which_matches='final_matches'):
+        import napari
+        from DLC_for_WBFM.utils.visualization.napari_from_config import napari_tracks_from_match_list
+
+        raw_red_data = self.red_data[pair[0]:pair[1] + 1, ...]
+        this_match = self.raw_matches[pair]
+        n0_zxy_raw = this_match.frame0.neuron_locs
+        n1_zxy_raw = this_match.frame1.neuron_locs
+
+        list_of_matches = getattr(this_match, which_matches)
+        all_tracks_list = napari_tracks_from_match_list(list_of_matches, n0_zxy_raw, n1_zxy_raw)
+
+        v = napari.view_image(raw_red_data, ndisplay=3)
+        v.add_points(n0_zxy_raw, size=3, face_color='green', symbol='x', n_dimensional=True)
+        v.add_points(n1_zxy_raw, size=3, face_color='blue', symbol='o', n_dimensional=True)
+        v.add_tracks(all_tracks_list, head_length=2, name=which_matches)
+
+        # Add text overlay
+        frames = {0: this_match.frame0, 1: this_match.frame1}
+        options = napari_labels_from_frames(frames, num_frames=2, to_flip_zxy=False)
+        v.add_points(**options)
+
+        return v
+
+    def add_layers_to_viewer(self, viewer, which_layers='all'):
+        print("Finished loading data, starting napari...")
+        viewer.add_image(self.red_data, name="Red data", opacity=0.5, colormap='red', visible=False)
+        viewer.add_image(self.green_data, name="Green data", opacity=0.5, colormap='green')
+        viewer.add_labels(self.raw_segmentation, name="Raw segmentation", opacity=0.4, visible=False)
+        if self.segmentation is not None:
+            viewer.add_labels(self.segmentation, name="Colored segmentation", opacity=0.4)
+
+        # Add a text overlay
+        df = self.red_traces
+        options = napari_labels_from_traces_dataframe(df)
+        viewer.add_points(**options)
+
+    def __repr__(self):
+        return f"=======================================\n\
+Project data for directory:\n\
+{self.project_dir} \n\
+=======================================\n\
+Found the following raw data files:\n\
+red_data: {self.red_data is not None}\n\
+green_data: {self.green_data is not None}\n\
+============Segmentation===============\n\
+raw_segmentation: {self.raw_segmentation is not None}\n\
+segmentation: {self.segmentation is not None}\n\
+============Tracklets==================\n\
+df_training_tracklets: {self.df_training_tracklets is not None}\n\
+reindexed_masks_training: {self.reindexed_masks_training is not None}\n\
+============Traces=====================\n\
+red_traces: {self.red_traces is not None}\n\
+green_traces: {self.green_traces is not None}\n\
+final_tracks: {self.final_tracks is not None}\n\
+behavior_annotations: {self.behavior_annotations is not None}\n"
 
 def napari_of_training_data(cfg: ModularProjectConfig) -> Tuple[napari.Viewer, np.ndarray, np.ndarray]:
 
