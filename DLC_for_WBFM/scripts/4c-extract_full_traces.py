805c11369fbd3e72725d1489fd0943a9c7ee9b3e
Charles-Fieseler-Vienna <charles.fieseler@univie.ac.at>
2021-11-08 12:26:36 +0100

    Make grid plots by default

--- DLC_for_WBFM/scripts/4c-extract_full_traces.py
+++ DLC_for_WBFM/scripts/4c-extract_full_traces.py
@@ -3,6 +3,8 @@ The top level function for getting final traces from 3d tracks and neuron masks
 """
 
 # Experiment tracking
+import logging
+
 import sacred
 from sacred import Experiment
 from sacred import SETTINGS
@@ -12,8 +14,10 @@ from DLC_for_WBFM.utils.external.monkeypatch_json import using_monkeypatch
 
 from DLC_for_WBFM.utils.pipeline.traces_pipeline import get_traces_from_3d_tracks_using_config, \
     extract_traces_using_config
+from DLC_for_WBFM.utils.projects.finished_project_data import ProjectData
 from DLC_for_WBFM.utils.projects.utils_filepaths import ModularProjectConfig
 from DLC_for_WBFM.utils.projects.utils_project import safe_cd
+from DLC_for_WBFM.utils.visualization.plot_traces import make_grid_plot_from_project
 
 SETTINGS.CONFIG.READ_ONLY_CONFIG = False
 
@@ -48,3 +52,8 @@ def make_full_tracks(_config, _run):
     with safe_cd(_config['project_dir']):
         # Reads masks from disk, and writes traces
         extract_traces_using_config(project_cfg, trace_cfg, DEBUG)
+
+        # By default make some visualizations
+        logging.info("Making default grid plots")
+        proj_dat = ProjectData.load_final_project_data_from_config(project_cfg)
+        make_grid_plot_from_project(proj_dat, channel_mode='all', calculation_mode='integration')
