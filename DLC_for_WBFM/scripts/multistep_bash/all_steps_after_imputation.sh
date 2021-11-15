16d7e66b966a097eefaca88caff1ebc30d024316
Charles-Fieseler-Vienna <charles.fieseler@univie.ac.at>
2021-11-15 18:23:55 +0100

    New script for all steps after imputation

---
+++ DLC_for_WBFM/scripts/multistep_bash/all_steps_after_imputation.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+pwd; hostname; date
+
+while getopts t:n: flag
+do
+    case "${flag}" in
+        t) project_path=${OPTARG};;
+        n) is_dry_run=${OPTARG};;
+        *) raise error "Unknown flag"
+    esac
+done
+
+# Make conda functions available
+# https://github.com/conda/conda/issues/7980
+#source /home/charles/anaconda3/etc/profile.d/conda.sh
+#conda init bash
+
+CODE_PATH="/scratch/zimmer/Charles/github_repos/dlc_for_wbfm/DLC_for_WBFM/scripts"
+SUFFIX="with project_path=$project_path"
+
+if [ "$is_dry_run" ]; then
+  echo "Dry run with command: $CMD with project_path=$project_path"
+else
+
+
+  # Initially start from manual matches
+  CMD="${CODE_PATH}/3b-match_tracklets_and_dlc_tracks.py"
+  python $CMD $SUFFIX start_from_manual_matches=True use_imputed_df=True
+  # Now there are new matches
+  CMD="${CODE_PATH}/3c-make_final_tracks_using_tracklet_matches.py"
+  python $CMD $SUFFIX start_from_manual_matches=False use_imputed_df=True
+
+  CMD="${CODE_PATH}/4a-match_tracks_and_segmentation.py"
+  python $CMD $SUFFIX
+  CMD="${CODE_PATH}/4b-reindex_segmentation_full.py"
+  python $CMD $SUFFIX
+  CMD="${CODE_PATH}/4c-extract_full_traces.py"
+  python $CMD $SUFFIX
+fi
