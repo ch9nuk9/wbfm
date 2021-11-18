5943e10e01547a8e2dde86c4e810a68c3fe1ca5d
Charles-Fieseler-Vienna <charles.fieseler@univie.ac.at>
2021-11-18 15:00:12 +0100

    More convinience methods

--- DLC_for_WBFM/utils/pipeline/matches_class.py
+++ DLC_for_WBFM/utils/pipeline/matches_class.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 
 import numpy as np
 from DLC_for_WBFM.utils.projects.utils_neuron_names import int2name
+from scipy.optimize import linear_sum_assignment
 
 
 @dataclass
@@ -28,8 +29,20 @@ class MatchesWithConfidence:
         return {n1: n0 for n0, n1 in zip(self.indices1, self.indices2)}
 
     @property
+    def mapping_0_to_1_names(self):
+        return {n0: n1 for n0, n1 in zip(self.names1, self.names2)}
+
+    @property
+    def mapping_1_to_0_names(self):
+        return {n1: n0 for n0, n1 in zip(self.names1, self.names2)}
+
+    @property
     def mapping_pair_to_conf(self):
         return {(n0, n1): c for n0, n1, c in zip(self.indices1, self.indices2, self.likelihoods)}
+
+    @property
+    def mapping_pair_to_conf_names(self):
+        return {(n0, n1): c for n0, n1, c in zip(self.names1, self.names2, self.likelihoods)}
 
     @property
     def matches_with_conf(self):
@@ -40,3 +53,9 @@ class MatchesWithConfidence:
         i1 = [int(m) for m in matches_with_conf[:, 0]]
         i2 = [int(m) for m in matches_with_conf[:, 1]]
         return MatchesWithConfidence(i1, i2, matches_with_conf[:, 2])
+
+    @staticmethod
+    def matches_from_distance_matrix(dist):
+        row_i, col_i = linear_sum_assignment(dist)
+        conf = [dist[i, j] for i, j in zip(row_i, col_i)]
+        return MatchesWithConfidence(row_i, col_i, conf)
