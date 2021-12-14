356bbe484dcf77e155fc9241f0dc0da5b402af39
Charles-Fieseler-Vienna <charles.fieseler@univie.ac.at>
2021-12-14 11:35:15 +0100

    New functions for transforming centerline coordinates

---
+++ DLC_for_WBFM/utils/external/centerline_utils.py
@@ -0,0 +1,55 @@
+import numpy as np
+from skimage import transform
+
+
+def get_scale_fluo_to_behavior():
+    #calculate scale
+    ##pixel size in um
+    img1_px_size = 0.325
+    img2_px_size = 2.4
+    scale = img1_px_size / img2_px_size
+    # print('scale is :', scale)
+    return scale
+
+
+def get_rotation_fluo_to_behavior():
+    return np.pi / 1.85
+
+
+def get_translation_fluo_to_behavior():
+    return (430, 330)  # (680,1000)
+
+
+def get_full_transformation_fluo_to_behavior():
+    # Example application:
+    # tf_img2 = transform.warp(img2, tform2.inverse, output_shape=img1.shape)
+    # Where img1 is the behavior and img2 is the fluorescence
+
+    scale2 = get_scale_fluo_to_behavior()
+    rotation2 = get_rotation_fluo_to_behavior()
+    translation2 = get_translation_fluo_to_behavior()
+
+    # transformation2
+    tform2 = transform.SimilarityTransform(
+        scale=scale2,
+        rotation=rotation2,
+        translation=translation2)
+
+    return tform2
+
+
+def get_full_transformation_behavior_to_fluo():
+    # Inverse of get_full_transformation_fluo_to_behavior
+    # TOCHECK
+
+    scale2 = 1 / get_scale_fluo_to_behavior()
+    rotation2 = - get_rotation_fluo_to_behavior()
+    translation2 = [-t for t in get_translation_fluo_to_behavior()]
+
+    # transformation2
+    tform2 = transform.SimilarityTransform(
+        scale=scale2,
+        rotation=rotation2,
+        translation=translation2)
+
+    return tform2
--- DLC_for_WBFM/utils/nn_utils/data_formatting.py
+++ DLC_for_WBFM/utils/nn_utils/data_formatting.py
@@ -1,15 +1,16 @@
 import numpy as np
 from sklearn.preprocessing import StandardScaler
 
-zimmer_um_per_pixel_xy = 0.325
+zimmer_fluroscence_um_per_pixel_xy = 0.325
+zimmer_behavior_um_per_pixel_xy = 2.4
 zimmer_um_per_pixel_z = 1
 
 leifer_um_per_unit = 84
 
 
-def zimmer2physical(vol0_zxy: np.ndarray) -> np.ndarray:
+def zimmer2physical_fluorescence(vol0_zxy: np.ndarray) -> np.ndarray:
     # xy, then z
-    xy_in_um = vol0_zxy[:, [1, 2]] * zimmer_um_per_pixel_xy
+    xy_in_um = vol0_zxy[:, [1, 2]] * zimmer_fluroscence_um_per_pixel_xy
     xy_in_physical = xy_in_um
 
     z_in_um = vol0_zxy[:, [0]] * zimmer_um_per_pixel_z
@@ -20,20 +21,18 @@ def zimmer2physical(vol0_zxy: np.ndarray) -> np.ndarray:
     return zxy_in_phyical
 
 
+def zimmer2physical_behavior(frame0_xy: np.ndarray) -> np.ndarray:
+    # Assume no z coordinate
+    xy_in_um = frame0_xy * zimmer_behavior_um_per_pixel_xy
+
+    return xy_in_um
+
+
 def zimmer2leifer(vol0_zxy: np.ndarray) -> np.ndarray:
     """ Target: 1 unit = 84 um"""
-    #
-    # scaler = StandardScaler()
-    # vol0_scaled = scaler.fit_transform(vol0_zxy)
-    # # Reduce z
-    # vol0_scaled[:, 0] /= 3.0
-    # # Reorder dimensions
-    # vol0_scaled = vol0_scaled[:, [2, 1, 0]]
-    # # Somehow their point clouds are much smaller than mine
-    # vol0_scaled /= 5.0
 
     # xy, then z
-    xy_in_um = vol0_zxy[:, [1, 2]] * zimmer_um_per_pixel_xy
+    xy_in_um = vol0_zxy[:, [1, 2]] * zimmer_fluroscence_um_per_pixel_xy
     xy_in_leifer = xy_in_um / leifer_um_per_unit
 
     z_in_um = vol0_zxy[:, [0]] * zimmer_um_per_pixel_z
