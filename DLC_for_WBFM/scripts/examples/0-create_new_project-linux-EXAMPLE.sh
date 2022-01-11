1b78f90928c0a02b548f7a0a4a306bd13cdc7172
Charles-Fieseler-Vienna <charles.fieseler@univie.ac.at>
2022-01-11 13:55:18 +0100

    Update example script for Ulises new data

--- DLC_for_WBFM/scripts/examples/0-create_new_project-linux-EXAMPLE.sh
+++ DLC_for_WBFM/scripts/examples/0-create_new_project-linux-EXAMPLE.sh
@@ -9,9 +9,13 @@ else
 fi
 
 # Hardcoded example locations
+#PROJECT_DIR="/scratch/zimmer/Charles/dlc_stacks/"
+#RED_PATH="/project/neurobiology/zimmer/wbfm/dat/ZIM2051/2021-03-04_16-07-57_worm3_ZIM2051/2021-03-04_16-07-57_worm3_ZIM2051-channel-0-pco_camera1/2021-03-04_16-07-57_worm3_ZIM2051-channel-0-pco_camera1.btf"
+#GREEN_PATH="/project/neurobiology/zimmer/wbfm/dat/ZIM2051/2021-03-04_16-07-57_worm3_ZIM2051/2021-03-04_16-07-57_worm3_ZIM2051-channel-0-pco_camera1/2021-03-04_16-07-57_worm3_ZIM2051-channel-0-pco_camera1.btf"
+
 PROJECT_DIR="/scratch/zimmer/Charles/dlc_stacks/"
-RED_PATH="/project/neurobiology/zimmer/wbfm/dat/ZIM2051/2021-03-04_16-07-57_worm3_ZIM2051/2021-03-04_16-07-57_worm3_ZIM2051-channel-0-pco_camera1/2021-03-04_16-07-57_worm3_ZIM2051-channel-0-pco_camera1.btf"
-GREEN_PATH="/project/neurobiology/zimmer/wbfm/dat/ZIM2051/2021-03-04_16-07-57_worm3_ZIM2051/2021-03-04_16-07-57_worm3_ZIM2051-channel-0-pco_camera1/2021-03-04_16-07-57_worm3_ZIM2051-channel-0-pco_camera1.btf"
+RED_PATH="/scratch/ulises/wbfm/20211217/data/worm9/2021-12-17_18-34-25_worm9-channel-0-pco_camera1/2021-12-17_18-34-25_worm9-channel-0-pco_camera1bigtiff.btf"
+GREEN_PATH="/scratch/ulises/wbfm/20211217/data/worm9/2021-12-17_18-34-25_worm9-channel-1-pco_camera2/2021-12-17_18-34-25_worm9-channel-1-pco_camera2bigtiff.btf"
 
 # Actually run
 COMMAND="scripts/0a-create_new_project.py"
