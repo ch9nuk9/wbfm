809248d68a01611dd6044c18559c1df2dd3d55de
Charles-Fieseler-Vienna <charles.fieseler@univie.ac.at>
2022-01-18 15:22:50 +0100

    Debug accidentally unset dataframe

--- DLC_for_WBFM/utils/external/utils_pandas.py
+++ DLC_for_WBFM/utils/external/utils_pandas.py
@@ -1,5 +1,11 @@
-def dataframe_to_standard_zxy_format(df_tracklets):
-    coords = ['z', 'x', 'y']
+
+
+def dataframe_to_standard_zxy_format(df_tracklets, flip_xy=False):
+    """Currently, flipxy is true when calling from napari"""
+    if not flip_xy:
+        coords = ['z', 'x', 'y']
+    else:
+        coords = ['z', 'y', 'x']
     df_tracklets = df_tracklets.loc(axis=1)[:, coords]
-    df_tracklets.sort_index(axis=1, level=0, sort_remaining=False)
+    df_tracklets = df_tracklets.sort_index(axis=1, level=0, sort_remaining=False)
     return df_tracklets
