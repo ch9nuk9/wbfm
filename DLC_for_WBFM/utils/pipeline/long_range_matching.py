891c2ee9a196eebe10f484c18d34aef32d7f8b3c
Charles-Fieseler-Vienna <charles.fieseler@univie.ac.at>
2022-01-26 14:08:42 +0100

    Do neuron matching using the neuron initialization method

--- DLC_for_WBFM/utils/pipeline/long_range_matching.py
+++ DLC_for_WBFM/utils/pipeline/long_range_matching.py
@@ -100,7 +100,7 @@ def global_track_matches_from_config(project_path, to_save=True, verbose=2, DEBU
         global2tracklet = final_matching.get_mapping_0_to_1(unique=False)
         track_config.pickle_in_local_project(global2tracklet, output_fname, make_sequential_filename=True)
 
-        # Also save raw intermediate products
+        logging.info("Also saving raw intermediate products")
         fname = '3-tracking/global_tracklet_neuron_graph.pickle'
         track_config.pickle_in_local_project(global_tracklet_neuron_graph, fname)
         fname = '3-tracking/worm_obj.pickle'
