8618f137f686bde44ca69ec39190500ef4f04de9
Charles-Fieseler-Vienna <charles.fieseler@univie.ac.at>
2022-02-19 09:30:37 +0100

    New script for resplitting segmentation (postprocessing)

---
+++ DLC_for_WBFM/scripts/alternate/1+resplit_segmentation_in_z.py
@@ -0,0 +1,45 @@
+# main function
+from pathlib import Path
+
+# Experiment tracking
+import sacred
+from sacred import Experiment
+from sacred import SETTINGS
+from sacred.observers import TinyDbObserver
+from DLC_for_WBFM.utils.external.monkeypatch_json import using_monkeypatch
+from segmentation.util.utils_pipeline import resplit_masks_in_z_from_config
+from DLC_for_WBFM.utils.projects.project_config_classes import ModularProjectConfig
+
+from DLC_for_WBFM.utils.projects.utils_project import safe_cd
+
+SETTINGS.CONFIG.READ_ONLY_CONFIG = False
+
+# Initialize sacred experiment
+ex = Experiment()
+ex.add_config(project_path=None, continue_from_frame=None, DEBUG=False)
+
+
+@ex.config
+def cfg(project_path, DEBUG):
+    # Manually load yaml files
+    cfg = ModularProjectConfig(project_path)
+    project_dir = cfg.project_dir
+
+    segment_cfg = cfg.get_segmentation_config()
+
+    if not DEBUG:
+        using_monkeypatch()
+    #     log_dir = cfg.get_log_dir()
+    #     ex.observers.append(TinyDbObserver(log_dir))
+
+@ex.automain
+def main(_config, _run):
+    sacred.commands.print_config(_run)
+
+    segment_cfg = _config['segment_cfg']
+    project_cfg = _config['cfg']
+
+    with safe_cd(_config['project_dir']):
+        resplit_masks_in_z_from_config(segment_cfg, project_cfg,
+                                       continue_from_frame=_config['continue_from_frame'],
+                                       DEBUG=_config['DEBUG'])
