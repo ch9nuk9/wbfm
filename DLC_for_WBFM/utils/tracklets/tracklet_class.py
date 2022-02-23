f6ec5d985eef89decb0260927cfc529f00873770
Charles-Fieseler-Vienna <charles.fieseler@univie.ac.at>
2022-02-23 16:25:43 +0100

    Overwrite click dictionary when splitting tracklets, instead of trying to append

--- DLC_for_WBFM/utils/tracklets/tracklet_class.py
+++ DLC_for_WBFM/utils/tracklets/tracklet_class.py
@@ -210,7 +210,7 @@ class DetectedTrackletsAndNeurons:
         col = self.df_tracklets_zxy[n][subcolumn_to_check].dropna(axis=0)
         idx = col.index
         for t, value in zip(idx, col):
-            self.segmentation_id_to_tracklet_name_database[(t, int(value))].add(n)
+            self.segmentation_id_to_tracklet_name_database[(t, int(value))] = set(n)
 
     @property
     def all_tracklet_names(self):
