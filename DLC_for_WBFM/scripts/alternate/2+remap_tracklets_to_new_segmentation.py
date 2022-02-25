e6aea28e854fcf6db218945e00d1d1de86a83506
Charles-Fieseler-Vienna <83711538+Charles-Fieseler-Vienna@users.noreply.github.com>
2022-02-25 16:23:25 +0100

    New sacred script for remapping tracklets to a new segmentation

---
+++ DLC_for_WBFM/scripts/alternate/2+remap_tracklets_to_new_segmentation.py
@@ -0,0 +1,36 @@
+
+# Experiment tracking
+import sacred
+from sacred import Experiment
+from sacred import SETTINGS
+from sacred.observers import TinyDbObserver
+from DLC_for_WBFM.utils.external.monkeypatch_json import using_monkeypatch
+from DLC_for_WBFM.utils.projects.utils_redo_steps import remap_tracklets_to_new_segmentation_using_config
+
+SETTINGS.CONFIG.READ_ONLY_CONFIG = False
+
+# Initialize sacred experiment
+ex = Experiment()
+ex.add_config(project_path=None, path_to_new_segmentation=None, path_to_new_metadata=None, DEBUG=False)
+
+
+@ex.config
+def cfg(project_path, DEBUG):
+
+    if not DEBUG:
+        using_monkeypatch()
+        # log_dir = cfg.get_log_dir()
+        # ex.observers.append(TinyDbObserver(log_dir))
+
+
+@ex.automain
+def main(_config, _run):
+    sacred.commands.print_config(_run)
+
+    project_path = _config['project_path']
+    path_to_new_segmentation = _config['path_to_new_segmentation']
+    path_to_new_metadata = _config['path_to_new_metadata']
+    DEBUG = _config['DEBUG']
+
+    remap_tracklets_to_new_segmentation_using_config(project_path, path_to_new_segmentation, path_to_new_metadata,
+                                                     DEBUG)
