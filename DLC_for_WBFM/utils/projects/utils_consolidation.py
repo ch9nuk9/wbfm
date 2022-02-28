810f954b4cc5ab6d1e3ab93d8a62436295a1b0cd
Charles-Fieseler-Vienna <charles.fieseler@univie.ac.at>
2022-02-28 15:51:54 +0100

    New function for combining many tracklets to one using manual annotation... for now unused because the performance boost is probably small

---
+++ DLC_for_WBFM/utils/projects/utils_consolidation.py
@@ -0,0 +1,55 @@
+import logging
+
+import pandas as pd
+from tqdm.auto import tqdm
+
+from DLC_for_WBFM.utils.external.utils_pandas import get_names_from_df
+from DLC_for_WBFM.utils.projects.finished_project_data import ProjectData
+
+
+def consolidate_tracklets_using_manual_annotation(project_data: ProjectData):
+    df_all_tracklets = project_data.df_all_tracklets
+    unmatched_tracklet_names = get_names_from_df(df_all_tracklets)
+
+    print(f"Original number of unique tracklets: {len(unmatched_tracklet_names)}")
+
+    track_cfg = project_data.project_config.get_tracking_config()
+    fname = track_cfg.resolve_relative_path("/manual_annotation/manual_tracking.csv", prepend_subfolder=True)
+    df_manual_tracking = pd.read_csv(fname)
+    neurons_that_are_finished = list(df_manual_tracking[df_manual_tracking['Finished?']]['Neuron ID'])
+    print(f"Found {len(neurons_that_are_finished)}/{df_manual_tracking.shape[0]} annotated neurons")
+
+    global2tracklet = project_data.global2tracklet
+    # Build list of new consolidated tracklets
+    consolidated_tracklets = []
+    for neuron in tqdm(neurons_that_are_finished):
+        these_tracklets_names = global2tracklet[neuron]
+        these_tracklets = [df_all_tracklets[n].dropna(axis=0) for n in these_tracklets_names]
+        [unmatched_tracklet_names.remove(n) for n in these_tracklets_names]
+
+        new_tracklet_name = these_tracklets_names[0]
+
+        # Add new name in one line: https://stackoverflow.com/questions/40225683/how-to-simply-add-a-column-level-to-a-pandas-dataframe
+        joined_tracklet = pd.concat(these_tracklets, axis=0)
+        joined_tracklet.columns = pd.MultiIndex.from_product([[new_tracklet_name], joined_tracklet.columns])
+
+        # Check for duplicated indices... shouldn't happen, but humans can do it!
+        idx_duplicated = joined_tracklet.index.duplicated(keep='first')
+        if idx_duplicated.any():
+            logging.warning(
+                f"Found {sum(idx_duplicated)} duplicated indices in neuron {neuron}; keeping first instances")
+            joined_tracklet = joined_tracklet[~idx_duplicated]
+
+        consolidated_tracklets.append(joined_tracklet)
+
+    # Get remaining, unmatched tracklets
+    df_unmatched = df_all_tracklets.loc[:, unmatched_tracklet_names]
+
+    consolidated_tracklets.append(df_unmatched)
+    df_new = pd.concat(consolidated_tracklets, axis=1)
+
+    final_tracklet_names = get_names_from_df(df_new)
+    print(f"Consolidated number of unique tracklets: {len(final_tracklet_names)}")
+
+    output_df_fname = "df_consolidated.pickle"
+    track_cfg.pickle_in_local_project(df_new, output_df_fname=output_df_fname, custom_writer=pd.to_pickle)
