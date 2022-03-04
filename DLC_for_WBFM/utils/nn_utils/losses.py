34f5b4f548db14c0cf8121f6e70991337ed9bc13
Charles-Fieseler-Vienna <charles.fieseler@univie.ac.at>
2022-03-04 09:59:42 +0100

    Add a different source for arcface loss; this one is easier to get working

--- DLC_for_WBFM/utils/nn_utils/losses.py
+++ DLC_for_WBFM/utils/nn_utils/losses.py
@@ -1,5 +1,6 @@
 # NOT From: https://github.com/deepinsight/insightface/blob/cfa54d80c7954264beadf86f402b10822f6f0411/recognition/arcface_torch/losses.py
 # from https://github.com/ronghuaiyang/arcface-pytorch/blob/master/models/metrics.py
+# AND: https://github.com/cvqluu/Angular-Penalty-Softmax-Losses-Pytorch/blob/master/loss_functions.py
 from __future__ import print_function
 from __future__ import division
 import torch
@@ -163,3 +164,68 @@ class SphereProduct(nn.Module):
                + 'in_features=' + str(self.in_features) \
                + ', out_features=' + str(self.out_features) \
                + ', m=' + str(self.m) + ')'
+
+
+class AngularPenaltySMLoss(nn.Module):
+
+    def __init__(self, in_features, out_features, loss_type='arcface', eps=1e-7, s=None, m=None):
+        '''
+        Angular Penalty Softmax Loss
+
+        Three 'loss_types' available: ['arcface', 'sphereface', 'cosface']
+        These losses are described in the following papers:
+
+        ArcFace: https://arxiv.org/abs/1801.07698
+        SphereFace: https://arxiv.org/abs/1704.08063
+        CosFace/Ad Margin: https://arxiv.org/abs/1801.05599
+
+        '''
+        super(AngularPenaltySMLoss, self).__init__()
+        loss_type = loss_type.lower()
+        assert loss_type in ['arcface', 'sphereface', 'cosface']
+        if loss_type == 'arcface':
+            self.s = 64.0 if not s else s
+            self.m = 0.5 if not m else m
+        if loss_type == 'sphereface':
+            self.s = 64.0 if not s else s
+            self.m = 1.35 if not m else m
+        if loss_type == 'cosface':
+            self.s = 30.0 if not s else s
+            self.m = 0.4 if not m else m
+        self.loss_type = loss_type
+        self.in_features = in_features
+        self.out_features = out_features
+        self.fc = nn.Linear(in_features, out_features, bias=False)
+        self.eps = eps
+
+    def predict(self, x):
+        for W in self.fc.parameters():
+            W = F.normalize(W, p=2, dim=1)
+        x = F.normalize(x, p=2, dim=1)
+        wf = self.fc(x)
+        return wf
+
+    def forward(self, x, labels):
+        '''
+        input shape (N, in_features)
+        '''
+        assert len(x) == len(labels)
+        assert torch.min(labels) >= 0
+        assert torch.max(labels) < self.out_features
+
+        wf = self.predict(x)
+
+        theta_i = torch.diagonal(wf.transpose(0, 1)[labels])
+        if self.loss_type == 'cosface':
+            numerator = self.s * (theta_i - self.m)
+        elif self.loss_type == 'arcface':
+            numerator = self.s * torch.cos(torch.acos(torch.clamp(theta_i, -1. + self.eps, 1 - self.eps)) + self.m)
+        elif self.loss_type == 'sphereface':
+            numerator = self.s * torch.cos(self.m * torch.acos(torch.clamp(theta_i, -1. + self.eps, 1 - self.eps)))
+
+        theta_not_i = torch.cat([torch.cat((wf[i, :y], wf[i, y + 1:])).unsqueeze(0) for i, y in enumerate(labels)], dim=0)
+        denominator = torch.exp(numerator) + torch.sum(torch.exp(self.s * theta_not_i), dim=1)
+        # Note: numerator is actually log(numerator) as given in eq 3 of the paper:
+        # https://arxiv.org/pdf/1801.07698.pdf
+        L = numerator - torch.log(denominator)
+        return -torch.mean(L)
