aa545a388c67a87c6bd92fe5706d3d204a3a1921
Charles-Fieseler-Vienna <charles.fieseler@univie.ac.at>
2022-04-14 12:29:32 +0200

    Draft of new unit tests

---
+++ DLC_for_WBFM/tests/test_track_tracklet_matching.py
@@ -0,0 +1,18 @@
+import unittest
+
+import numpy as np
+import pandas as pd
+
+class TestTrackTrackletMatching(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.neuron_name = 'neuron_001'
+        self.tracklet_names = ['tracklet_0000000', 'tracklet_0000001']
+
+        tmp_df = pd.DataFrame({(self.neuron_name, 'z'): list(range(20)),
+                              (self.neuron_name, 'x'): 10*np.array(list(range(20)))})
+        self.df_tracks = tmp_df
+
+        tmp_df = pd.DataFrame({(self.neuron_name, 'z'): list(range(20)),
+                              (self.neuron_name, 'x'): 10*np.array(list(range(20)))})
+        self.df_tracks = tmp_df
--- DLC_for_WBFM/tests/test_tracklet_splitting.py
+++ DLC_for_WBFM/tests/test_tracklet_splitting.py
@@ -37,3 +37,25 @@ class TestTrackletSplitting(unittest.TestCase):
         df_split_sparse = to_sparse_multiindex(df_split)
         self.assertTrue(check_if_fully_sparse(df_split_sparse))
 
+
+    def test_split_out_of_order(self):
+        split_list_dict = {self.neuron_name: [15, 10]}
+        df_split, all_new_tracklets = split_all_tracklets_at_once(self.df, split_list_dict, name_mode='neuron')
+
+        expected_names = ['neuron_002', 'neuron_003', 'neuron_004']
+        split_names = get_names_from_df(df_split)
+        self.assertEqual(expected_names, split_names)
+
+        # Two columns per neuron
+        expected_lens = [10, 10, 5, 5, 5, 5]
+        split_lens = list(df_split.count())
+        self.assertEqual(expected_lens, split_lens)
+
+        # Specific indices
+        expected_indices = [list(range(10)), list(range(10, 15)), list(range(15, 20))]
+        split_indices = [list(t.dropna(axis=0).index) for t in all_new_tracklets]
+        self.assertEqual(expected_indices, split_indices)
+
+        # Test that they are sparse
+        df_split_sparse = to_sparse_multiindex(df_split)
+        self.assertTrue(check_if_fully_sparse(df_split_sparse))
