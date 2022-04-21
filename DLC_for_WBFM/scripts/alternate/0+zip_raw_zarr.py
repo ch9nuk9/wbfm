1f1854b818809d4741133654a29f7f87cf2bca0f
Charles-Fieseler-Vienna <charles.fieseler@univie.ac.at>
2022-04-21 11:05:16 +0200

    New sacred script for zipping

---
+++ DLC_for_WBFM/scripts/alternate/0+zip_raw_zarr.py
@@ -0,0 +1,50 @@
+"""
+"""
+
+# main function
+import os
+import subprocess
+from pathlib import Path
+
+# Experiment tracking
+import sacred
+
+from DLC_for_WBFM.utils.external.utils_zarr import zip_raw_data_zarr
+from DLC_for_WBFM.utils.general.preprocessing.bounding_boxes import calculate_bounding_boxes_from_fnames
+from sacred import Experiment
+from sacred import SETTINGS
+from sacred.observers import TinyDbObserver
+from DLC_for_WBFM.utils.external.monkeypatch_json import using_monkeypatch
+from DLC_for_WBFM.utils.general.preprocessing.utils_preprocessing import zip_zarr_using_config
+
+from DLC_for_WBFM.utils.projects.utils_data_subsets import write_data_subset_from_config
+from DLC_for_WBFM.utils.projects.project_config_classes import ModularProjectConfig
+from DLC_for_WBFM.utils.projects.utils_project import safe_cd
+import cgitb
+cgitb.enable(format='text')
+
+SETTINGS.CONFIG.READ_ONLY_CONFIG = False
+
+# Initialize sacred experiment
+
+ex = Experiment()
+ex.add_config(project_path=None, DEBUG=False)
+
+
+@ex.config
+def cfg(project_path, DEBUG):
+    # Manually load yaml files
+    cfg = ModularProjectConfig(project_path)
+
+    if not DEBUG:
+        using_monkeypatch()
+
+
+@ex.automain
+def main(_config, _run):
+    sacred.commands.print_config(_run)
+
+    zip_zarr_using_config(_config['cfg'])
+
+    print("Finished.")
+
