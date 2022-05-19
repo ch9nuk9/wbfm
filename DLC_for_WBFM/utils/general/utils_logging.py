bc801d7a626cfb607fec8e249c019f1c75b5f76e
Charles-Fieseler-Vienna <charles.fieseler@univie.ac.at>
2022-05-19 16:20:44 +0200

    Don't duplicate logging when importing multiple projects

--- DLC_for_WBFM/utils/general/utils_logging.py
+++ DLC_for_WBFM/utils/general/utils_logging.py
@@ -9,23 +9,28 @@ def setup_logger_object(log_filename):
     # Log to a file and the console
     # https://docs.python.org/3/howto/logging-cookbook.html
     log_name = Path(log_filename).stem
-    # Set up file handler
-    log_filename = get_sequential_filename(log_filename)
-    print(f"Setting up log at: {log_filename}")
-    fh = logging.FileHandler(log_filename)
-    fh.setLevel(logging.DEBUG)
-    formatter = logging.Formatter(fmt='%(asctime)s %(name)-12s %(levelname)-8s %(message)s', datefmt='%m-%d %H:%M')
-    fh.setFormatter(formatter)
 
     # Main log object
     logger = logging.getLogger(f'{log_name}')
     logger.setLevel(logging.DEBUG)
 
-    ch = _get_console_handler()
+    if len(logger.handlers) >= 2:
+        # Assume it has already been set up
+        pass
+    else:
+        # Set up file handler
+        log_filename = get_sequential_filename(log_filename)
+        print(f"Setting up log at: {log_filename}")
+        fh = logging.FileHandler(log_filename)
+        fh.setLevel(logging.DEBUG)
+        formatter = logging.Formatter(fmt='%(asctime)s %(name)-12s %(levelname)-8s %(message)s', datefmt='%m-%d %H:%M')
+        fh.setFormatter(formatter)
 
-    # Actually add the handlers
-    logger.addHandler(ch)
-    logger.addHandler(fh)
+        ch = _get_console_handler()
+
+        # Actually add the handlers
+        logger.addHandler(ch)
+        logger.addHandler(fh)
     logger.propagate = False
 
     logger.info(f"Set up logger with name: {log_name}")
