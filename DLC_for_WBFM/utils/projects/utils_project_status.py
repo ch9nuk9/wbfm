50b08055f4978536a6c21a86caa8d94f1271d137
Charles-Fieseler-Vienna <charles.fieseler@univie.ac.at>
2022-05-23 16:17:03 +0200

    Fix off by one

--- DLC_for_WBFM/utils/projects/utils_project_status.py
+++ DLC_for_WBFM/utils/projects/utils_project_status.py
@@ -180,7 +180,7 @@ def print_project_status(project_config: ModularProjectConfig):
     for i_step in tqdm([1, 2, 3, 4]):
         passed = check_all_needed_data_for_step(step_index=i_step, **opt)
         if not passed:
-            project_config.logger.info(f"Next pipeline step required: {i_step}")
+            project_config.logger.info(f"Next pipeline step required: {i_step-1}")
             break
     else:
         project_config.logger.info("All steps of project are complete; manual annotation can begin")
