94cf0e1b5baabfeae2c3e32c072c05cf659ff00a
Charles-Fieseler-Vienna <charles.fieseler@univie.ac.at>
2022-05-23 14:36:08 +0200

    New scripts for using snakemake on the cluster

---
+++ DLC_for_WBFM/new_project_defaults/snakemake/RUNME_cluster.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Example from: https://hackmd.io/@bluegenes/BJPrrj7WB
+
+OPT="sbatch -t {cluster.time} -p {cluster.partition} --cpus-per-task {cluster.cpus_per_task} --mem {cluster.mem} --output {cluster.output}"
+NUM_JOBS_TO_SUBMIT=2
+
+snakemake -s full_pipeline.smk --latency-wait 60 --cluster "$OPT" --cluster-config cluster_config.yaml --jobs $NUM_JOBS_TO_SUBMIT
