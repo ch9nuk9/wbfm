76f191fb41f62789e4e4138e9a1c85b27b198634
Charles-Fieseler-Vienna <charles.fieseler@univie.ac.at>
2022-07-07 11:38:14 +0200

    Update url

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='wbfm',
       version='1.0.0',
       description='Whole Brain Freely Moving neuron tracking and analysis',
-      url='https://github.com/Zimmer-lab/dlc_for_wbfm',
+      url='https://github.com/Zimmer-lab/wbfm',
       author='Charles Fieseler',
       author_email='charles.fieseler@gmail.com',
       license='MIT',
