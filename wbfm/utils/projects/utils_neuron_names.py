8d42cccd211d30dadc90778032ab17c80ffd9220
Charles-Fieseler-Vienna <charles.fieseler@univie.ac.at>
2022-07-07 11:37:24 +0200

    Update type annotations

--- wbfm/utils/projects/utils_neuron_names.py
+++ wbfm/utils/projects/utils_neuron_names.py
@@ -1,24 +1,34 @@
 
 
-def int2name_neuron(i_neuron: int):
+def int2name_neuron(i_neuron: int) -> str:
     assert 0 < i_neuron <= 1000, f"Value {i_neuron} not supported"
     return f"neuron_{i_neuron:03d}"
 
 
-def int2name_tracklet(i_tracklet: int):
+def int2name_tracklet(i_tracklet: int) -> str:
+    """
+
+    Parameters
+    ----------
+    i_tracklet
+
+    Returns
+    -------
+
+    """
     assert 0 <= i_tracklet <= 1000000, f"Value {i_tracklet} not supported"
     return f"tracklet_{i_tracklet:07d}"
 
 
-def name2int_neuron_and_tracklet(name_neuron):
+def name2int_neuron_and_tracklet(name_neuron: str) -> int:
     return int(name_neuron.split('_')[1])
 
 
-def int2name_deprecated(i_neuron: int):
+def int2name_deprecated(i_neuron: int) -> str:
     return f"neuron{i_neuron}"
 
 
-def int2name_using_mode(i_neuron: int, mode: str):
+def int2name_using_mode(i_neuron: int, mode: str) -> str:
     if mode == 'neuron':
         return int2name_neuron(i_neuron)
     elif mode == 'tracklet':
@@ -27,5 +37,5 @@ def int2name_using_mode(i_neuron: int, mode: str):
         raise NotImplementedError
 
 
-def int2name_dummy(i):
+def int2name_dummy(i) -> str:
     return f"zzz_{i:03d}"
