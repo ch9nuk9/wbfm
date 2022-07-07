e23100f7fbcc3f6b4559b5cb4760401fc7cff658
Charles-Fieseler-Vienna <charles.fieseler@univie.ac.at>
2022-07-07 10:05:46 +0200

    Update setup.py and increase version

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
-setup(name='dlc_for_wbfm',
-      version='0.5.0',
-      description='Using DeepLabCut for Whole Brain Freely Moving neuron tracking',
-      url='https://bitbucket.vbc.ac.at/projects/ZL/repos/dlc_for_wbfm/browse',
+setup(name='wbfm',
+      version='1.0.0',
+      description='Whole Brain Freely Moving neuron tracking and analysis',
+      url='https://github.com/Zimmer-lab/dlc_for_wbfm',
       author='Charles Fieseler',
       author_email='charles.fieseler@gmail.com',
       license='MIT',
