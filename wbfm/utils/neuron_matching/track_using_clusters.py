300e34e1484b3ed1239f650cc5d68d4d18f0564c
Charles-Fieseler-Vienna <charles.fieseler@univie.ac.at>
2022-08-05 16:20:01 +0200

    Allow cluster class to work with non-adjacent time points

--- wbfm/utils/neuron_matching/track_using_clusters.py
+++ wbfm/utils/neuron_matching/track_using_clusters.py
@@ -10,7 +10,7 @@ from hdbscan import HDBSCAN
 
 from wbfm.utils.external.utils_pandas import fill_missing_indices_with_nan
 from wbfm.utils.neuron_matching.utils_candidate_matches import rename_columns_using_matching, \
-    combine_dataframes_using_mode
+    combine_dataframes_using_mode, combine_dataframes_using_bipartite_matching
 
 import matplotlib
 
@@ -87,7 +87,7 @@ class WormTsneTracker:
 
     @staticmethod
     def load_from_config(project_config, svd_components=50):
-        project_data = ProjectData.load_final_project_data_from_config(project_config)
+        project_data = ProjectData.load_final_project_data_from_config(project_config, to_load_frames=True)
 
         # Use old tracker just as a feature-space embedder
         frames_old = project_data.raw_frames
@@ -124,7 +124,7 @@ class WormTsneTracker:
         all_start_volumes.append(self.num_frames - self.n_volumes_per_window - 1)
         return all_start_volumes
 
-    def cluster_obj2dataframe(self, db_svd, start_volume):
+    def cluster_obj2dataframe(self, db_svd, start_volume, vol_ind: list=None):
         # Associate cluster label ids to a (time, local ind) tuple
         # i.e. build a dict
         # Note: the dict key should be a tuple of (neuron_name, 'raw_neuron_ind_in_list'), because we want it to be a multilevel dataframe
@@ -134,7 +134,27 @@ class WormTsneTracker:
 
         cluster_dict = {}
         # Assume the labels are in sequential order
-        current_time = start_volume
+        i_current_time = 0
+        if vol_ind is None:
+            current_time = start_volume
+
+            def get_next_time(_i_current_time, _current_time):
+                return _i_current_time + 1, _current_time + 1
+
+            def get_empty_col():
+                tmp = np.empty(n_vols + start_volume)
+                tmp[:] = np.nan
+                return tmp
+        else:
+            def get_next_time(_i_current_time, _tmp):
+                return _i_current_time + 1, vol_ind[_i_current_time + 1]
+            i_current_time, current_time = get_next_time(i_current_time, None)
+
+            def get_empty_col():
+                tmp = np.empty(np.max(vol_ind))
+                tmp[:] = np.nan
+                return tmp
+
         current_global_ind = list(linear_ind_to_local[current_time].copy())
         current_local_ind = 0
 
@@ -149,9 +169,7 @@ class WormTsneTracker:
                 key = (this_neuron_name, 'raw_neuron_ind_in_list')
 
                 if key not in cluster_dict:
-                    tmp = np.empty(n_vols + start_volume)
-                    tmp[:] = np.nan
-                    cluster_dict[key] = tmp
+                    cluster_dict[key] = get_empty_col()
 
                 if np.isnan(cluster_dict[key][current_time]):
                     # This is a numpy array
@@ -162,7 +180,8 @@ class WormTsneTracker:
                     # print(f"Multiple assignments found for {this_neuron_name} at t={current_time}")
 
             if len(current_global_ind) == 0:
-                current_time += 1
+                i_current_time, current_time = get_next_time(i_current_time, current_time)
+                # current_time += 1
                 current_global_ind = list(linear_ind_to_local[current_time].copy())
                 current_local_ind = 0
             else:
@@ -170,23 +189,24 @@ class WormTsneTracker:
         df_cluster = pd.DataFrame(cluster_dict)
         return df_cluster
 
-    def cluster_single_window(self, start_volume=0):
+    def cluster_single_window(self, start_volume=0, vol_ind=None):
         # Unpack
-        X_svd = self.X_svd
         linear_ind_to_local = self.linear_ind_to_local
-        n_vols = self.n_volumes_per_window
 
         # Options
         opt_tsne = self.opt_tsne
         opt_db = self.opt_db
 
         # Get this window of data
-        vol_ind = np.arange(start_volume, start_volume + n_vols)
+        if vol_ind is None:
+            n_vols = self.n_volumes_per_window
+            vol_ind = np.arange(start_volume, start_volume + n_vols)
         linear_ind = np.hstack([linear_ind_to_local[i] for i in vol_ind])
+        X = self.X_svd[linear_ind, :]
 
         # tsne + cluster
         tsne = TSNE(**opt_tsne)
-        Y_tsne_svd = tsne.fit_transform(X_svd[linear_ind, :])
+        Y_tsne_svd = tsne.fit_transform(X)
         db_svd = HDBSCAN(**opt_db).fit(Y_tsne_svd)
 
         return db_svd, Y_tsne_svd
@@ -215,21 +235,29 @@ class WormTsneTracker:
             all_tsnes.append(Y_tsne_svd)  # TODO: check kl divergence of tsne?
 
         # Choose a base dataframe and rename all to that one
+        # For now, combine as we go so that the matching gets the benefit of any overlaps (but is slower)
         # TODO: for now just choosing the one with the most neurons
-        i_most = np.argmax([df.shape[1] for df in all_raw_dfs])
-        df_base = all_raw_dfs[i_most]
-        all_dfs = [df_base]
-        for i, df in enumerate(all_raw_dfs):
-            if i == i_most:
-                continue
-            df_renamed, *_ = rename_columns_using_matching(df_base, df, try_to_fix_inf=True)
-            all_dfs.append(df_renamed)
+        # i_most = np.argmax([df.shape[1] for df in all_raw_dfs])
+        # df_base = all_raw_dfs[i_most]
+        # all_dfs = [df_base]
+        # for i, df in enumerate(all_raw_dfs):
+        #     if i == i_most:
+        #         continue
+        #     df_renamed, *_ = rename_columns_using_matching(df_base, df, try_to_fix_inf=True)
+        #     all_dfs.append(df_renamed)
+        df_previous = all_raw_dfs[0]
+        for df in all_raw_dfs[1:]:
+            df_renamed, *_ = rename_columns_using_matching(df_previous, df, try_to_fix_inf=True)
+            # df_next = combine_dataframes_using_mode([df_previous, df_renamed])
+            df_next = combine_dataframes_using_bipartite_matching([df_previous, df_renamed])
+            df_previous = df_next
+        df_combined = df_previous
 
         # Combine to one dataframe
-        if len(all_dfs) > 1:
-            df_combined = combine_dataframes_using_mode(all_dfs)
-        else:
-            df_combined = all_dfs[0]
+        # if len(all_dfs) > 1:
+        #     df_combined = combine_dataframes_using_mode(all_dfs)
+        # else:
+        #     df_combined = all_dfs[0]
 
         return df_combined, all_raw_dfs
 
