4d487b9bc29c545d1e16a0c0705f16a585be7e08
Charles-Fieseler-Vienna <charles.fieseler@univie.ac.at>
2022-08-25 10:15:29 +0200

    New test scripts for calculation on the gpu

---
+++ wbfm/scripts/examples/check_gpu_calculation.py
@@ -0,0 +1,15 @@
+import torch
+
+
+if __name__ == "__main__":
+
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    print(f"Cuda device is found: {device}")
+
+    sz = (10000, 10000)
+    X = torch.rand(*sz, device=device)
+
+    Y = torch.matmul(X.T, X)
+
+    print("Mean value: ", Y.mean())
+    print("Finished calculations")
