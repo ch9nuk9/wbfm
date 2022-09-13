49260f4b64a38d56cf52248dfa616eedcaad1a8d
Charles-Fieseler-Vienna <charles.fieseler@univie.ac.at>
2022-09-13 10:51:00 +0200

    Reduce number of plots generated

--- wbfm/utils/visualization/plot_traces.py
+++ wbfm/utils/visualization/plot_traces.py
@@ -66,8 +66,8 @@ def make_grid_plot_using_project(project_data: ProjectData,
         # Also try to remove outliers and filter
         all_modes = ['ratio', 'dr_over_r_20', 'linear_model']
         opt['remove_outliers'] = True
-        for mode in all_modes:
-            make_grid_plot_using_project(channel_mode=mode, **opt)
+        # for mode in all_modes:
+        #     make_grid_plot_using_project(channel_mode=mode, **opt)
         opt['filter_mode'] = 'rolling_mean'
         for mode in all_modes:
             make_grid_plot_using_project(channel_mode=mode, **opt)
