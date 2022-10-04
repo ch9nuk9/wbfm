2eb6a0c3f6cfb0310909ba4fea8ce5a50c9bd10d
Charles-Fieseler-Vienna <charles.fieseler@univie.ac.at>
2022-10-04 16:01:33 +0200

    Add info

--- wbfm/scripts/cluster/multi_step_dispatcher.sh
+++ wbfm/scripts/cluster/multi_step_dispatcher.sh
@@ -7,11 +7,13 @@ while getopts t:n:s: flag
 do
     case "${flag}" in
         t) project_path=${OPTARG};;
-        n) is_dry_run=${OPTARG};;
         s) step_reference_array+=("$OPTARG");;
         *) raise error "Unknown flag"
     esac
 done
+
+NUM_JOBS=$#-1
+echo "Found $NUM_JOBS jobs"
 
 for step_reference in ${step_reference_array[@]}; do
   sbatch --wait single_step_dispatcher.sbatch -s "$step_reference" -t "$project_path"
