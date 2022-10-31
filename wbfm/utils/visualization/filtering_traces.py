a8c4de21be9cc30ec567250427f2622f92349afb
Charles-Fieseler-Vienna <charles.fieseler@univie.ac.at>
2022-10-31 14:39:36 +0100

    Small refactor

--- wbfm/utils/visualization/filtering_traces.py
+++ wbfm/utils/visualization/filtering_traces.py
@@ -55,22 +55,28 @@ def filter_linear_interpolation(y: pd.DataFrame, window=15):
     return y.interpolate(method='linear', limit=window, limit_direction='both')
 
 
+def _calc_background_subtracted_trace(df_tmp, i, background_per_pixel):
+    try:
+        y_raw = df_tmp[i]['intensity_image']
+        vol = df_tmp[i]['area']
+        y = y_raw - background_per_pixel * vol
+    except KeyError:
+        # Then we just have a single level
+        y = df_tmp[i]
+    return y
+
+
 def trace_from_dataframe_factory(calculation_mode, background_per_pixel, bleach_correct) -> callable:
     # Way to process a single dataframe
     if calculation_mode == 'integration':
         def calc_single_trace(i, df_tmp) -> pd.Series:
-            try:
-                y_raw = df_tmp[i]['intensity_image']
-                vol = df_tmp[i]['area']
-            except KeyError:
-                y_raw = df_tmp[i]['brightness']
-                vol = df_tmp[i]['volume']
-            y = y_raw - background_per_pixel * vol
+            y = _calc_background_subtracted_trace(df_tmp, i, background_per_pixel)
             if bleach_correct:
                 y = pd.Series(detrend_exponential_lmfit(y)[0])
             if any(y < 0):
                 logging.warning(f"Found negative trace value; check background_per_pixel value ({background_per_pixel})")
             return y
+
     # elif calculation_mode == 'max':
     #     def calc_single_trace(i, df_tmp):
     #         y_raw = df_tmp[i]['all_values']
