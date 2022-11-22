b59488912d80fed01ac333d473752dbd48111d15
Charles-Fieseler-Vienna <charles.fieseler@univie.ac.at>
2022-11-22 11:05:45 +0100

    New script for running multiple snakemakes within tmux (wip)

--- wbfm/scripts/cluster/run_snakemake_for_all_in_folder.sh
+++ wbfm/scripts/cluster/run_snakemake_for_all_in_folder.sh
@@ -1,8 +1,4 @@
-#!/usr/bin/env bash
-# Example usage:
-# bash apply_step_to_all_in_folder.sh -t '/scratch/neurobiology/zimmer/Charles/dlc_stacks/exposure_12ms' -s 0b
-# Found within the package in:
-# wbfm/scripts/cluster
+#!/bin/bash
 
 # Get all user flags
 while getopts t:n:s: flag
@@ -10,12 +6,12 @@ do
     case "${flag}" in
         t) folder_of_projects=${OPTARG};;
         n) is_dry_run=${OPTARG};;
-        s) step_reference=${OPTARG};;
         *) raise error "Unknown flag"
     esac
 done
 
 # Loop through the parent folder, then try to get the config file within each of these parent folders
+i_tmux=0
 for f in "$folder_of_projects"/*; do
     if [ -d "$f" ] && [ ! -L "$f" ]; then
         echo "Checking folder: $f"
@@ -23,10 +19,14 @@ for f in "$folder_of_projects"/*; do
         for f_config in "$f"/*; do
             if [ -f "$f_config" ] && [ "${f_config##*/}" = "project_config.yaml" ]; then
                 if [ "$is_dry_run" ]; then
-                    echo "DRYRUN: Dispatching on config file: $f_config"
+                    # Run the snakemake dryrun
+                    tmux_name="worm$i_tmux"
+                    echo "DRYRUN: Dispatching on config file: $f_config with tmux name $tmux_name"
                 else
-                    sbatch ./single_step_dispatcher.sbatch -s "$step_reference" -t "$f_config"
+                    # Get the snakemake command and run it
+                    echo "TODO"
                 fi
+                i_tmux+=1
             fi
         done
     fi
