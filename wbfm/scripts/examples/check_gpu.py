084e974edd0a0626ca24e3d934ab4f1360b73f7d
Charles-Fieseler-Vienna <charles.fieseler@univie.ac.at>
2022-11-25 15:02:28 +0100

    Add more gpu printing

--- wbfm/scripts/examples/check_gpu.py
+++ wbfm/scripts/examples/check_gpu.py
@@ -4,4 +4,5 @@ import torch
 
 if __name__ == "__main__":
     print(f"Cuda is found by pytorch: {torch.cuda.is_available()}")
-    print(f"Cuda is found by tensorflow: {tf.test.is_gpu_available()}")
+    print(f"Cuda is found by tensorflow: {tf.config.list_physical_devices('GPU')}")
+    print(f"Cuda is built with tensorflow: {tf.test.is_built_with_cuda()}")
