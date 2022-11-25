200c7c567cb4b534e336fed3bca7740821baee69
Charles-Fieseler-Vienna <charles.fieseler@univie.ac.at>
2022-11-25 14:57:16 +0100

    Add test to gpu

--- wbfm/scripts/examples/check_gpu.py
+++ wbfm/scripts/examples/check_gpu.py
@@ -1,5 +1,7 @@
+import tensorflow as tf
 import torch
 
 
 if __name__ == "__main__":
-    print(f"Cuda is found: {torch.cuda.is_available()}")
+    print(f"Cuda is found by pytorch: {torch.cuda.is_available()}")
+    print(f"Cuda is found by tensorflow: {tf.test.is_gpu_available()}")
