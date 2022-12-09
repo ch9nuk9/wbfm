e45778464fa47f2eed2b7b3bdaa5a74f71e4db61
Charles-Fieseler-Vienna <charles.fieseler@univie.ac.at>
2022-12-09 15:14:43 +0100

    New file for loading projects that we will use for the paper (stub for now)

--- wbfm/utils/visualization/load_all_projects.py
+++ wbfm/utils/visualization/load_all_projects.py
@@ -247,6 +247,12 @@ all_projects_spacer_2per = load_all_projects_in_folder(foldername)
 foldername = "/scratch/neurobiology/zimmer/Charles/dlc_stacks/2022-11-27_spacer_7b_2per_agar"
 all_projects_spacer_2per_round2 = load_all_projects_in_folder(foldername)
 
+foldername = "/scratch/neurobiology/zimmer/Charles/dlc_stacks/2022-11-30_spacer_7b_2per_agar"
+all_projects_spacer_2per_round3 = load_all_projects_in_folder(foldername)
+
+foldername = "/scratch/neurobiology/zimmer/Charles/dlc_stacks/2022-12_05_spacer_7b_2per_agar"
+all_projects_spacer_2per_round4 = load_all_projects_in_folder(foldername)
+
 ##
 foldername = "/scratch/neurobiology/zimmer/Charles/dlc_stacks/2022-11-23_spacer_on_food"
 all_projects_spacer_food = load_all_projects_in_folder(foldername)
@@ -255,5 +261,3 @@ foldername = "/scratch/neurobiology/zimmer/Charles/dlc_stacks/2022-11-27_spacer_
 all_projects_spacer_food_round2 = load_all_projects_in_folder(foldername)
 
 
-foldername = "/scratch/neurobiology/zimmer/Charles/dlc_stacks/2022-11-30_spacer_7b_2per_agar"
-all_projects_spacer_2per_round3 = load_all_projects_in_folder(foldername)
---
+++ wbfm/utils/visualization/load_final_projects.py
@@ -0,0 +1,35 @@
+
+
+
+##
+# "Final" set of good datasets
+##
+
+
+def get_summary_visualization_dir():
+    """
+    Directory to save overall files, e.g. from anything using load_good_datasets
+
+    Returns
+    -------
+
+    """
+
+    return "/scratch/neurobiology/zimmer/Charles/multiproject_visualizations"
+
+
+def load_good_datasets():
+    """
+    As of Dec 2022, these are the datasets we will use, with this condition:
+        gcamp7b
+        spacer
+        2 percent agar
+
+    Returns
+    -------
+
+    """
+
+    good_projects = []
+
+    return good_projects
