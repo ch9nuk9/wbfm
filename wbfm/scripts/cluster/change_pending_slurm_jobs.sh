b44658300f459dd3b598fa8094816d6a6984cc8e
Charles-Fieseler-Vienna <charles.fieseler@univie.ac.at>
2022-12-12 14:32:06 +0100

    New bash script

---
+++ wbfm/scripts/cluster/change_pending_slurm_jobs.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Designed to change all slurm jobs to the himem partition, which is often empty when the basic partition is full
+
+# Get all slurm IDs from my user
+readarray -t MY_IDS < <(squeue -u fieseler -h -o '%i')
+
+# Define new slurm settings
+NEW_SETTINGS="MinMemoryNode=600000 partition=himem"
+
+# Loop through jobs and update each
+
+for ID in ${MY_IDS[@]}; do
+  scontrol update jobid="$ID" "$NEW_SETTINGS"
+done
