8871fef2a22fb81edebbd0557b8599465407d101
Charles-Fieseler-Vienna <charles.fieseler@univie.ac.at>
2022-12-12 14:47:56 +0100

    Remove unused variable

--- wbfm/scripts/cluster/change_pending_slurm_jobs.sh
+++ wbfm/scripts/cluster/change_pending_slurm_jobs.sh
@@ -5,12 +5,8 @@
 # Get all slurm IDs from my user
 readarray -t MY_IDS < <(squeue -u fieseler -h -o '%i')
 
-# Define new slurm settings
-NEW_SETTINGS="MinMemoryNode=600000 partition=himem"
-
 # Loop through jobs and update each
-
 for ID in ${MY_IDS[@]}; do
-  echo "Running: scontrol update jobid=$ID $NEW_SETTINGS"
+  echo "Running: scontrol update jobid=$ID MinMemoryNode=600000 partition=himem"
   scontrol update jobid=$ID MinMemoryNode=600000 partition=himem
 done
