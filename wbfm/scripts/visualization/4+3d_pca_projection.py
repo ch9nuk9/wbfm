2dc26df7db5ce6709ec18774e915e202eb42a784
Charles-Fieseler-Vienna <charles.fieseler@univie.ac.at>
2022-12-15 15:06:02 +0100

    Change plot

--- wbfm/scripts/visualization/4+3d_pca_projection.py
+++ wbfm/scripts/visualization/4+3d_pca_projection.py
@@ -6,19 +6,17 @@ from pathlib import Path
 
 # Experiment tracking
 import sacred
+from matplotlib import pyplot as plt
 from sacred import Experiment
 from sacred.observers import TinyDbObserver
 
 # main function
 from wbfm.utils.projects.finished_project_data import ProjectData, plot_pca_projection_3d_from_project
-from wbfm.utils.projects.utils_project import safe_cd
-from wbfm.utils.visualization.napari_from_project_data_class import take_screenshot_using_project
-from wbfm.utils.visualization.plot_traces import make_grid_plot_using_project
 
 # Initialize sacred experiment
 ex = Experiment(save_git_info=False)
 # Add single variable so that the cfg() function works
-ex.add_config(project_path=None)
+ex.add_config(project_path=None, t_end=None)
 
 
 @ex.config
@@ -31,4 +29,5 @@ def main(_config, _run):
     sacred.commands.print_config(_run)
 
     project_data = ProjectData.load_final_project_data_from_config(_config['project_path'])
-    plot_pca_projection_3d_from_project(project_data)
+    plot_pca_projection_3d_from_project(project_data, trace_kwargs={'filter_mode': 'rolling_mean'})
+    plt.show()
