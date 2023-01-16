aab3e38a2a1ca0361bda7abadc04891cb43c44ab
Charles-Fieseler-Vienna <charles.fieseler@univie.ac.at>
2023-01-16 13:58:17 +0100

    Finish unit test

--- wbfm/tests/test_worm_posture_class.py
+++ wbfm/tests/test_worm_posture_class.py
@@ -9,7 +9,7 @@ class TestWormPostureClass(unittest.TestCase):
 
     def setUp(self):
         # Example project
-        fname = "/scratch/neurobiology/zimmer/Charles/dlc_stacks/2022-11-27_spacer_7b_2per_agar/ZIM2165_Gcamp7b_worm1-2022_11_28/project_config.yaml"
+        fname = "/scratch/neurobiology/zimmer/Charles/dlc_stacks/2022-11-23_spacer_7b_2per_agar/2022-11-23_worm8/project_config.yaml"
         project_data = ProjectData.load_final_project_data_from_config(fname)
         self.project_data = project_data
         self.worm = project_data.worm_posture_class
@@ -31,3 +31,28 @@ class TestWormPostureClass(unittest.TestCase):
 
         for p in properties:
             self.assertTrue(p(fluorescence_fps=True) is not None)
+
+    def test_speeds(self):
+        properties = [self.worm.worm_speed, self.worm.worm_angular_velocity]
+
+        for p in properties:
+            self.assertTrue(p(fluorescence_fps=False) is not None)
+            self.assertTrue(p(fluorescence_fps=True) is not None)
+
+    def test_speed_special_args(self):
+        # print(self.worm.beh_annotation(fluorescence_fps=True) == 1)
+        k0 = "signed"
+        kwarg0 = [False, True]
+        k1 = "subsample_before_derivative"
+        kwarg1 = [False, True]
+        k2 = "strong_smoothing"
+        kwarg2 = [False, True]
+        k3 = "signed"
+        kwarg3 = [False, True]
+
+        for v0 in kwarg0:
+            for v1 in kwarg1:
+                for v2 in kwarg2:
+                    for v3 in kwarg3:
+                        kwargs = {k0: v0, k1: v1, k2: v2, k3: v3}
+                        self.assertTrue(self.worm.worm_speed(**kwargs) is not None)
