125c4706302414410914753242ebfdfe9e733c50
Charles-Fieseler-Vienna <charles.fieseler@univie.ac.at>
2023-01-16 17:03:27 +0100

    Function for calculating residuals of a traces dataframe

---
+++ wbfm/utils/traces/residuals.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+
+
+def calculate_residual_subtract_pca(df: pd.DataFrame, n_components=1) -> pd.DataFrame:
+    """Note: must not contain nan"""
+    pca = PCA(n_components=n_components, whiten=False)
+
+    df_dat = df.to_numpy()
+    scaler = StandardScaler(with_std=False)
+    scaler.fit(df_dat)
+    dat_normalized = scaler.transform(df_dat)
+    dat_low_dimensional = pca.fit_transform(dat_normalized)
+    dat_reprojected = pca.inverse_transform(dat_low_dimensional)
+    dat_imputed = scaler.inverse_transform(dat_reprojected)
+
+    dat_residual = df_dat - dat_imputed
+    df_residual = pd.DataFrame(data=dat_residual, columns=df.columns)
+
+    return df_residual
