027610225585f8981a278551a50ba9ef69377319
Charles-Fieseler-Vienna <charles.fieseler@univie.ac.at>
2023-01-17 15:16:54 +0100

    Helper function for getting numbers similar to seaborn plots

---
+++ wbfm/utils/external/utils_statsmodels.py
@@ -0,0 +1,37 @@
+from typing import Optional
+import pandas as pd
+import statsmodels.api as sm
+
+
+def ols_groupby(df: pd.DataFrame, x: str, y: str, hue: Optional[str] = None):
+    """
+    Does ols, separating by hue, as used by seaborn
+
+    Parameters
+    ----------
+    df
+
+    Returns
+    -------
+
+    """
+
+    if hue is not None:
+        hue_vals = df[hue].unique()
+    else:
+        hue_vals = [None]
+
+    all_results = []
+    for val in hue_vals:
+        x_vec, y_vec = df[x], df[y]
+        if hue is not None:
+            ind = df[hue] == val
+            x_vec = x_vec[ind]
+            y_vec = y_vec[ind]
+
+        x_vec = sm.add_constant(x_vec)
+        results = sm.OLS(y_vec, x_vec, missing='drop').fit()
+
+        all_results.append(results)
+
+    return all_results
