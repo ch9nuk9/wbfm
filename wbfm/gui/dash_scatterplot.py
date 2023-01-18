66dbc7f4e0addd8950ac2e08bbe3e28af93b1238
Charles-Fieseler-Vienna <charles.fieseler@univie.ac.at>
2023-01-18 14:26:47 +0100

    Allow rectified regression

--- wbfm/gui/dash_scatterplot.py
+++ wbfm/gui/dash_scatterplot.py
@@ -26,6 +26,7 @@ def main():
     # Define layout
     app.layout = html.Div([
         build_dropdowns(df_correlation, df_behavior),
+        build_regression_menu(),
         build_plots_html()
         ]
     )
@@ -46,27 +47,43 @@ def main():
         _fig.update_layout(height=325, margin={'l': 20, 'b': 30, 'r': 10, 't': 30})
         return _fig
 
-    # Clicking callbacks
+    # Neuron selection updates
     @app.callback(
         Output('neuron-trace', 'figure'),
         Input('correlation-scatterplot', 'clickData'))
-    def _update_trace(clickData):
+    def _update_neuron_trace(clickData):
         neuron_name = clickData["points"][0]["customdata"][0]
-        _fig = make_trace_plot(df_behavior_and_traces, neuron_name)
-        _fig.update_layout(height=800, margin={'l': 40, 'b': 40, 't': 30, 'r': 0})
+        _fig = make_neuron_trace_plot(df_behavior_and_traces, neuron_name)
+        _fig.update_layout(height=325, margin={'l': 40, 'b': 40, 't': 30, 'r': 0})
         return _fig
 
     @app.callback(
-        Output('trace-behavior-scatterplot', 'figure'),
+        Output('trace-and-behavior-scatterplot', 'figure'),
         Input('correlation-scatterplot', 'clickData'),
-        Input('behavior-scatter-yaxis', 'value')
+        Input('behavior-scatter-yaxis', 'value'),
+        Input('regression-type', 'value')
     )
-    def _update_second_scatter(clickData, behavior_name):
+    def _update_behavior_scatter(clickData, behavior_name, regression_type):
+        if regression_type == 'Rectified regression':
+            opt = {'color': 'reversal'}
+        else:
+            opt = {}
         neuron_name = clickData["points"][0]["customdata"][0]
         _fig = px.scatter(df_behavior_and_traces, x=behavior_name, y=neuron_name,
-                          title=f"{neuron_name} and {behavior_name}",
-                          trendline='ols')
+                          title=f"Behavior-neuron scatterplot",
+                          trendline='ols', **opt)
         # Half as tall
+        _fig.update_layout(height=325, margin={'l': 20, 'b': 30, 'r': 10, 't': 30})
+        return _fig
+
+    # Behavior updates
+    @app.callback(
+        Output('behavior-trace', 'figure'),
+        Input('behavior-scatter-yaxis', 'value')
+    )
+    def _update_behavior_trace(behavior_name):
+        _fig = px.line(df_behavior_and_traces, x=df_behavior_and_traces.index, y=behavior_name,
+                       title=f"Trace of {behavior_name}")
         _fig.update_layout(height=325, margin={'l': 20, 'b': 30, 'r': 10, 't': 30})
         return _fig
 
@@ -75,19 +92,18 @@ def main():
 
 
 def build_plots_html() -> html.Div:
-
     # Second trace plot, which is actually initialized through a clickData field on the scatterplot
     initial_clickData = {'points': [{'customdata': ['neuron_001']}]}
 
     left_side = html.Div([
             dcc.Graph(id='correlation-scatterplot', clickData=initial_clickData),
-            dcc.Graph(id='trace-behavior-scatterplot')
+            dcc.Graph(id='trace-and-behavior-scatterplot')
     ], style={'width': '49%', 'display': 'inline-block'})
 
     right_side = html.Div([
-            dcc.Graph(
-                id='neuron-trace'
-            )], style={'width': '49%', 'display': 'inline-block', 'padding': '0 20'}
+            dcc.Graph(id='neuron-trace'),
+            dcc.Graph(id='behavior-trace')],
+        style={'width': '49%', 'display': 'inline-block', 'padding': '0 20'}
     )
 
     return html.Div([left_side, right_side])
@@ -105,39 +121,59 @@ def build_dropdowns(df_correlation, df_behavior) -> html.Div:
     x_initial = 'dummy'
     behavior_initial = 'signed_speed'
 
-    return html.Div([
+    return html.Div(children=[
         html.Div([
-            dcc.Dropdown(
-                correlation_names_no_dummy,
-                y_initial,
-                id='scatter-yaxis',
-            ),
-        ], style={'width': '32%', 'display': 'inline-block'}),
+            html.Label(['Scatter y axis'], style={'font-weight': 'bold', "text-align": "center"}),
+            html.Div([
+                dcc.Dropdown(
+                    correlation_names_no_dummy,
+                    y_initial,
+                    id='scatter-yaxis',
+                ),
+            ])],
+            style={'width': '33%'}),
 
         html.Div([
-            dcc.Dropdown(
-                correlation_names,
-                x_initial,
-                id='scatter-xaxis'
-            ),
-        ], style={'width': '32%', 'float': 'right', 'display': 'inline-block'}),
+            html.Label(['Scatter x axis'], style={'font-weight': 'bold', "text-align": "center"}),
+            html.Div([
+                dcc.Dropdown(
+                    correlation_names,
+                    x_initial,
+                    id='scatter-xaxis'
+                ),
+            ])],
+            style={'width': '33%'}),
 
         html.Div([
-            dcc.Dropdown(
-                behavior_names,
-                behavior_initial,
-                id='behavior-scatter-yaxis',
-            ),
-        ], style={'width': '32%', 'display': 'inline-block'}),
+            html.Label(["Behavior to show and correlate"], style={'font-weight': 'bold', "text-align": "center"}),
+            html.Div([
+                dcc.Dropdown(
+                    behavior_names,
+                    behavior_initial,
+                    id='behavior-scatter-yaxis',
+                ),
+            ])],
+            style={'width': '33%'})
 
-    ], style={'padding': '10px 5px'}
+        ], style={'display': 'flex', 'padding': '10px 5px'}
     )
 
 
-def make_trace_plot(df_traces, neuron_name):
-    fig_trace = px.line(df_traces, x=df_traces.index, y=neuron_name,
-                        title=neuron_name)
-    fig_trace.update_traces(mode='lines')
+def build_regression_menu() -> html.Div:
+    return html.Div([
+        html.Label(['Style of regression line']),
+        dcc.RadioItems(
+            ['Overall regression', 'Rectified regression'],
+            'Overall regression',
+            id='regression-type',
+            labelStyle={'display': 'inline-block', 'width': '33%'}
+        )
+        ]
+    )
+
+
+def make_neuron_trace_plot(df_traces, neuron_name):
+    fig_trace = px.line(df_traces, x=df_traces.index, y=neuron_name, title=f"Trace for {neuron_name}")
     return fig_trace
 
 
