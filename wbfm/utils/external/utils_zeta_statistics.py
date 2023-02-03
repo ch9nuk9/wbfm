deb00a4fd10878e9168ecf36e74d63fcd6ab6c1e
Charles-Fieseler-Vienna <charles.fieseler@univie.ac.at>
2023-02-03 12:17:49 +0100

    Attempt to speed up zeta calculations

--- wbfm/utils/external/utils_zeta_statistics.py
+++ wbfm/utils/external/utils_zeta_statistics.py
@@ -1,6 +1,5 @@
 import math
 from typing import List
-
 import numpy as np
 
 
@@ -8,6 +7,7 @@ import numpy as np
 # https://elifesciences.org/articles/71969#s4
 
 
+# @numba.jit(nopython=True)
 def jitter_indices(triggered_average_indices: List[np.ndarray], max_jitter: int, max_len: int) -> \
         List[np.ndarray]:
     """
@@ -19,15 +19,15 @@ def jitter_indices(triggered_average_indices: List[np.ndarray], max_jitter: int,
     ind_jittered = []
     for vec in triggered_average_indices:
         # Do not allow jitters that would put the window fully outside the recording
-        this_max_jitter = min(max_len - vec[-1], max_jitter + 1)
-        this_min_jitter = max(-vec[0], -max_jitter)
+        this_max_jitter = np.min([max_len - vec[-1], max_jitter + 1])
+        this_min_jitter = np.max([-vec[0], -max_jitter])
         new_vec = vec + np.random.randint(this_min_jitter, this_max_jitter)
-        new_vec = np.array([v for v in new_vec if v < max_len])
+        # new_vec = np.array([v for v in new_vec if v < max_len])
         ind_jittered.append(new_vec)
     return ind_jittered
 
 
-def calculate_zeta_cumsum(mat):
+def calculate_zeta_cumsum(mat: np.ndarray):
     """
     Calculates the cumulative sum minus a baseline. See:
     https://elifesciences.org/articles/71969#s4
@@ -53,6 +53,7 @@ def calculate_zeta_cumsum(mat):
     return delta_corrected
 
 
+# @numba.jit(nopython=True)
 def calculate_p_value_from_zeta(zeta_dat, zetas_baseline):
     """
     Uses gumbel distribution
