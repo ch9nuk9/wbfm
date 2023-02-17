8fe6dd5ec222d1c563b1dd240edfa9e92906f70c
Charles-Fieseler-Vienna <charles.fieseler@univie.ac.at>
2023-02-17 10:44:34 +0100

    Add colormap helpers

--- wbfm/utils/external/utils_behavior_annotation.py
+++ wbfm/utils/external/utils_behavior_annotation.py
@@ -1,5 +1,8 @@
 from enum import IntEnum
 import numpy as np
+import plotly.express as px
+
+from wbfm.utils.external.utils_pandas import get_contiguous_blocks_from_column
 
 
 class BehaviorCodes(IntEnum):
@@ -25,6 +28,15 @@ class BehaviorCodes(IntEnum):
         return cmap
 
     @classmethod
+    def ethogram_cmap(cls):
+        """Colormap for shading as a stand-alone ethogram"""
+        base_cmap = px.colors.qualitative.Plotly
+        cmap = {cls.UNKNOWN: None,
+                cls.FWD: base_cmap[0],
+                cls.REV: base_cmap[1]}
+        return cmap
+
+    @classmethod
     def has_value(cls, value):
         return value in cls._value2member_map_
 
@@ -36,3 +48,30 @@ class BehaviorCodes(IntEnum):
     def assert_all_are_valid(cls, vec):
         for v in np.unique(vec):
             cls.assert_is_valid(v)
+
+
+def options_for_ethogram(beh_vec):
+    all_shape_opt = []
+    cmap = BehaviorCodes.ethogram_cmap()
+
+    # Reversals
+    bin_behavior = beh_vec == BehaviorCodes.REV
+    starts, ends = get_contiguous_blocks_from_column(bin_behavior, already_boolean=True)
+    for s, e in zip(starts, ends):
+        this_val = beh_vec[s]
+        color = cmap[this_val]
+        shape_opt = dict(type="rect", x0=s, x1=e, y0=0, y1=1, fillcolor=color,
+                         line_width=0)
+        all_shape_opt.append(shape_opt)
+
+    # Forwards
+    bin_behavior = beh_vec == BehaviorCodes.FWD
+    starts, ends = get_contiguous_blocks_from_column(bin_behavior, already_boolean=True)
+    for s, e in zip(starts, ends):
+        this_val = beh_vec[s]
+        color = cmap[this_val]
+        shape_opt = dict(type="rect", x0=s, x1=e, y0=0, y1=1, fillcolor=color,
+                         line_width=0)
+        all_shape_opt.append(shape_opt)
+
+    return all_shape_opt
