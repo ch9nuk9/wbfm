e98fe51f4b8208d50befc2dc3546dafb23b0f266
Charles-Fieseler-Vienna <charles.fieseler@univie.ac.at>
2023-03-22 15:22:38 +0100

    Bash refactor

--- wbfm/gui/examples/dash_triggered_average_guis.sh
+++ wbfm/gui/examples/dash_triggered_average_guis.sh
@@ -9,17 +9,32 @@ COMMAND="/home/charles/Current_work/repos/dlc_for_wbfm/wbfm/gui/interactive_two_
 # Each folder is in this subfolder, and in general will have different options for the x and y axes, and color.
 PARENT_FOLDER="/home/charles/Current_work/presentations/Feb_2023"
 
-# Define a list of options for each call: folder, x, y, and c
+# Define a function that does the following:
+#   Open a tmux session, activate conda, and run the command with the options
+function open_tmux_and_run() {
+  # Args should be in the order: SUBFOLDER, X, Y, C, PORT
+  # Session name should be unique, and include the port number
+  SESS="dash_triggered_average_guis_${5}"
+  tmux new-session -d -s $SESS
+  tmux send-keys "conda activate wbfm38" C-m
+  tmux send-keys "cd ${PARENT_FOLDER}/${1}" C-m
+  tmux send-keys "python ${COMMAND} -p . -x '${2}' -y '${3}' -c ${4} --port ${5} --allow_public_access True" C-m
+  echo "Opened ${PARENT_FOLDER}/${1} with port ${5}"
+  echo "Accessible from the intranet at zimmer-ws00.neuro.univie.ac.at:${5}"
+}
+
+# Newest gui: semi-plateau
 SUBFOLDER="volcano_semi_plateau-reversal_triggered"
 X="effect size"
 Y="-log(p value)"
 C="genotype"
 PORT="8050"
+open_tmux_and_run ${SUBFOLDER} "${X}" "${Y}" ${C} ${PORT}
 
-# Open a tmux session, activate conda, and run the command with the options
-tmux new-session -d -s "dash_triggered_average_guis"
-tmux send-keys "conda activate wbfm38" C-m
-tmux send-keys "cd ${PARENT_FOLDER}/${SUBFOLDER}" C-m
-tmux send-keys "python ${COMMAND} -p . -x '${X}' -y '${Y}' -c ${C} --port ${PORT} --allow_public_access True" C-m
-echo "Opened ${PARENT_FOLDER}/${SUBFOLDER} with port ${PORT}"
-echo "Accessible from the intranet at zimmer-ws00.neuro.univie.ac.at:${PORT}"
+# Speed gui
+SUBFOLDER="gui_speed_encodings"
+X="genotype"
+Y="multi_neuron"
+C="genotype"
+PORT="8051"
+open_tmux_and_run ${SUBFOLDER} "${X}" "${Y}" ${C} ${PORT}
