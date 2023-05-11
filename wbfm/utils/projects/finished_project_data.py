2d194e3e545d2c6c33a4c582a27a288c066beb10
Charles-Fieseler-Vienna <charles.fieseler@univie.ac.at>
2023-05-11 16:09:22 +0200

    Add a behavior time series to the default

--- wbfm/utils/projects/finished_project_data.py
+++ wbfm/utils/projects/finished_project_data.py
@@ -683,6 +683,7 @@ class ProjectData:
         """
 
         behavior_codes = ['signed_stage_speed', 'summed_curvature', 'signed_speed_angular',
+                          'signed_middle_body_speed_smoothed',
                           'signed_middle_body_speed', 'head_curvature',
                           'fwd_empirical_distribution', 'rev_phase_counter']
 
