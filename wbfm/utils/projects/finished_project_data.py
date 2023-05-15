164777d3f009aa39db8216bd44d3cf9dccb4b375
Charles-Fieseler-Vienna <charles.fieseler@univie.ac.at>
2023-05-15 17:43:58 +0200

    Add more to the behavior

--- wbfm/utils/projects/finished_project_data.py
+++ wbfm/utils/projects/finished_project_data.py
@@ -685,7 +685,8 @@ class ProjectData:
         behavior_codes = ['signed_stage_speed', 'summed_curvature', 'signed_speed_angular',
                           'signed_middle_body_speed_smoothed',
                           'signed_middle_body_speed', 'head_curvature',
-                          'fwd_empirical_distribution', 'rev_phase_counter']
+                          'fwd_empirical_distribution', 'rev_phase_counter',
+                          'interpolated_ventral_midbody_curvature', 'interpolated_ventral_head_curvature']
 
         behavior_dict = {}
         for code in behavior_codes:
