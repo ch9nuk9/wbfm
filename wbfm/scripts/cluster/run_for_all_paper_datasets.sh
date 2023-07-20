2748875ac840447088780fd7cf8273399e3d4de3
Charles-Fieseler-Vienna <charles.fieseler@univie.ac.at>
2023-07-20 17:36:08 +0200

    Fix bash list typos

--- wbfm/scripts/cluster/run_for_all_paper_datasets.sh
+++ wbfm/scripts/cluster/run_for_all_paper_datasets.sh
@@ -2,7 +2,7 @@
 
 # Runs a script for all datasets to be used in the paper
 PARENT_DIR="/scratch/neurobiology/zimmer/Charles/dlc_stacks"
-DATASET_LIST=["2022-12-10_spacer_7b_2per_agar", "2022-12-05_spacer_7b_2per_agar", "2022-11-23_spacer_7b_2per_agar", "2022-11-30_spacer_7b_2per_agar", "2022-11-27_spacer_7b_2per_agar"]
+DATASET_LIST=("2022-12-10_spacer_7b_2per_agar" "2022-12-05_spacer_7b_2per_agar" "2022-11-23_spacer_7b_2per_agar" "2022-11-30_spacer_7b_2per_agar" "2022-11-27_spacer_7b_2per_agar")
 SCRIPT_DIR="/scratch/neurobiology/zimmer/Charles/dlc_stacks/wbfm/scripts/cluster"
 
 # Get the exact step to run
@@ -15,8 +15,7 @@ do
 done
 
 # Loop over all datasets
-for DATASET in $DATASET_LIST
-do
+for DATASET in ${DATASET_LIST[@]}; do
     # Run the command, using my folder wrapper
     bash $SCRIPT_DIR/apply_step_to_all_in_folder.sh -t $PARENT_DIR/"$DATASET" -s "$step_reference"
 done
--- wbfm/scripts/examples/copy_pngs_for_paper_folders.sh
+++ wbfm/scripts/examples/copy_pngs_for_paper_folders.sh
@@ -3,7 +3,7 @@
 # Runs a script for all datasets to be used in the paper
 # See copy_pngs_for_paper_folders.sh for more details
 PARENT_DIR="/scratch/neurobiology/zimmer/Charles/dlc_stacks"
-DATASET_LIST=["2022-12-10_spacer_7b_2per_agar", "2022-12-05_spacer_7b_2per_agar", "2022-11-23_spacer_7b_2per_agar", "2022-11-30_spacer_7b_2per_agar", "2022-11-27_spacer_7b_2per_agar"]
+DATASET_LIST=("2022-12-10_spacer_7b_2per_agar" "2022-12-05_spacer_7b_2per_agar" "2022-11-23_spacer_7b_2per_agar" "2022-11-30_spacer_7b_2per_agar" "2022-11-27_spacer_7b_2per_agar")
 SCRIPT="/scratch/neurobiology/zimmer/Charles/dlc_stacks/wbfm/scripts/examples/copy_pngs_for_paper_folders.sh"
 
 # Parse arguments
@@ -17,8 +17,7 @@ do
 done
 
 # Loop over all datasets
-for DATASET in $DATASET_LIST
-do
+for DATASET in ${DATASET_LIST[@]}; do
     # Run the command, using my folder wrapper
     bash $SCRIPT -t "$target" -d "$destination_folder" -s $PARENT_DIR/"$DATASET"
 done
