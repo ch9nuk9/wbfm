f785cca06529626b874af8e0676746c1ce899c76
Charles-Fieseler-Vienna <charles.fieseler@univie.ac.at>
2023-08-02 15:08:56 +0200

    Change default grid plots

--- wbfm/scripts/visualization/4+make_triggered_average_grid_plot.py
+++ wbfm/scripts/visualization/4+make_triggered_average_grid_plot.py
@@ -40,7 +40,8 @@ def main(_config, _run):
 
     # Hilbert phase
     make_summary_hilbert_triggered_average_grid_plot(project_data)
-    make_summary_hilbert_triggered_average_grid_plot(project_data, return_fast_scale_separation=True)
+    # make_summary_hilbert_triggered_average_grid_plot(project_data, return_fast_scale_separation=True)
     make_summary_hilbert_triggered_average_grid_plot(project_data, residual_mode='pca', interpolate_nan=True)
+    make_summary_hilbert_triggered_average_grid_plot(project_data, residual_mode='pca_global', interpolate_nan=True)
 
     # make_pirouette_split_triggered_average_plots(project_data)
