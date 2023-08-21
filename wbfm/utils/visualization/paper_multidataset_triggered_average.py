7e8ab13c99e07bd45ad7204e7f1c3aabe6c4d31a
Charles-Fieseler-Vienna <charles.fieseler@univie.ac.at>
2023-08-21 17:23:23 +0200

    WIP code for paper triggered averages

--- wbfm/utils/visualization/paper_multidataset_triggered_average.py
+++ wbfm/utils/visualization/paper_multidataset_triggered_average.py
@@ -30,10 +30,13 @@ class PaperMultiDatasetTriggeredAverage:
     dataset_clusterer_residual_rectified_fwd = None
     dataset_clusterer_residual_rectified_rev = None
 
+    intermediates_residual_rectified_fwd = None
+    intermediates_residual_rectified_rev = None
+
     def __post_init__(self):
         # Analyze the project data to get the clusterers and intermediates
-
-        trace_base_opt = dict(interpolate_nan=True, channel_mode='dr_over_r_50', remove_outliers=True)
+        trace_base_opt = dict(interpolate_nan=True, channel_mode='dr_over_r_50', remove_outliers=True,
+                              rename_neurons_using_manual_ids=True)
 
         # Fast (residual)
         trigger_opt = dict(use_hilbert_phase=True, state=None)
@@ -44,6 +47,22 @@ class PaperMultiDatasetTriggeredAverage:
         self.dataset_clusterer_residual = out[0]
         self.intermediates_residual = out[1]
 
+        # Residual rectified forward
+        trigger_opt['only_allow_events_during_state'] = BehaviorCodes.FWD
+        cluster_opt = {}
+        out = clustered_triggered_averages_from_list_of_projects(self.all_projects, cluster_opt=cluster_opt,
+                                                                 trigger_opt=trigger_opt, trace_opt=trace_opt)
+        self.dataset_clusterer_residual_rectified_fwd = out[0]
+        self.intermediates_residual_rectified_fwd = out[1]
+
+        # Residual rectified reverse
+        trigger_opt['only_allow_events_during_state'] = BehaviorCodes.REV
+        cluster_opt = {}
+        out = clustered_triggered_averages_from_list_of_projects(self.all_projects, cluster_opt=cluster_opt,
+                                                                 trigger_opt=trigger_opt, trace_opt=trace_opt)
+        self.dataset_clusterer_residual_rectified_rev = out[0]
+        self.intermediates_residual_rectified_rev = out[1]
+
         # Slow (global)
         trigger_opt = dict(use_hilbert_phase=False, state=BehaviorCodes.REV)
         trace_opt = dict(residual_mode='pca_global')
@@ -53,5 +72,15 @@ class PaperMultiDatasetTriggeredAverage:
         self.dataset_clusterer_global = out[0]
         self.intermediates_global = out[1]
 
-        # Residual rectified
+    def get_clusterer_from_trigger_type(self, trigger_type):
+        trigger_mapping = {'raw': self.dataset_clusterer_raw,
+                           'global': self.dataset_clusterer_global,
+                           'residual': self.dataset_clusterer_residual,
+                           'residual_rectified_fwd': self.dataset_clusterer_residual_rectified_fwd,
+                           'residual_rectified_rev': self.dataset_clusterer_residual_rectified_rev}
+        if trigger_type not in trigger_mapping:
+            raise ValueError(f'Invalid trigger type: {trigger_type}; must be one of {list(trigger_mapping.keys())}')
+        return trigger_mapping[trigger_type]
 
+    def plot_triggered_average_single_neuron(self, neuron_name, trigger_type):
+        pass
