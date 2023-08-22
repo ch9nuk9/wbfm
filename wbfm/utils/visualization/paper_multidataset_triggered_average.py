17c6dcfba41e634afcacbdf48b502c73875c18ba
Charles-Fieseler-Vienna <charles.fieseler@univie.ac.at>
2023-08-22 14:31:51 +0200

    Add plotting functionality

--- wbfm/utils/visualization/paper_multidataset_triggered_average.py
+++ wbfm/utils/visualization/paper_multidataset_triggered_average.py
@@ -1,13 +1,16 @@
 """
 Designed to plot the triggered average of the paper's datasets.
 """
-
+import os
 from dataclasses import dataclass
 from typing import Dict
 
+from matplotlib import pyplot as plt
+
 from wbfm.utils.general.utils_behavior_annotation import BehaviorCodes
 from wbfm.utils.projects.finished_project_data import ProjectData
-from wbfm.utils.traces.triggered_averages import clustered_triggered_averages_from_list_of_projects
+from wbfm.utils.traces.triggered_averages import clustered_triggered_averages_from_list_of_projects, \
+    ClusteredTriggeredAverages, plot_triggered_average_from_matrix_low_level
 
 
 @dataclass
@@ -17,9 +20,9 @@ class PaperMultiDatasetTriggeredAverage:
     all_projects: Dict[str, ProjectData]
 
     # Three different sets of parameters: raw, global, and residual
-    dataset_clusterer_raw = None
-    dataset_clusterer_global = None
-    dataset_clusterer_residual = None
+    dataset_clusterer_raw: ClusteredTriggeredAverages = None
+    dataset_clusterer_global: ClusteredTriggeredAverages = None
+    dataset_clusterer_residual: ClusteredTriggeredAverages = None
 
     intermediates_raw = None
     intermediates_global = None
@@ -27,8 +30,8 @@ class PaperMultiDatasetTriggeredAverage:
 
     # Use these to build rectified (single-state only) triggered averages
     # For now, only need the residual ones
-    dataset_clusterer_residual_rectified_fwd = None
-    dataset_clusterer_residual_rectified_rev = None
+    dataset_clusterer_residual_rectified_fwd: ClusteredTriggeredAverages = None
+    dataset_clusterer_residual_rectified_rev: ClusteredTriggeredAverages = None
 
     intermediates_residual_rectified_fwd = None
     intermediates_residual_rectified_rev = None
@@ -82,5 +85,58 @@ class PaperMultiDatasetTriggeredAverage:
             raise ValueError(f'Invalid trigger type: {trigger_type}; must be one of {list(trigger_mapping.keys())}')
         return trigger_mapping[trigger_type]
 
-    def plot_triggered_average_single_neuron(self, neuron_name, trigger_type):
-        pass
+    def get_df_triggered_from_trigger_type(self, trigger_type):
+        df_mapping = {#'raw': self.intermediates_raw[1],  # TODO: implement raw
+                      'global': self.intermediates_global[1],
+                      'residual': self.intermediates_residual[1],
+                      'residual_rectified_fwd': self.intermediates_residual_rectified_fwd[1],
+                      'residual_rectified_rev': self.intermediates_residual_rectified_rev[1]}
+        if trigger_type not in df_mapping:
+            raise ValueError(f'Invalid trigger type: {trigger_type}; must be one of {list(df_mapping.keys())}')
+        return df_mapping[trigger_type]
+
+    def get_color_from_trigger_type(self, trigger_type):
+        color_mapping = {'raw': 'tab:blue', 'global': 'tab:orange', 'residual': 'tab:green', 'kymo': 'black'}
+        if trigger_type not in color_mapping:
+            raise ValueError(f'Invalid trigger type: {trigger_type}; must be one of {list(color_mapping.keys())}')
+        return color_mapping[trigger_type]
+
+    def get_fig_opt(self):
+        return dict(dpi=300, figsize=(5 / 3, 5 / 3))
+
+    def plot_triggered_average_single_neuron(self, neuron_name, trigger_type, output_foldername=None,
+                                             DEBUG=False):
+        # clusterer = self.get_clusterer_from_trigger_type(trigger_type)
+        color = self.get_color_from_trigger_type(trigger_type)
+        df = self.get_df_triggered_from_trigger_type(trigger_type)
+
+        # Get the full names of all the neurons with this name
+        # Names will be like '2022-11-23_worm9_BAGL' and we are checking for 'BAGL'
+        neuron_names = [n for n in list(df.columns) if neuron_name in n]
+        if DEBUG:
+            print(f"Found {len(neuron_names)} neurons with name {neuron_name}")
+            print(f"Neuron names: {neuron_names}")
+        if len(neuron_names) == 0:
+            print(f"Neuron name {neuron_name} not found in {list(df.columns)}")
+            return
+
+        # Plot the triggered average for each neuron
+        fig_opt_trigger = self.get_fig_opt()
+        fig, ax = plt.subplots(**fig_opt_trigger)
+
+        # TODO: do not hardcode the ind_preceding
+        df_subset = df.loc[:, neuron_names].T
+        min_lines = min(3, len(neuron_names))
+        if DEBUG:
+            print(df_subset)
+        plot_triggered_average_from_matrix_low_level(df_subset, 0, min_lines, False, False, ax=ax,
+                                                     color=color)
+        plt.ylabel("dR/R50")
+        plt.title("Reversal triggered")
+        plt.xlabel("Time (s)")
+        plt.tight_layout()
+
+        if output_foldername is not None:
+            fname = os.path.join(output_foldername, 'rev_trigger_all_datasets.png')
+            plt.savefig(fname)
+            fig.savefig(fname.replace(".png", ".svg"))
