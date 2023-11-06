4a33539dc2e7e0c703cce7a26830a7cef89220c8
Charles-Fieseler-Vienna <charles.fieseler@univie.ac.at>
2023-11-06 15:48:34 +0100

    Fix typo

--- wbfm/scripts/examples/change_permission_of_manual_annotation_file.sh
+++ wbfm/scripts/examples/change_permission_of_manual_annotation_file.sh
@@ -17,7 +17,7 @@ subfolders=("$parent_dir"/*/)
 echo "Found ${#subfolders[@]} subfolders"
 
 # Iterate through top-level subfolders and apply permissions
-for subfolder in subfolders[@]; do
+for subfolder in "${subfolders[@]}"; do
     target_dir="$subfolder$folder_path"
     if [ -d "$target_dir" ]; then
         chmod -R g+w,o+rw "$target_dir"
