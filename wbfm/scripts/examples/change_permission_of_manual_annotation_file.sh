c86ee6a438a8e640ad616f83c040f3f1d544bf08
Charles-Fieseler-Vienna <charles.fieseler@univie.ac.at>
2023-11-06 13:59:47 +0100

    Fix bug

--- wbfm/scripts/examples/change_permission_of_manual_annotation_file.sh
+++ wbfm/scripts/examples/change_permission_of_manual_annotation_file.sh
@@ -13,4 +13,4 @@ done
 file_path="3-tracking/manual_annotation/manual_annotation.xlsx"
 
 # Use find to discover subfolders and apply chmod
-find "$parent_dir" -type d -exec chmod g+w {}/"$file_path" \;
+find "$parent_dir" -maxdepth 1 -mindepth 1 -type d -exec chmod g+w {}/"$file_path" \;
