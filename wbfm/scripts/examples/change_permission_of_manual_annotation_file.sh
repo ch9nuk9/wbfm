992888d06a67aba1269e797b20abd0dd294b6d40
Charles-Fieseler-Vienna <charles.fieseler@univie.ac.at>
2023-11-06 15:46:20 +0100

    Refactor to just a loop

--- wbfm/scripts/examples/change_permission_of_manual_annotation_file.sh
+++ wbfm/scripts/examples/change_permission_of_manual_annotation_file.sh
@@ -12,5 +12,11 @@ done
 # Define the file path relative to each subfolder
 folder_path="3-tracking/manual_annotation"
 
-# Use find to discover subfolders and apply chmod
-find "$parent_dir" -maxdepth 1 -mindepth 1 -type d -name -wholename "*/$folder_path" -exec chmod -R g+w,o+rw "{}" \;
+# Iterate through top-level subfolders and apply permissions
+for subfolder in "$parent_dir"/*/; do
+    target_dir="$subfolder$folder_path"
+    if [ -d "$target_dir" ]; then
+        chmod -R g+w,o+rw "$target_dir"
+        echo "Changed permissions for $target_dir"
+    fi
+done
