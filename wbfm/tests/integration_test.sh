3fd1064255a2588f1e7785be8450f8dca9d0f36e
Charles-Fieseler-Vienna <charles.fieseler@univie.ac.at>
2023-11-20 15:13:41 +0100

    Script for rerunning integration test

---
+++ wbfm/tests/integration_test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Clean and run the integration test, i.e. a shortened dataset
+PROJECT_DIR="/scratch/neurobiology/zimmer/fieseler/wbfm_projects/project_pytest"
+CODE_DIR="/scratch/neurobiology/zimmer/wbfm/code/wbfm"
+
+# Clean
+COMMAND=$CODE_DIR/"scripts/postprocessing/delete_analysis_files.py"
+PROJECT_PATH=$PROJECT_DIR/"project_config.yaml"
+python $COMMAND with project_path=$PROJECT_PATH dryrun=False
+
+# Run using the snakemake pipeline
+cd $PROJECT_DIR/snakemake || exit
+bash RUNME_cluster.sh
