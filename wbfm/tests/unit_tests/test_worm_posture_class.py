1cf12ee00078a7d0e5bcab28f532def4b8b1c714
Charles-Fieseler-Vienna <charles.fieseler@univie.ac.at>
2024-02-12 16:09:34 +0100

    Update tests

--- wbfm/tests/unit_tests/test_worm_posture_class.py
+++ wbfm/tests/unit_tests/test_worm_posture_class.py
@@ -1,6 +1,6 @@
 import unittest
-
 import numpy as np
+from parameterized import parameterized_class
 
 from wbfm.utils.projects.finished_project_data import ProjectData
 
@@ -8,9 +8,13 @@ from wbfm.utils.projects.finished_project_data import ProjectData
 class TestWormPostureClass(unittest.TestCase):
 
     def setUp(self):
+
+        # Could use fancier things like parameterized_class, but this is a simple example
+        project_list = ["/scratch/neurobiology/zimmer/fieseler/wbfm_projects/manually_annotated/paper_data/ZIM2165_Gcamp7b_worm1-2022_11_28/project_config.yaml",
+                        '/scratch/neurobiology/zimmer/brenner/wbfm_projects/analyze/immobilized_wt/2023-09-07_16-11_CaMP7b_O2_worm1-2023-09-07/project_config.yaml']
+
         # Example project
-        fname = "/scratch/neurobiology/zimmer/fieseler/wbfm_projects/paper_datasets/ZIM2165_Gcamp7b_worm1-2022_11_28/project_config.yaml"
-        project_data = ProjectData.load_final_project_data_from_config(fname)
+        project_data = ProjectData.load_final_project_data_from_config(project_list[1], verbose=False)
         self.project_data = project_data
         self.worm = project_data.worm_posture_class
 
@@ -31,6 +35,17 @@ class TestWormPostureClass(unittest.TestCase):
 
         for p in properties:
             self.assertTrue(p(fluorescence_fps=True) is not None)
+
+    def test_subsampling_index_deltas(self):
+        # Get beh annotation at both high-resolution and fluorescence_fps
+        beh_annotation = self.worm.beh_annotation()
+        beh_annotation_fluorescence_fps = self.worm.beh_annotation(fluorescence_fps=True)
+
+        # Should only be one index difference
+        self.assertTrue(np.unique(np.diff(beh_annotation_fluorescence_fps.index)).size == 1,
+                        msg=f"Unique differences: {np.unique(np.diff(beh_annotation_fluorescence_fps.index))}")
+        self.assertTrue(np.unique(np.diff(beh_annotation.index)).size == 1,
+                        msg=f"Unique differences: {np.unique(np.diff(beh_annotation.index))}")
 
     def test_speeds(self):
         properties = [self.worm.worm_speed, self.worm.worm_angular_velocity]
