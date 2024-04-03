1d91398f9e2e4a41ce9dcb714a54b68c30cdec32
Charles-Fieseler-Vienna <charles.fieseler@univie.ac.at>
2024-04-03 15:42:05 +0200

    Slight refactor

--- wbfm/scripts/hardcoded_protocols/update_filepaths_in_old_projects.py
+++ wbfm/scripts/hardcoded_protocols/update_filepaths_in_old_projects.py
@@ -17,15 +17,15 @@ def main():
     all_projects_gfp = load_paper_datasets('gfp')
     all_projects_immob = load_paper_datasets('immob')
 
-    def _update_paths_in_project(p):
-        for k, v in p.project_config.config.items():
+    def _update_paths_in_project(_p):
+        for k, v in _p.project_config.config.items():
             is_updated = False
             v_new = None
             if isinstance(v, str):
                 v_new, is_updated = correct_mounted_path_prefix(v)
             if is_updated and v_new is not None:
-                p.project_config.config[k] = v_new
-        p.project_config.update_self_on_disk()
+                _p.project_config.config[k] = v_new
+        _p.project_config.update_self_on_disk()
 
     for _, p in tqdm(all_projects_gcamp.items()):
         try:
