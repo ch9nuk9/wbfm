9218388daa04b62b36ebbfee73ae5ee498a88a21
Charles-Fieseler-Vienna <charles.fieseler@univie.ac.at>
2024-04-19 10:35:06 +0200

    Update script to change partition

--- wbfm/scripts/cluster/change_pending_slurm_jobs.sh
+++ wbfm/scripts/cluster/change_pending_slurm_jobs.sh
@@ -2,11 +2,14 @@
 
 # Designed to change all slurm jobs to the himem partition, which is often empty when the basic partition is full
 
-# Get all slurm IDs from my user
-readarray -t MY_IDS < <(squeue -u fieseler -h -o '%i')
+# Get user name
+USER=$(whoami)
+
+# Get all slurm IDs
+readarray -t MY_IDS < <(squeue -u "$USER" -h -o '%i')
 
 # Loop through jobs and update each
 for ID in ${MY_IDS[@]}; do
-  echo "Running: scontrol update jobid=$ID MinMemoryNode=600000 partition=himem"
+  echo "If pending, moving job to himem partition: scontrol update jobid=$ID MinMemoryNode=600000 partition=himem"
   scontrol update jobid=$ID MinMemoryNode=600000 partition=himem
 done
