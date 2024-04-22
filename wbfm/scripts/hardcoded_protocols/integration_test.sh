3b93dd56f83a4c7a53ee4ff26b6146770a408ebd
Charles-Fieseler-Vienna <charles.fieseler@univie.ac.at>
2024-04-22 14:08:13 +0200

    Update integration test to run on multiple projects

--- wbfm/scripts/hardcoded_protocols/integration_test.sh
+++ wbfm/scripts/hardcoded_protocols/integration_test.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
 # Clean and run the integration test, i.e. a shortened dataset
-PROJECT_DIR="/lisc/scratch/neurobiology/zimmer/wbfm/test_projects/pytest-raw"
+PARENT_PROJECT_DIR="/lisc/scratch/neurobiology/zimmer/wbfm/test_projects"
 CODE_DIR="/lisc/scratch/neurobiology/zimmer/wbfm/code/wbfm/wbfm"
 
 # Clean
 COMMAND=$CODE_DIR/"scripts/postprocessing/delete_analysis_files.py"
-PROJECT_PATH=$PROJECT_DIR/"project_config.yaml"
-python $COMMAND with project_path=$PROJECT_PATH dryrun=False
+# Loop through each subfolder in the parent project directory
+for f in "$PARENT_PROJECT_DIR"/*; do
+    if [ -d "$f" ] && [ ! -L "$f" ]; then
+        echo "Checking folder: $f"
+        PROJECT_PATH=$f/"project_config.yaml"
+        python $COMMAND with project_path=$PROJECT_PATH dryrun=False
+    fi
+done
 
-# Run using the snakemake pipeline from an sbatch controller job
-cd $PROJECT_DIR/snakemake || exit
-COMMAND="RUNME.sh"
+# Run using my helper script for all in folder
+COMMAND=$CODE_DIR/"scripts/cluster/run_all_projects_in_parent_folder.sh"
+bash $COMMAND -t $PARENT_PROJECT_DIR -s traces_and_behavior
 
-# The bash script also accepts command line arguments, so we want to avoid them being passed to sbatch
-sbatch --time 1-00:00:00 \
-        --cpus-per-task 1 \
-        --mem 1G \
-        $COMMAND
