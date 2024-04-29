8fe4fe0ab541f43446208f584ec9ed412b7b06d9
Charles-Fieseler-Vienna <charles.fieseler@univie.ac.at>
2024-04-29 13:54:53 +0200

    Update integration test to actually create the project, really starting from raw data

--- wbfm/scripts/hardcoded_protocols/integration_test_local.sh
+++ wbfm/scripts/hardcoded_protocols/integration_test_local.sh
@@ -2,24 +2,25 @@
 
 # Clean and run the integration test, i.e. a shortened dataset
 DATA_DIR="/lisc/scratch/neurobiology/zimmer/wbfm/example_data/freely_moving/ZIM2165_Gcamp7b_worm1"
-PROJECT_DIR="/lisc/scratch/neurobiology/zimmer/wbfm/test_projects"
+PARENT_PROJECT_DIR="/lisc/scratch/neurobiology/zimmer/wbfm/test_projects"
 CODE_DIR="/lisc/scratch/neurobiology/zimmer/wbfm/code/wbfm/wbfm"
 
+# Remove project if it exists
+PROJECT_PATH=$PARENT_PROJECT_DIR/"pytest-raw"
+if [ -d "$PROJECT_PATH" ]; then
+  echo "Removing existing project at $PROJECT_PATH"
+  rm -r "$PROJECT_PATH"
+fi
+
 # Initialize project
+echo "Creating new project at $PROJECT_PATH"
 COMMAND=$CODE_DIR/"scripts/0a-create_new_project.py"
-COMMAND="$COMMAND with parent_data_folder=$DATA_DIR project_dir=$PROJECT_DIR experimenter=pytest"
-python "$COMMAND"
-
-# Get the newest folder using find and assume that is the one that was just created
-NEWEST_PROJECT_DIR=$(find $PROJECT_DIR -maxdepth 1 -type d -printf '%T@ %p\n' | sort -n | tail -1 | cut -f2- -d" ")
-
-# Clean
-#COMMAND=$CODE_DIR/"scripts/postprocessing/delete_analysis_files.py"
-#PROJECT_PATH=$PROJECT_DIR/"project_config.yaml"
-#python $COMMAND with project_path=$PROJECT_PATH dryrun=False
+ARGS="with parent_data_folder=$DATA_DIR project_dir=$PARENT_PROJECT_DIR experimenter=pytest"
+# shellcheck disable=SC2086
+python "$COMMAND" $ARGS
 
 # Run using the snakemake pipeline from a bash (would be sbatch on the cluster) controller job
-cd "$NEWEST_PROJECT_DIR"/snakemake || exit
-COMMAND="RUNME.sh -c"
+cd "$PROJECT_PATH"/snakemake || exit
+echo "Running the pipeline in project $PWD"
 
-bash "$COMMAND"
+bash RUNME.sh -c
