40273215684febc8856b418ee4453e91185babf3
Charles-Fieseler-Vienna <charles.fieseler@univie.ac.at>
2024-06-14 11:16:13 +0200

    Add helper function

--- wbfm/utils/external/utils_plotly.py
+++ wbfm/utils/external/utils_plotly.py
@@ -1,4 +1,5 @@
 import plotly.graph_objects as go
+import plotly.express as px
 
 
 def plotly_boxplot_colored_boxes(df, color_list):
@@ -27,5 +28,45 @@ def plotly_boxplot_colored_boxes(df, color_list):
     fig.update_layout(showlegend=False)
     # Make one grid line at 0, and make it black
     fig.update_yaxes(zeroline=True, zerolinewidth=1, zerolinecolor="black")
+
+    return fig
+
+
+def add_trendline_annotation(fig):
+    """
+    Given a scatter plot with a trendline added, add an annotation with the slope, R² and p-value of the trendline
+
+    Parameters
+    ----------
+    fig
+    df
+
+    Returns
+    -------
+
+    """
+    # Extract trendline results
+    results = px.get_trendline_results(fig)
+    trendline = results.iloc[0].px_fit_results
+    slope = trendline.params[1]
+    r2 = trendline.rsquared
+    pvalue = trendline.pvalues[1]
+
+    # Get a reasonable position (top right) for the annotation
+    x = fig.layout.xaxis.range[1]
+    y = fig.layout.yaxis.range[1]
+
+    # Add the annotation
+    annotation_text = f'Slope: {slope:.2f}<br>R²: {r2:.2f}<br>p-value: {pvalue:.3f}'
+    fig.add_annotation(
+        x=x, y=y,
+        text=annotation_text,
+        showarrow=False,
+        bordercolor='black',
+        borderwidth=1,
+        borderpad=4,
+        bgcolor='white',
+        opacity=0.8
+    )
 
     return fig
