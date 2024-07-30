e8f27c49765f2f8ff8e05d14aabfb2f90690d5af
Charles-Fieseler-Vienna <charles.fieseler@univie.ac.at>
2024-07-30 14:13:07 +0200

    Fix default datatype; related to #450

--- wbfm/gui/trace_explorer.py
+++ wbfm/gui/trace_explorer.py
@@ -7,21 +7,26 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Build GUI with a project')
     parser.add_argument('--project_path', '-p', default=None,
                         help='path to config file')
-    parser.add_argument('--force_tracklets_to_be_sparse', default=True)
-    parser.add_argument('--load_tracklets', default=True)
-    parser.add_argument('--DEBUG', default=False,
-                        help='')
+    # Boolean args
+    parser.add_argument('--force_tracklets_to_be_sparse', default="False", help='')
+    parser.add_argument('--load_tracklets', default="True", help='')
+    parser.add_argument('--DEBUG', default=False, help='')
+
     args = parser.parse_args()
+
     project_path = args.project_path
     force_tracklets_to_be_sparse = args.force_tracklets_to_be_sparse
-    force_tracklets_to_be_sparse = (force_tracklets_to_be_sparse == "True")
+    force_tracklets_to_be_sparse = (force_tracklets_to_be_sparse.lower() == "true")
     load_tracklets = args.load_tracklets
-    load_tracklets = (load_tracklets == "True")
+    load_tracklets = (load_tracklets.lower() == "true")
     if not force_tracklets_to_be_sparse:
         logging.warning("Tracklets will not be forced to be sparse. This may cause interactivity to crash.")
     DEBUG = args.DEBUG
 
-    print("Starting trace explorer GUI, may take up to a minute to load...")
+    print(f"Starting trace explorer GUI with options:"
+          f" force_tracklets_to_be_sparse={force_tracklets_to_be_sparse}, "
+          f"load_tracklets={load_tracklets}, "
+          f"may take a couple minutes to load...")
 
     napari_trace_explorer_from_config(project_path, load_tracklets=load_tracklets,
                                       force_tracklets_to_be_sparse=force_tracklets_to_be_sparse,
