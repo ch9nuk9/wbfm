ca0b6cbea8a54d384b1aeb4ac55778499c0b67c5
Charles-Fieseler-Vienna <charles.fieseler@univie.ac.at>
2024-09-03 16:05:44 +0200

    Properly catch timeout errors in snakemake; related to #569

--- wbfm/new_project_defaults/snakemake/RUNME.sh
+++ wbfm/new_project_defaults/snakemake/RUNME.sh
@@ -35,6 +35,33 @@ done
 OPT="sbatch -t {cluster.time} -p {cluster.partition} --cpus-per-task {cluster.cpus_per_task} --mem {cluster.mem} --output {cluster.output} --gres {cluster.gres}"
 NUM_JOBS_TO_SUBMIT=8
 
+# Slurm doesn't properly deal with TIMEOUT errors in subjobs, so we need to create a script to deal with them
+# In principle we will use a python script, but because this needs to be valid for all users, we will create a temporary
+# script that will be deleted after the job is done
+# Details: https://snakemake.readthedocs.io/en/v7.7.0/tutorial/additional_features.html#using-cluster-status
+CLUSTER_STATUS_SCRIPT=$(mktemp /tmp/slurm_script.XXXXXX)
+
+cat << EOF > $CLUSTER_STATUS_SCRIPT
+#!/usr/bin/env python
+import subprocess
+import sys
+
+jobid = sys.argv[1]
+
+output = str(subprocess.check_output("sacct -j %s --format State --noheader | head -1 | awk '{print $1}'" % jobid, shell=True).strip())
+
+running_status=["PENDING", "CONFIGURING", "COMPLETING", "RUNNING", "SUSPENDED"]
+if "COMPLETED" in output:
+  print("success")
+elif any(r in output for r in running_status):
+  print("running")
+else:
+  print("failed")
+EOF
+
+# Make the script executable
+chmod +x $CLUSTER_STATUS_SCRIPT
+
 # Actual command
 if [ "$DRYRUN" ]; then
     echo "DRYRUN of snakemake rule: $RULE. Common options: traces_and_behavior (default), traces, behavior"
@@ -46,5 +73,5 @@ elif [ -z "$USE_CLUSTER" ]; then
 else
     echo "Running snakemake rule on the cluster: $RULE. Common options: traces_and_behavior (default), traces, behavior"
     snakemake -s pipeline.smk --unlock  # Unlock the folder, just in case
-    snakemake "$RULE" -s pipeline.smk --latency-wait 60 --cluster "$OPT" --cluster-config cluster_config.yaml --jobs $NUM_JOBS_TO_SUBMIT
+    snakemake "$RULE" -s pipeline.smk --latency-wait 60 --cluster "$OPT --parsable" --cluster-config cluster_config.yaml --jobs $NUM_JOBS_TO_SUBMIT --cluster-status $CLUSTER_STATUS_SCRIPT
 fi
