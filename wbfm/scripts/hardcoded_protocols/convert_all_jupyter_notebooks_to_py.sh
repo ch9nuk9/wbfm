9bd775644a829392ee05f0c5f800b00b6b1eebaa
Charles-Fieseler-Vienna <charles.fieseler@univie.ac.at>
2024-11-15 09:30:17 +0100

    Helper script for saving jupyter notebooks

---
+++ wbfm/scripts/hardcoded_protocols/convert_all_jupyter_notebooks_to_py.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Convert all Jupyter notebooks in target folder (or below) to Python scripts
+# See https://stackoverflow.com/questions/45802690/converting-all-files-in-folder-to-py-files/49297443
+# One answer is just incorrect; current last answer is correct
+
+# Usage:
+# ./convert_all_jupyter_notebooks_to_py.sh /path/to/target/folder
+
+find "$1" -name "*.ipynb" -exec jupyter nbconvert --to script {} \;
