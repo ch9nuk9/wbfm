79744cda301052d8b37ae57c824ad049b2f13e77
Charles-Fieseler-Vienna <83711538+Charles-Fieseler-Vienna@users.noreply.github.com>
2025-02-05 12:12:11 +0100

    Fix gpu script paths; related to #676

--- wbfm/scripts/hardcoded_protocols/check_gpu/check_gpu.py
+++ wbfm/scripts/hardcoded_protocols/check_gpu/check_gpu.py
@@ -4,5 +4,5 @@ import torch
 
 if __name__ == "__main__":
     print(f"Cuda is found by pytorch: {torch.cuda.is_available()}")
-    print(f"Cuda is found by tensorflow: {tf.config.list_physical_devices('GPU')}")
+    print(f"Cuda devices found by tensorflow: {tf.config.list_physical_devices('GPU')}")
     print(f"Cuda is built with tensorflow: {tf.test.is_built_with_cuda()}")
