e2109526125180ac1c97248c3a9b37b616517723
Charles-Fieseler-Vienna <83711538+Charles-Fieseler-Vienna@users.noreply.github.com>
2025-04-16 16:27:28 +0200

    Script to export behavior + pca videos of all freely moving datasets; related to #753

---
+++ wbfm/scripts/hardcoded_protocols/trace_exporting/export_pca_movies.py
@@ -0,0 +1,40 @@
+import os
+
+from tqdm.auto import tqdm
+import argparse
+
+from wbfm.utils.general.hardcoded_paths import load_paper_datasets
+from wbfm.utils.visualization.utils_export_videos import save_video_of_heatmap_and_pca_with_behavior
+
+if __name__ == '__main__':
+    # Get args
+    parser = argparse.ArgumentParser(description='Export traces in nwb format')
+    # Debug mode
+    parser.add_argument('--debug', action='store_true', help='Debug mode')
+    args = parser.parse_args()
+
+    DEBUG = args.debug
+
+    # Export to hardcoded locations
+    parent_dir = '/lisc/user/fieseler/zimmer/fieseler/paper/pca_movies'
+    all_suffixes = ['gfp', '', 'mutant']  # don't include immob
+
+    for suffix in tqdm(all_suffixes):
+        subfolder_name = f'movies_{suffix}'
+        all_projects = load_paper_datasets(suffix)
+
+        for name, project in all_projects.items():
+            this_folder = os.path.join(parent_dir, subfolder_name)
+            output_fname = os.path.join(this_folder, f'{project.shortened_name}.mp4')
+
+            # Skip if file exists
+            # if args.skip_if_exists and project.exported_data_path.exists():
+            #     print(f'Skipping {project.exported_data_path}')
+            #     continue
+
+            # Export data
+            save_video_of_heatmap_and_pca_with_behavior(project, output_fname=output_fname)
+
+            if DEBUG:
+                print(f'Exported {name} to {this_folder}, breaking')
+                break
