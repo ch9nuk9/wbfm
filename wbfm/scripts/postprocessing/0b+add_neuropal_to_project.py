146532327994db3651bcb4f25b136de8993613c1
Charles-Fieseler-Vienna <charles.fieseler@univie.ac.at>
2025-04-17 14:13:43 +0200

    Helper script for adding neuropal to existing projects; related to #638

---
+++ wbfm/scripts/postprocessing/0b+add_neuropal_to_project.py
@@ -0,0 +1,54 @@
+"""
+"""
+
+# main function
+import os
+import subprocess
+from pathlib import Path
+
+# Experiment tracking
+import sacred
+
+from wbfm.utils.external.utils_zarr import zip_raw_data_zarr
+from sacred import Experiment
+from sacred import SETTINGS
+from sacred.observers import TinyDbObserver
+from wbfm.utils.external.monkeypatch_json import using_monkeypatch
+
+from wbfm.pipeline.project_initialization import write_data_subset_using_config, zip_zarr_using_config, \
+    subtract_background_using_config
+from wbfm.utils.projects.project_config_classes import ModularProjectConfig
+from wbfm.utils.projects.utils_project import safe_cd
+from wbfm.utils.projects.utils_neuropal import add_neuropal_to_project
+import cgitb
+cgitb.enable(format='text')
+
+SETTINGS.CONFIG.READ_ONLY_CONFIG = False
+
+# Initialize sacred experiment
+
+ex = Experiment(save_git_info=False)
+ex.add_config(project_path=None, raw_neuropal_path=None, copy_data=True)
+
+
+@ex.config
+def cfg(project_path, DEBUG):
+    # Manually load yaml files
+    cfg = ModularProjectConfig(project_path)
+
+    if not DEBUG:
+        using_monkeypatch()
+        # log_dir = cfg.get_log_dir()
+        # ex.observers.append(TinyDbObserver(log_dir))
+
+
+@ex.automain
+def main(_config, _run):
+    sacred.commands.print_config(_run)
+
+    project_path = _config['project_path']
+    raw_neuropal_path = _config['raw_neuropal_path']
+    copy_data = _config['copy_data']
+
+    add_neuropal_to_project(project_path, raw_neuropal_path, copy_data=copy_data)
+
