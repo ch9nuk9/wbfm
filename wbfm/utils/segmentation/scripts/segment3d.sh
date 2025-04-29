b82a4fe158a4a1f72420cb8fb8690da060bbf353
Charles-Fieseler-Vienna <charles.fieseler@univie.ac.at>
2025-04-29 11:14:00 +0200

    Delete old segmentation files

--- wbfm/utils/segmentation/scripts/segment3d.sh
+++
@@ -1,6 +0,0 @@
-#!C:\Users\charles.fieseler\AppData\Local\Programs\Git\usr\bin\bash.exe
-
-DAT_PATH="D:\More-stabilized-wbfm\test2020-10-22_16-15-20_test4-channel-0-pco_camera1\test2020-10-22_16-15-20_test4-channel-0-pco_camera1bigtiff.btf"
-COMMAND="scripts/segment3d.py"
-# echo $(pwd)
-python $COMMAND with video_path=$DAT_PATH dataset_params.start_volume=100 segmentation_params.stardist_model_name='charlie_3d' 
