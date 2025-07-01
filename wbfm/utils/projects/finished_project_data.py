7982d599e92eac28e15dcdb252d6ca9d8a078493
Charles-Fieseler-Vienna <83711538+Charles-Fieseler-Vienna@users.noreply.github.com>
2025-07-01 17:39:51 +0200

    Implement segmentation loading in hybrid projects; #733

--- wbfm/utils/projects/finished_project_data.py
+++ wbfm/utils/projects/finished_project_data.py
@@ -686,28 +686,33 @@ class ProjectData:
 
         # Hybrid loading using both styles:
         #   If the project was loaded via a config file path, but has steps missing, then try to also load the nwb
-        preprocessing_class = project_data.project_config.get_preprocessing_class()
-        has_raw_data = preprocessing_class.has_raw_data
-        if not loaded_via_nwb and allow_hybrid_loading and not has_raw_data:
-            project_data.logger.info("Missing raw data when loading from project config, "
-                                     "attempting to load remaining steps from nwb file")
+        if not loaded_via_nwb and allow_hybrid_loading:
             cfg_nwb = project_data.project_config.get_nwb_config()
             nwb_filename = cfg_nwb.resolve_relative_path_from_config('nwb_filename')
             if nwb_filename is not None:
-                project_data.logger.info(f"Found nwb file at {nwb_filename}")
+                project_data.logger.info(f"Found nwb file at {nwb_filename}; attempting to additional data or analysis")
                 initialization_kwargs = kwargs.get('initialization_kwargs', dict())
                 project_data_nwb = ProjectData.load_final_project_data_from_nwb(nwb_filename, **initialization_kwargs)
-                # For now, only allow raw data to be loaded from the nwb file
+                # First: raw data loaded from the nwb file
                 nwb_preprocessing_class = project_data_nwb.project_config.get_preprocessing_class()
-                if nwb_preprocessing_class.has_raw_data:
-                    # Merge the classes (data from nwb, proper config file links from project)
-                    nwb_preprocessing_class.cfg_preprocessing = preprocessing_class.cfg_preprocessing
-                    nwb_preprocessing_class.cfg_project = preprocessing_class.cfg_project
-                    project_data.project_config._preprocessing_class = nwb_preprocessing_class
-                    project_data._nwb_io = project_data_nwb._nwb_io
-                    project_data.logger.info(f"Successfully imported raw data from nwb file")
-                else:
-                    project_data.logger.info(f"Did not find raw data in nwb file, continuing")
+                preprocessing_class = project_data.project_config.get_preprocessing_class()
+                if not preprocessing_class.has_raw_data:
+                    if nwb_preprocessing_class.has_raw_data:
+                        # Merge the classes (data from nwb, proper config file links from project)
+                        nwb_preprocessing_class.cfg_preprocessing = preprocessing_class.cfg_preprocessing
+                        nwb_preprocessing_class.cfg_project = preprocessing_class.cfg_project
+                        project_data.project_config._preprocessing_class = nwb_preprocessing_class
+                        project_data._nwb_io = project_data_nwb._nwb_io
+                        project_data.logger.info(f"Successfully imported raw data from nwb file")
+                    else:
+                        project_data.logger.info(f"Did not find raw data in nwb file, continuing")
+                # Second: load raw segmentation
+                if project_data.raw_segmentation is None:
+                    if project_data_nwb.raw_segmentation is not None:
+                        project_data.raw_segmentation = project_data_nwb.raw_segmentation
+                        project_data.logger.info(f"Successfully loaded raw segmentation from nwb file (no metadata loaded)")
+                    else:
+                        project_data.logger.info(f"Did not find raw segmentation in nwb file, continuing")
             else:
                 project_data.logger.info(f"Found no nwb file, continuing")
 
